6607859918a2fcb7e715291b0ad4f6ea54548f07
Jan Hoek <github@uncommonsense.nl>
2015-04-15 16:27:35 +0200

    More property types!

---
+++ CBreeze.NextGen/CBreeze.NextGen/NullableIntProperty.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CBreeze.NextGen
+{
+	public class NullableIntProperty : ValueProperty<int?>
+	{
+		internal NullableIntProperty(string name)
+			: base(name)
+		{
+		}
+
+		public override bool HasValue
+		{
+			get
+			{
+				return Value.HasValue;
+			}
+		}
+	}
+}
+
---
+++ CBreeze.NextGen/CBreeze.NextGen/StringProperty.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CBreeze.NextGen
+{
+	public class StringProperty : ValueProperty<string>
+	{
+		internal StringProperty(string name)
+			: base(name)
+		{
+		}
+
+		public override bool HasValue
+		{
+			get
+			{
+				return Value != null;
+			}
+		}
+
+	}
+}
+
