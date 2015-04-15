05212c1628b9febb00521261e9272100274a5e1f
Jan Hoek <github@uncommonsense.nl>
2015-04-15 22:37:58 +0200

    Added more properties/property types

---
+++ CBreeze.NextGen/CBreeze.NextGen/TriggerProperty.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CBreeze.NextGen
+{
+	public class TriggerProperty : ReferenceProperty<Trigger>
+	{
+		internal TriggerProperty(string name) : base(name)
+		{
+		}
+
+		public override bool HasValue
+		{
+			get
+			{
+				return false; // FIXME
+			}
+		}
+	}
+}
+
