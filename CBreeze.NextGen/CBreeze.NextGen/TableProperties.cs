a2fd645f0cac3c7b12ac913853b89b6a91823603
Jan Hoek <github@uncommonsense.nl>
2015-04-18 20:39:47 +0200

    Added more table properties

--- CBreeze.NextGen/CBreeze.NextGen/TableProperties.cs
+++ CBreeze.NextGen/CBreeze.NextGen/TableProperties.cs
@@ -4,110 +4,107 @@ using System.Collections.Generic;
 
 namespace CBreeze.NextGen
 {
-	public class TableProperties : Properties
-	{
-		private MultiLanguageProperty captionML = new MultiLanguageProperty("CaptionML");
-		private FieldListProperty dataCaptionFields = new FieldListProperty("DataCaptionFields");
-		private NullableIntProperty lookupPageID = new NullableIntProperty("LookupPageID");
-		private NullableIntProperty drillDownPageID = new NullableIntProperty("DrillDownPageID");
-		private NullableBoolProperty dataPerCompany = new NullableBoolProperty("DataPerCompany");
-		private StringProperty description = new StringProperty("Description");
-		//		private NullableBoolProperty linkedInTransaction = new NullableBoolProperty("LinkedInTransaction");
-		//		private NullableBoolProperty linkedObject = new NullableBoolProperty("LinkedObject");
-		//		private TriggerProperty onInsert = new TriggerProperty("OnInsert");
-		//		private TriggerProperty onModify = new TriggerProperty("OnModify");
-		//		private TriggerProperty onDelete = new TriggerProperty("OnDelete");
-		//		private TriggerProperty onRename = new TriggerProperty("OnRename");
+    public class TableProperties : Properties
+    {
+        private MultiLanguageProperty captionML = new MultiLanguageProperty("CaptionML");
+        private FieldListProperty dataCaptionFields = new FieldListProperty("DataCaptionFields");
+        private NullableIntProperty lookupPageID = new NullableIntProperty("LookupPageID");
+        private NullableIntProperty drillDownPageID = new NullableIntProperty("DrillDownPageID");
+        private NullableBoolProperty dataPerCompany = new NullableBoolProperty("DataPerCompany");
+        private StringProperty description = new StringProperty("Description");
+        private NullableBoolProperty linkedInTransaction = new NullableBoolProperty("LinkedInTransaction");
+        private NullableBoolProperty linkedObject = new NullableBoolProperty("LinkedObject");
+        private TriggerProperty onInsert = new TriggerProperty("OnInsert");
+        private TriggerProperty onModify = new TriggerProperty("OnModify");
+        private TriggerProperty onDelete = new TriggerProperty("OnDelete");
+        private TriggerProperty onRename = new TriggerProperty("OnRename");
+        private NullableBoolProperty pasteIsValid = new NullableBoolProperty("PasteIsValid");
+        private PermissionsProperty permissions = new PermissionsProperty("Permissions");
 
-		/*
-			<member name="PasteIsValid" propertyType="NullableBooleanProperty" fieldName="pasteIsValid" sortOrder="100"/>
-			<member name="Permissions" propertyType="PermissionsProperty" fieldName="permissions" sortOrder="20"/>
-         */
+        internal TableProperties(Node parentNode)
+            : base(parentNode)
+        {
+        }
 
-		internal TableProperties(Node parentNode)
-			: base(parentNode)
-		{
-		}
+        public override string ToString()
+        {
+            return "Properties";
+        }
 
-		public override string ToString()
-		{
-			return "Properties";
-		}
+        public MultiLanguageValue CaptionML
+        {
+            get
+            {
+                return this.captionML.Value;
+            }
+        }
 
-		public MultiLanguageValue CaptionML
-		{
-			get
-			{
-				return this.captionML.Value;
-			}
-		}
+        public FieldList DataCaptionFields
+        {
+            get
+            {
+                return this.dataCaptionFields.Value;
+            }
+        }
 
-		public FieldList DataCaptionFields
-		{
-			get
-			{
-				return this.dataCaptionFields.Value;
-			}
-		}
+        public int? LookupPageID
+        {
+            get
+            {
+                return this.lookupPageID.Value;
+            }
+            set
+            {
+                this.lookupPageID.Value = value;
+            }
+        }
 
-		public int? LookupPageID
-		{
-			get
-			{
-				return this.lookupPageID.Value;
-			}
-			set
-			{
-				this.lookupPageID.Value = value;
-			}
-		}
+        public int? DrillDownPageID
+        {
+            get
+            {
+                return this.drillDownPageID.Value;
+            }
+            set
+            {
+                this.drillDownPageID.Value = value;
+            }
+        }
 
-		public int? DrillDownPageID
-		{
-			get
-			{
-				return this.drillDownPageID.Value;
-			}
-			set
-			{
-				this.drillDownPageID.Value = value;
-			}
-		}
+        public bool? DataPerCompany
+        {
+            get
+            {
+                return this.dataPerCompany.Value;
+            }
+            set
+            {
+                this.dataPerCompany.Value = value;
+            }
+        }
 
-		public bool? DataPerCompany
-		{
-			get
-			{
-				return this.dataPerCompany.Value;
-			}
-			set
-			{
-				this.dataPerCompany.Value = value;
-			}
-		}
+        public string Description
+        {
+            get
+            {
+                return this.description.Value;
+            }
+            set
+            {
+                this.description.Value = value;
+            }
+        }
 
-		public string Description
-		{
-			get
-			{
-				return this.description.Value;
-			}
-			set
-			{
-				this.description.Value = value;
-			}
-		}
-
-		public override IEnumerable<INode> ChildNodes
-		{
-			get
-			{
-				yield return captionML;
-				yield return dataPerCompany;
-				yield return dataCaptionFields;
-				yield return lookupPageID;
-			}
-		}
-	}
+        public override IEnumerable<INode> ChildNodes
+        {
+            get
+            {
+                yield return captionML;
+                yield return dataPerCompany;
+                yield return dataCaptionFields;
+                yield return lookupPageID;
+            }
+        }
+    }
 }
 
