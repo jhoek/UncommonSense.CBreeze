0067540f4b33adf9a22cebb9e5d2bfe9defe903f
Jan Hoek <github@uncommonsense.nl>
2015-04-30 19:47:22 +0200

    Added more conversion logic.
    Note to self: figure out how to instantiate a type with params in its
    ctor.

--- UncommonSense.CBreeze.DomBuilder/ConvertTo-CompilationUnit.ps1
+++ UncommonSense.CBreeze.DomBuilder/ConvertTo-CompilationUnit.ps1
@@ -6,8 +6,8 @@
         [UncommonSense.CBreeze.ObjectModelBuilder.ObjectModel]$ObjectModel
     )
 
-    $ObjectModel.Elements | Where-Object -Is [UncommonSense.CBreeze.ObjectModelBuilder.Item] | Convert-ItemToCompilationUnit
-    $ObjectModel.Elements | Where-Object -Is [UncommonSense.CBreeze.ObjectModelBuilder.Container] | Convert-ContainerToCompilationUnit
+    $ObjectModel.Elements | Where-Object { $_ -is [UncommonSense.CBreeze.ObjectModelBuilder.Item] } | Convert-ItemToCompilationUnit
+    $ObjectModel.Elements | Where-Object { $_ -is [UncommonSense.CBreeze.ObjectModelBuilder.Container] } | Convert-ContainerToCompilationUnit
 }
 
 function Convert-ItemToCompilationUnit
@@ -20,14 +20,19 @@ function Convert-ItemToCompilationUnit
 
     Process
     {
-        $CompilationUnit = New-Object -TypeName UncommonSense.CSharp.CompilationUnit -ArgumentList $Item.ObjectModel.Namespaces
+        $Class = New-Object -TypeName UncommonSense.CSharp.Class -ArgumentList [UncommonSense.CSharp.Visibility]::Public, $Item.Name, $Item.BaseTypeName
+        $Namespace = New-Object -TypeName UncommonSense.CSharp.Namespace -ArgumentList $Item.ObjectModel.Namespace, $Class
+        $CompilationUnit = New-Object UncommonSense.CSharp.CompilationUnit -ArgumentList $Namespace
+
         $CompilationUnit
     }
 }
 
+Clear-Host
 Add-Type -Path C:\Users\jhoek\GitHub\UncommonSense.CSharp\UncommonSense.CSharp\bin\Debug\UncommonSense.CSharp.dll
 Add-Type -Path C:\Users\jhoek\GitHub\UncommonSense.CBreeze\UncommonSense.CBreeze.DomBuilder\UncommonSense.CBreeze.ObjectModelBuilder\bin\Debug\UncommonSense.CBreeze.ObjectModelBuilder.dll
 
+$ErrorActionPreference = 'Stop'
 $ObjectModel = New-Object -TypeName UncommonSense.CBreeze.ObjectmodelBuilder.ObjectModel -ArgumentList MyNamespace
 $Item = New-Object -TypeName UncommonSense.CBreeze.ObjectModelBuilder.Item -ArgumentList "MyItem"
 $ObjectModel.Elements.Add($Item)
