4c3c8f7215ec5d3b4dd19a811dee340f2a9eabee
Jan Hoek <github@uncommonsense.nl>
2015-05-17 17:21:15 +0200

    Add Integer variable

---
+++ CBreeze.NextGen/CBreeze.NextGen/IntegerVariable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CBreeze.NextGen
+{
+    public class IntegerVariable : Variable
+    {
+        public IntegerVariable(int uid, string name)
+            : base(uid, name)
+        {
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}@{1} : Integer", Name, UID);
+        }
+    }
+}
