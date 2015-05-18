9c0dae767c197aa3fdf43c847bfaedb92ea18518
Jan Hoek <github@uncommonsense.nl>
2015-05-18 22:35:44 +0200

    Add BooleanVariable and ByteVariable

---
+++ CBreeze.NextGen/CBreeze.NextGen/BooleanVariable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CBreeze.NextGen
+{
+    public class BooleanVariable : Variable
+    {
+        public BooleanVariable(int uid, string name) : base(uid, name)
+        {
+        }
+
+        public bool? IncludeInDataset
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ CBreeze.NextGen/CBreeze.NextGen/ByteVariable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CBreeze.NextGen
+{
+    public class ByteVariable : Variable
+    {
+        public ByteVariable(int uid, string name)
+            : base(uid, name)
+        {
+        }
+    }
+}
