eac3fa7993f831c65fb727d15dc035f913971844
Jan Hoek <github@uncommonsense.nl>
2015-05-19 10:41:30 +0200

    Added more parameter types

---
+++ CBreeze.NextGen/CBreeze.NextGen/DateParameter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CBreeze.NextGen
+{
+    public class DateParameter : Parameter
+    {
+        public DateParameter(int uid, string name)
+            : base(uid, name)
+        {
+        }
+    }
+}
---
+++ CBreeze.NextGen/CBreeze.NextGen/DateTimeParameter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CBreeze.NextGen
+{
+    public class DateTimeParameter : Parameter
+    {
+        public DateTimeParameter(int uid, string name) : base(uid, name)
+        {
+        }
+    }
+}
