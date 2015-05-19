a2db96d120e80d8b761ec738a507a714356f31fd
Jan Hoek <github@uncommonsense.nl>
2015-05-19 13:29:33 +0200

    Adding DateFormulaParameter

---
+++ CBreeze.NextGen/CBreeze.NextGen/DateFormulaParameter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CBreeze.NextGen
+{
+    public class DateFormulaParameter : Parameter
+    {
+        public DateFormulaParameter(int uid, string name)
+            : base(uid, name)
+        {
+        }
+    }
+}
