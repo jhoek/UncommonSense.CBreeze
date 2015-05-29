cd167b5b10e060e315dcf0dd86f3b3b485b4f5bb
Jan Hoek <github@uncommonsense.nl>
2015-05-29 21:16:12 +0200

    Restructured demo code; added property collections

--- UncommonSense.CBreeze.DomBuilder/UncommonSense.CBreeze.ObjectModelBuilder.Demo/Program.cs
+++ UncommonSense.CBreeze.DomBuilder/UncommonSense.CBreeze.ObjectModelBuilder.Demo/Program.cs
@@ -7,55 +7,32 @@ using UncommonSense.CSharp;
 
 namespace UncommonSense.CBreeze.ObjectModelBuilder.Demo
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			var objectModel = 
-				new ObjectModel(
-					"Demo",
-                    new Enumeration(
-                        "BlankNumbers", 
-                        "DontBlank", 
-                        "BlankNeg", 
-                        "BlankNegAndZero", 
-                        "BlankZero", 
-                        "BlankZeroAndPos", 
-                        "BlankPos"),
-					new Item(
-						"Application",
-						new ReferenceAttribute("Tables", "Tables"),
-						new ReferenceAttribute("Pages", "Pages"),
-						new ReferenceAttribute("Reports", "Reports"),
-						new ReferenceAttribute("Codeunits", "Codeunits")
-					),
-					new Item(
-						"Object",
-						new ValueAttribute("int", "ID"),
-						new ValueAttribute("string", "Name")
-					).MakeAbstract(),
-					new Item(
-						"Table",
-						"Object"
-					),
-					new Item(
-						"Page",
-						"Object"
-					),
-					new Container(
-						"Table",
-						"Tables"
-					),
-					new Container(
-						"Page",
-						"Pages"
-					)
-				);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var objectModel =
+                new ObjectModel(
+                    "Demo",
+                    new Enumeration("BlankNumbers", "DontBlank", "BlankNeg", "BlankNegAndZero", "BlankZero", "BlankZeroAndPos", "BlankPos"),
+                    new Item("Application", new ReferenceAttribute("Tables", "Tables"), new ReferenceAttribute("Pages", "Pages"), new ReferenceAttribute("Reports", "Reports"), new ReferenceAttribute("Codeunits", "Codeunits")),
+                    new Item("Object", new ValueAttribute("int", "ID"), new ValueAttribute("string", "Name")).MakeAbstract(),
+                    new Item("Table", "Object", new ReferenceAttribute("TableProperties", "Properties")),
+                    new Item("Page", "Object"),
+                    new Item("Report", "Object"),
+                    new Item("Codeunit", "Object", new ReferenceAttribute("CodeunitProperties", "Properties")),
+                    new Container("Table", "Tables"),
+                    new Container("Page", "Pages"),
+                    new Container("Report", "Reports"),
+                    new Container("Codeunit", "Codeunits"),
+                    new Properties("TableProperties"),
+                    new Properties("CodeunitProperties")
+                );
 
-			foreach (var compilationUnit in objectModel.ToCompilationUnits())
-			{
-				compilationUnit.WriteTo(Console.Out);
-			}
-		}
-	}
+            foreach (var compilationUnit in objectModel.ToCompilationUnits())
+            {
+                compilationUnit.WriteTo(Console.Out);
+            }
+        }
+    }
 }
