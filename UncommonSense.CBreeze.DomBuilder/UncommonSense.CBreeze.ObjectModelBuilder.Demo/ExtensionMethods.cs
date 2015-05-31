066af030d89fc4f28d188efee4d56085267fb7ee
Jan Hoek <github@uncommonsense.nl>
2015-05-31 11:22:36 +0200

    More extension methods to allow a fluent-like interface

--- UncommonSense.CBreeze.DomBuilder/UncommonSense.CBreeze.ObjectModelBuilder.Demo/ExtensionMethods.cs
+++ UncommonSense.CBreeze.DomBuilder/UncommonSense.CBreeze.ObjectModelBuilder.Demo/ExtensionMethods.cs
@@ -4,11 +4,34 @@ namespace UncommonSense.CBreeze.ObjectModelBuilder.Demo
 {
 	public static class ExtensionMethods
 	{
-		public static Item MakeAbstract(this Item item)
-		{
-			item.Abstract = true;
-			return item;
-		}
+        public static Item AddReferenceAttribute(this Item item, string typeName, string name)
+        {
+            item.Attributes.Add(new ReferenceAttribute(typeName, name));
+            return item;
+        }
+
+        public static Item AddValueAttribute(this Item item, string typeName, string name)
+        {
+            item.Attributes.Add(new ValueAttribute(typeName, name));
+            return item;
+        }
+
+        public static Item AddIdentifierAttribute(this Item item, string typeName, string name)
+        {
+            item.Attributes.Add(new IdentifierAttribute(typeName, name));
+            return item;
+        }
+
+        public static Item AddAnotherDerivedItem(this Item item, string name)
+        {
+            return item.BaseType.AddDerivedItem(name);
+        }
+
+        public static Item AddDerivedItem(this Item item, string name)
+        {
+            item.Abstract = true;
+            return item.ObjectModel.Elements.Add(new Item(name, item.Name));
+        }
 	}
 }
 
