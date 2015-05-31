0096b35ace42e6ba0ddb6c2c04aea46fa3471b1f
Jan Hoek <github@uncommonsense.nl>
2015-05-31 11:24:29 +0200

    Introduced IdentifierAttribute
    Used for attributes (properties) that are set from the ctor, and have an
    internal setter.

---
+++ UncommonSense.CBreeze.DomBuilder/UncommonSense.CBreeze.ObjectModelBuilder/IdentifierAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UncommonSense.CBreeze.ObjectModelBuilder
+{
+    public class IdentifierAttribute : ValueAttribute
+    {
+        public IdentifierAttribute(string typeName, string name)
+            : base(typeName, name)
+        {
+        }
+    }
+}
