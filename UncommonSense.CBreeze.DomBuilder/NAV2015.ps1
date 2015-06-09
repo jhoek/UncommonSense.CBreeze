f8b206a2ee821db100369bbb3c40f906ea16a23b
Jan Hoek <github@uncommonsense.nl>
2015-06-09 10:04:24 +0200

    Moved TableField items; added some Variable items

--- UncommonSense.CBreeze.DomBuilder/NAV2015.ps1
+++ UncommonSense.CBreeze.DomBuilder/NAV2015.ps1
@@ -9,11 +9,7 @@ Add-Enum -Name BlankNumbers -Values DontBlank,BlankNeg,BlankNegAndZero,BlankZero
 Add-Enum -Name BlobSubType -Values UserDefined,Bitmap,Memo 
 Add-Enum -Name FieldClass -Values Normal,FlowField,FlowFilter
 Add-Enum -Name MinOccurs -Values Once,Zero 
-Add-Enum -Name MaxOccurs -Values Unbounded,Once 
-
-Add-Item -Name TableField -Abstract | Add-Attribute -Name No -TypeName int -ValueAttribute| Add-Attribute -Name Name -TypeName string -ValueAttribute | Add-Attribute -Name Enabled -TypeName bool? -ValueAttribute | Out-Null
-Add-Item -Name IntegerTableField -BaseTypeName TableField | Add-Attribute -Name Properties -TypeName IntegerTableFieldProperties | Out-null
-Add-Item -Name DecimalTableField -BaseTypeName TableField | Add-Attribute -Name Properties -Typename DecimalTableFieldProperties | Out-Null
+Add-Enum -Name MaxOccurs -Values Unbounded,Once
 
 Add-Item -Name Application | Add-Attribute -TypeName Tables | Add-Attribute -TypeName Pages |Add-Attribute -TypeName Reports | Add-Attribute -TypeName Codeunits | Add-Attribute -TypeName XmlPorts | Add-Attribute -TypeName Queries | Add-Attribute -TypeName MenuSuites| out-null
 Add-Item -Name Object -Abstract | Add-Attribute -Name ID -TypeName int -ValueAttribute | Add-Attribute -Name Name -TypeName string -ValueAttribute | out-null
@@ -24,6 +20,16 @@ Add-Item -Name Codeunit -BaseTypeName Object -CreateContainer| Add-Attribute -Na
 Add-Item -Name XmlPort -BaseTypeName Object -CreateContainer| out-null
 Add-Item -Name Query -BaseTypeName Object -CreateContainer -ContainerName Queries| out-null
 Add-Item -Name MenuSuite -BaseTypeName Object -CreateContainer | out-null
+
+Add-Item -Name TableField -Abstract | Add-Attribute -Name No -TypeName int -ValueAttribute| Add-Attribute -Name Name -TypeName string -ValueAttribute | Add-Attribute -Name Enabled -TypeName bool? -ValueAttribute | Out-Null
+Add-Item -Name BigIntegerTableField -BaseTypeName TableField -PassThru:$false
+Add-Item -Name BinaryTableField -BaseTypeName TableField -PassThru:$false
+Add-Item -Name BlobTableField -BaseTypeName TableField -PassThru:$false
+Add-Item -Name IntegerTableField -BaseTypeName TableField | Add-Attribute -Name Properties -TypeName IntegerTableFieldProperties | Out-null
+Add-Item -Name DecimalTableField -BaseTypeName TableField | Add-Attribute -Name Properties -Typename DecimalTableFieldProperties | Out-Null
+
+Add-Item -Name Variable -Abstract | Add-Attribute -Name ID -TypeName int -ValueAttribute | Add-Attribute -Name Name -TypeName string -ValueAttribute | Out-Null
+Add-Item -Name ActionVariable -BaseTypeName Variable | Add-Attribute -Name Dimensions -TypeName string -ValueAttribute | Out-Null
 
 Add-Properties -Name TableProperties 
 Add-Properties -Name CodeunitProperties
