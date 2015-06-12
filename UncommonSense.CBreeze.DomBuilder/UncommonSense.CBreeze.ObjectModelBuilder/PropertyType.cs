f65661d00f21d3b363c11bf8d44f444f721f234d
Jan Hoek <github@uncommonsense.nl>
2015-06-12 18:26:32 +0200

    Added PropertyType class

---
+++ UncommonSense.CBreeze.DomBuilder/UncommonSense.CBreeze.ObjectModelBuilder/PropertyType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UncommonSense.CBreeze.ObjectModelBuilder
+{
+    public class PropertyType : ObjectModelElement
+    {
+        public PropertyType(string name, string innerValue)
+            : base(name)
+        {
+            InnerValue = innerValue;
+        }
+
+        public string InnerValue
+        {
+            get;
+            internal set;
+        }
+    }
+}
