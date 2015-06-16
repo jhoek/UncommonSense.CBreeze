3495f9be24c7da3dfff813c83f8df285692d4b40
Jan Hoek <github@uncommonsense.nl>
2015-06-16 22:28:26 +0200

    Reintroduced builder project for NAV 2013

---
+++ UncommonSense.CBreeze.DomBuilder/UncommonSense.CBreeze.DomBuilder.70/ExtensionMethods.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UncommonSense.CBreeze.DomBuilder._70
+{
+    public static class ExtensionMethods
+    {
+
+    }
+}
---
+++ UncommonSense.CBreeze.DomBuilder/UncommonSense.CBreeze.DomBuilder.70/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UncommonSense.CBreeze.ObjectModelBuilder;
+
+namespace UncommonSense.CBreeze.DomBuilder._70
+{
+    class Program
+    {
+        const string @namespace = "UncommonSense.CBreeze.70.Core";
+        static ObjectModel objectModel = new ObjectModel(@namespace);
+
+        static void Main(string[] args)
+        {
+            Item("Application");
+
+            Item("Object")
+        }
+
+        static Item Item(string name)
+        {
+            var item = new Item(name);
+            objectModel.Elements.Add(item);
+            return item;
+        }
+
+        static Item Derive(string name)
+        {
+            var item = new Item(name, 
+        }
+    }
+}
