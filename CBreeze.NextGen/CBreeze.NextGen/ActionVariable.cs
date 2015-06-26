57ec0977cd846d536f5d9294a44deb5b72303752
Jan Hoek <github@uncommonsense.nl>
2015-06-26 19:16:52 +0200

    Restoring ActionVariable

---
+++ CBreeze.NextGen/CBreeze.NextGen/ActionVariable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CBreeze.NextGen
+{
+	public class ActionVariable : Variable
+	{
+		public ActionVariable(int uid, string name)
+			: base(uid, name)
+		{
+		}
+
+		public override VariableType Type
+		{
+			get
+			{
+				return VariableType.Action;
+			}
+		}
+	}
+}
