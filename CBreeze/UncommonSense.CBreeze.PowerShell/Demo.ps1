63f0e4cb99a9d6ad7d994278e6c87256a9bc623f
Jan Hoek <github@uncommonsense.nl>
2015-06-26 17:15:19 +0200

    Removing old C#-based PowerShell project

--- CBreeze/UncommonSense.CBreeze.PowerShell/Demo.ps1
+++
@@ -1,7 +0,0 @@
-﻿# Import-Module ".\UncommonSense.CBreeze.PowerShell.dll" -Verbose
-
-$manifest = New-CBreezeSetupEntityType -TableID 50000 -TableName "Foo Setup" -PageID 50000 -PageName "Foo Setup"
-$manifest.Table.Fields.AddCodeTableField(2, "Foo", 50) | Out-Null
-
-$manifest = $manifest.Application | New-CBreezeSetupEntityType -TableID 50001 -TableName "Baz Setup" -PageID 50001 -PageName "Baz Setup" 
-$manifest.Application | ConvertTo-NavObjectText 
