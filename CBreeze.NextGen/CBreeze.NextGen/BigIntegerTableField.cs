51edd6dfe6e04717ac0d06481f096e93fc0613d0
Jan Hoek <github@uncommonsense.nl>
2015-06-26 23:02:02 +0200

    Added more variable, field and parameter types

---
+++ CBreeze.NextGen/CBreeze.NextGen/BigIntegerTableField.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CBreeze.NextGen
+{
+	public class BigIntegerTableField : TableField
+	{
+		public BigIntegerTableField(int id, string name) : base(id, name)
+		{
+			Properties = new BigIntegerTableFieldProperties(this);
+		}
+
+		public override TableFieldType Type
+		{
+			get
+			{
+				return TableFieldType.BigInteger;
+			}
+		}
+
+		public BigIntegerTableFieldProperties Properties
+		{
+			get;
+			internal set;
+		}
+
+		public override string TypeName
+		{
+			get
+			{
+				return "BigInteger";
+			}
+		}
+	}
+}
+
---
+++ CBreeze.NextGen/CBreeze.NextGen/BigIntegerVariable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CBreeze.NextGen
+{
+	public class BigIntegerVariable : Variable
+	{
+		public BigIntegerVariable(int uid, string name) : base(uid, name)
+		{
+		}
+
+		public override VariableType Type
+		{
+			get
+			{
+				return VariableType.BigInteger;
+			}
+		}
+	}
+}
+
