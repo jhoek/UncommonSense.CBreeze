f635025dc14badfd35da5aa4743bfbe19c842510
Jan Hoek <github@uncommonsense.nl>
2015-06-26 08:43:22 +0200

    Derived PageActionContainer from PageActionBase

--- CBreeze.NextGen/CBreeze.NextGen/PageActionBase.cs
+++ CBreeze.NextGen/CBreeze.NextGen/PageActionBase.cs
@@ -2,6 +2,14 @@
 
 namespace CBreeze.NextGen
 {
+	// Normally, names for derived types are formed by prefixing the base type
+	// name with it's specialisation description, e.g. Parameter -> IntegerParameter.
+	// In case of page actions, the base class would be called PageAction, from
+	// which e.g. ContainerPageAction would be derived. However, that would make
+	// normal page actions ActionPageAction. Instead we'll call the base class
+	// PageActionBase, and the derived classes PageActionContainer, PageActionGroup
+	// PageActionSeparator and PageAction.
+
 	public abstract class PageActionBase : Node, IKeyedValue<int>, IEquatable<PageActionBase>
 	{
 		public PageActionBase(int id, string name, int? indentationLevel)
---
+++ CBreeze.NextGen/CBreeze.NextGen/PageActionContainer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CBreeze.NextGen
+{
+	public class PageActionContainer : PageActionBase
+	{
+		public PageActionContainer(int id, string name, int? indentationLevel)
+			: base(id, name, indentationLevel)
+		{
+			
+		}
+	}
+}
+
