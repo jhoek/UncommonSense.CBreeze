d051a7679cccc56578c611ca56b66a0b337c7629
Jan Hoek <github@uncommonsense.nl>
2015-06-26 19:15:03 +0200

    Introducing more variable, table field and parameter types

---
+++ CBreeze.NextGen/CBreeze.NextGen/ActionParameter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CBreeze.NextGen
+{
+	public class ActionParameter : Parameter
+	{
+		public ActionParameter(int uid, string name)
+			: base(uid, name)
+		{
+		}
+
+		public override ParameterType Type
+		{
+			get
+			{
+				return ParameterType.Action;
+			}
+		}
+	}
+}
+
