f0b89fd180e966e225036df060cd778f05b76f1f
Jan Hoek <github@uncommonsense.nl>
2015-06-26 12:43:25 +0200

    Worked on page action implementation

---
+++ CBreeze.NextGen/CBreeze.NextGen/PageActionContainer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CBreeze.NextGen
+{
+	public class PageActionContainer : PageActionBase
+	{
+		public PageActionContainer(int id, string name, int? indentationLevel)
+			: base(id, name, indentationLevel)
+		{
+			Properties = new PageActionContainerProperties(this);
+		}
+
+		public PageActionContainerProperties Properties
+		{
+			get;
+			internal set;
+		}
+
+		public override PageActionType Type
+		{
+			get
+			{
+				return PageActionType.Container;
+			}
+		}
+
+		public override IEnumerable<INode> ChildNodes
+		{
+			get
+			{
+				yield return Properties;
+			}
+		}
+	}
+}
+
