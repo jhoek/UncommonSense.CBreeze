d90c127784bec5b4467ba551df4493da277047a2
Jan Hoek <github@uncommonsense.nl>
2015-06-28 09:38:38 +0200

    Added boolean, byte parameter/table field/variable

---
+++ CBreeze.NextGen/CBreeze.NextGen/BooleanTableFieldProperties.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CBreeze.NextGen
+{
+	public class BooleanTableFieldProperties : Properties
+	{
+		internal BooleanTableFieldProperties(Node parentNode)
+			: base(parentNode)
+		{
+		}
+
+		public override System.Collections.Generic.IEnumerable<INode> ChildNodes
+		{
+			get
+			{
+				yield break; // FIXME
+			}
+		}
+	}
+}
+
