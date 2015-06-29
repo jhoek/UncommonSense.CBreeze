f6bc30443a31cc2c09cf2a7f56f31d89fbf92e5d
Jan Hoek <github@uncommonsense.nl>
2015-06-29 16:01:33 +0200

    Worked on XmlPortProperties

--- CBreeze.NextGen/CBreeze.NextGen/XmlPortProperties.cs
+++ CBreeze.NextGen/CBreeze.NextGen/XmlPortProperties.cs
@@ -8,16 +8,55 @@ namespace CBreeze.NextGen
 {
     public class XmlPortProperties : Properties
     {
+        private MultiLanguageProperty captionML = new MultiLanguageProperty("CaptionML");
+        private NullableValueProperty<bool> defaultFieldsValidation = new NullableValueProperty<bool>("DefaultFieldsValidation");
+        private StringProperty defaultNamespace = new StringProperty("DefaultNamespace");
+
         internal XmlPortProperties(Node parentNode)
             : base(parentNode)
         {
+        }
+
+        public MultiLanguageValue CaptionML
+        {
+            get
+            {
+                return this.captionML.Value;
+            }
+        }
+
+        public bool? DefaultFieldsValidation
+        {
+            get
+            {
+                return this.defaultFieldsValidation.Value;
+            }
+            set
+            {
+                this.defaultFieldsValidation.Value = value;
+            }
+        }
+
+        public string DefaultNamespace
+        {
+            get
+            {
+                return this.defaultNamespace.Value;
+            }
+            set
+            {
+                this.defaultNamespace.Value = value;
+            }
         }
 
         public override IEnumerable<INode> ChildNodes
         {
             get
             {
-                yield break; // FIXME
+                yield return captionML;
+                yield return defaultFieldsValidation;
+                yield return defaultNamespace;
+                // FIXME: Rest
             }
         }
     }
