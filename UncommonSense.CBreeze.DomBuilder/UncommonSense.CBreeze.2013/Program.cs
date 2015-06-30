49b5353786b689c684457a4affb456bb93c01e7a
Jan Hoek <github@uncommonsense.nl>
2015-06-30 10:38:32 +0200

    Added new builder project for C/Breeze 2013

---
+++ UncommonSense.CBreeze.DomBuilder/UncommonSense.CBreeze.2013/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UncommonSense.CBreeze.ObjectModelBuilder;
+using UncommonSense.CBreeze.ObjectModelWriter;
+
+namespace UncommonSense.CBreeze
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			new ObjectModel(
+				"UncommonSense.CBreeze.70.Core",
+				new Item(
+					"Application",
+					new ChildNode("Tables"),
+					new ChildNode("Pages")
+				)
+			).WriteToFolder(Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
+		}
+	}
+}
