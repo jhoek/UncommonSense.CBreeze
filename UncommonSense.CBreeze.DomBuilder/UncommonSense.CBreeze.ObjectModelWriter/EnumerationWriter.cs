f5d7e465f241820583309a54c9f90afefd2bc7fb
Jan Hoek <github@uncommonsense.nl>
2015-06-30 10:39:17 +0200

    Reformatted (?) the EnumerationWriter

--- UncommonSense.CBreeze.DomBuilder/UncommonSense.CBreeze.ObjectModelWriter/EnumerationWriter.cs
+++ UncommonSense.CBreeze.DomBuilder/UncommonSense.CBreeze.ObjectModelWriter/EnumerationWriter.cs
@@ -8,18 +8,18 @@ using System.IO;
 
 namespace UncommonSense.CBreeze.ObjectModelWriter
 {
-    public static class EnumerationWriter
-    {
-        public static void WriteToFolder(this Enumeration enumeration, string folderName)
-        {
-            var @enum = new UncommonSense.CSharp.Enum(Visibility.Public, enumeration.Name, IntegralType.None);
+	public static class EnumerationWriter
+	{
+		public static void WriteToFolder(this Enumeration enumeration, string folderName)
+		{
+			var @enum = new UncommonSense.CSharp.Enum(Visibility.Public, enumeration.Name, IntegralType.None);
 
-            foreach (var value in enumeration.Values)
-            {
-                @enum.Values.Add(new EnumValue(value));
-            }
+			foreach (var value in enumeration.Values)
+			{
+				@enum.Values.Add(new EnumValue(value));
+			}
 
-            @enum.WriteTo(Path.Combine(folderName, @enum.FileName));
-        }
-    }
+			@enum.WriteTo(Path.Combine(folderName, @enum.FileName));
+		}
+	}
 }
