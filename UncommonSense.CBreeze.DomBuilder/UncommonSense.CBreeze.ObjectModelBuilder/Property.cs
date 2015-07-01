4b928fb171fe566f689f6861b73bff913b06608d
Jan Hoek <github@uncommonsense.nl>
2015-07-01 09:51:50 +0200

    Simplified InternalName for properties

--- UncommonSense.CBreeze.DomBuilder/UncommonSense.CBreeze.ObjectModelBuilder/Property.cs
+++ UncommonSense.CBreeze.DomBuilder/UncommonSense.CBreeze.ObjectModelBuilder/Property.cs
@@ -2,42 +2,39 @@
 
 namespace UncommonSense.CBreeze.ObjectModelBuilder
 {
-    public class Property
-    {
-        public Property(string name, string typeName)
-        {
-            Name = name;
-            TypeName = typeName;
-        }
+	public class Property
+	{
+		public Property(string name, string typeName)
+		{
+			Name = name;
+			TypeName = typeName;
+		}
 
-        public string Name
-        {
-            get;
-            internal set;
-        }
+		public string Name
+		{
+			get;
+			internal set;
+		}
 
-        public string InternalName
-        {
-            get
-            {
-                if (Name == "ID")
-                    return "id";
+		public string InternalName
+		{
+			get
+			{
+				return Name.Substring(0, 1).ToLowerInvariant() + Name.Substring(1);
+			}
+		}
 
-                return Name.Substring(0, 1).ToLowerInvariant() + Name.Substring(1);
-            }
-        }
+		public string TypeName
+		{
+			get;
+			internal set;
+		}
 
-        public string TypeName
-        {
-            get;
-            internal set;
-        }
-
-        public PropertyCollection Collection
-        {
-            get;
-            internal set;
-        }
-    }
+		public PropertyCollection Collection
+		{
+			get;
+			internal set;
+		}
+	}
 }
 
