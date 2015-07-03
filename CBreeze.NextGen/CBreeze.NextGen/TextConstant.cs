9f700fef5e22bf8a04a4863895ab0e479217030e
Jan Hoek <github@uncommonsense.nl>
2015-07-03 10:54:51 +0200

    Added textconstant

---
+++ CBreeze.NextGen/CBreeze.NextGen/TextConstant.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CBreeze.NextGen
+{
+	public class TextConstant : Variable
+	{
+		public TextConstant(int uid, string name)
+			: base(uid, name)
+		{
+			Values = new MultiLanguageValue();
+		}
+
+		public override string ToString()
+		{
+			return string.Format("{0} {1}@{2}: {3}", Type, Name, UID, Values);
+		}
+
+		public override VariableType Type
+		{
+			get
+			{
+				return VariableType.TextConstant;
+			}
+		}
+
+		public MultiLanguageValue Values
+		{
+			get;
+			internal set;
+		}
+	}
+}
+
--- CBreeze.NextGen/CBreeze.NextGen/VariableType.cs
+++ CBreeze.NextGen/CBreeze.NextGen/VariableType.cs
@@ -1,17 +1,18 @@
 ﻿namespace CBreeze.NextGen
 {
-    public enum VariableType
-    {
-        Action,
-        Automation,
-        BigInteger,
-        BigText,
-        Binary,
-        Boolean,
-        Byte,
-        Char,
-        Codeunit,
-        Integer,
-        Record,
-    }
+	public enum VariableType
+	{
+		Action,
+		Automation,
+		BigInteger,
+		BigText,
+		Binary,
+		Boolean,
+		Byte,
+		Char,
+		Codeunit,
+		Integer,
+		Record,
+		TextConstant
+	}
 }
