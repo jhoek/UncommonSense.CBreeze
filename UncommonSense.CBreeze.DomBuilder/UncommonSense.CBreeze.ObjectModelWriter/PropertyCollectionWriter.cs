dfbf5faeb499c767df3d993a61501895d5b93a8f
Jan Hoek <github@uncommonsense.nl>
2015-07-05 12:23:27 +0200

    Worked on writing property collections

--- UncommonSense.CBreeze.DomBuilder/UncommonSense.CBreeze.ObjectModelWriter/PropertyCollectionWriter.cs
+++ UncommonSense.CBreeze.DomBuilder/UncommonSense.CBreeze.ObjectModelWriter/PropertyCollectionWriter.cs
@@ -19,6 +19,14 @@ namespace UncommonSense.CBreeze.ObjectModelWriter
 				var expression = string.Format("new {0}(\"{1}\")", property.TypeName, property.Name);
 				var field = new Field(Visibility.Protected, property.InternalName, property.TypeName, expression);
 				@class.Fields.Add(field);
+
+				// Find property type name
+				var propertyType = propertyCollection.ObjectModel.Elements.OfType<PropertyType>().First(p => p.Name == property.TypeName);
+
+				var getter = new PropertyAccessor(AccessorVisibility.Unspecified, null); // FIXME
+				var setter = new PropertyAccessor(AccessorVisibility.Unspecified, null); // not required for reference property types! FIXME: codeblock
+				var property2 = new UncommonSense.CSharp.Property(Visibility.Public, property.Name, propertyType.InnerTypeName, getter, setter);
+				@class.Properties.Add(property2);
 			}
 
 			new CompilationUnit(new Namespace(propertyCollection.ObjectModel.Namespace, @class)).WriteTo(Path.Combine(folderName, string.Format("{0}.cs", propertyCollection.Name)));
