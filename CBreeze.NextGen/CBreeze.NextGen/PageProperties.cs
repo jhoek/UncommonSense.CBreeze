2c77c07e445efe2e16faa18b9215b0b5f56c503c
Jan Hoek <github@uncommonsense.nl>
2015-07-05 10:23:38 +0200

    Added more page properties

--- CBreeze.NextGen/CBreeze.NextGen/PageProperties.cs
+++ CBreeze.NextGen/CBreeze.NextGen/PageProperties.cs
@@ -4,21 +4,15 @@ namespace CBreeze.NextGen
 {
 	public class PageProperties : Properties
 	{
-		private MultiLanguageProperty captionML = new MultiLanguageProperty("CaptionML");
 		private ActionListProperty actionList = new ActionListProperty("ActionList");
+		private NullableValueProperty<bool> autoSplitKey = new NullableValueProperty<bool>("AutoSplitKey");
+		private MultiLanguageProperty captionML = new MultiLanguageProperty("CaptionML");
+		private StringProperty cardPageID = new StringProperty("CardPageID");
 
 		internal PageProperties(Node parentNode)
 			: base(parentNode)
 		{
 			actionList.ParentNode = ParentNode;
-		}
-
-		public MultiLanguageValue CaptionML
-		{
-			get
-			{
-				return this.captionML.Value;
-			}
 		}
 
 		public ActionList ActionList
@@ -29,11 +23,45 @@ namespace CBreeze.NextGen
 			}
 		}
 
+		public bool? AutoSplitKey
+		{
+			get
+			{
+				return this.autoSplitKey.Value;
+			}
+			set
+			{
+				this.autoSplitKey.Value = value;
+			}
+		}
+
+		public MultiLanguageValue CaptionML
+		{
+			get
+			{
+				return this.captionML.Value;
+			}
+		}
+
+		public string CardPageID
+		{
+			get
+			{
+				return this.cardPageID.Value;
+			}
+			set
+			{
+				this.cardPageID.Value = value;
+			}
+		}
+
 		public override System.Collections.Generic.IEnumerable<INode> ChildNodes
 		{
 			get
 			{
+				// FIXME: Verify order
 				yield return captionML;
+				yield return autoSplitKey;
 				yield return actionList;
 			}
 		}
