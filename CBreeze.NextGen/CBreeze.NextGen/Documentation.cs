8ff821b41eb6af1f01f1074b2d7e4f6ee2ac925a
Jan Hoek <github@uncommonsense.nl>
2015-07-06 15:35:30 +0200

    Added Documentation and Events

---
+++ CBreeze.NextGen/CBreeze.NextGen/Documentation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CBreeze.NextGen
+{
+	public class Documentation :Node
+	{
+		public Documentation()
+		{
+			Lines = new List<string>();
+		}
+
+		public List<string> Lines
+		{
+			get;
+			internal set;
+		}
+	}
+}
+
---
+++ CBreeze.NextGen/CBreeze.NextGen/Events.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CBreeze.NextGen
+{
+	public class Events : KeyedContainer<
+	{
+		public Events()
+		{
+		}
+	}
+}
+
