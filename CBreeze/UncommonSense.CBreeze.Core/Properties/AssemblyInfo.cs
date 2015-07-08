7431f9cd27b18ad1b467703f114ff90dc8d6e555
Jan Hoek <github@uncommonsense.nl>
2015-07-08 09:52:38 +0200

    Worked on auto-generating Type properties

--- CBreeze/UncommonSense.CBreeze.Core/Properties/AssemblyInfo.cs
+++ CBreeze/UncommonSense.CBreeze.Core/Properties/AssemblyInfo.cs
@@ -6,5 +6,5 @@ using System.Reflection;
 [assembly: AssemblyCompany("Jan Hoek")]
 [assembly: AssemblyProduct("C/Breeze")]
 [assembly: AssemblyCopyright("Copyright \u00a9 2013 Jan Hoek")]
-[assembly: AssemblyVersion("0.1.10884.0")]
-[assembly: AssemblyFileVersion("0.1.10884.0")]
+[assembly: AssemblyVersion("0.1.10885.0")]
+[assembly: AssemblyFileVersion("0.1.10885.0")]
