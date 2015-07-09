11defd92d597efe7cafff609d9234a2129ad8a7d
Jan Hoek <github@uncommonsense.nl>
2015-07-09 16:50:13 +0200

    Changed demo code

--- CBreeze/UncommonSense.CBreeze.Model.Demo/Program.cs
+++ CBreeze/UncommonSense.CBreeze.Model.Demo/Program.cs
@@ -15,11 +15,13 @@ namespace UncommonSense.CBreeze.Model.Demo
             var applicationDesign = new ApplicationDesign();
             var setup = applicationDesign.Add(new SetupEntityType("Demo Setup"));
             var item = applicationDesign.Add(new MasterEntityType("Item", setup));
-            var vendor = applicationDesign.Add(new MasterEntityType("Vendor", setup));
+            //var vendor = applicationDesign.Add(new MasterEntityType("Vendor", setup));
 
-            var subsidiaryEntityType = applicationDesign.Add(new SubsidiaryEntityType("Item Vendor", "Item Vendors", item, vendor));
-            subsidiaryEntityType.DifferentiatorType = DifferentiatorType.None;
+            //var subsidiaryEntityType = applicationDesign.Add(new SubsidiaryEntityType("Item Vendor", "Item Vendors", item, vendor));
+            //subsidiaryEntityType.DifferentiatorType = DifferentiatorType.None;
 
+            var ledgerEntityType = applicationDesign.Add(new LedgerEntityType("Item Ledger Entry", "Item Ledger Entries"));
+            ledgerEntityType.MasterEntityType = item;
 
             var application = applicationDesign.RenderTo(new Application(), new RenderingContext());
             application.Write(Console.Out);
