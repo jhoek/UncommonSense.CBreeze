0a73147468ea0934390a4cc455f334f0fdeed265
Jan Hoek <github@uncommonsense.nl>
2015-08-07 12:59:37 +0200

    Added CmdletExtensionMethods class

---
+++ CBreeze/UncommonSense.CBreeze.Automation/CmdletExtensionMethods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+
+namespace UncommonSense.CBreeze.Automation
+{
+    public static class CmdletExtensionMethods
+    {
+        public static void WriteObjectIf(this Cmdlet cmdlet, bool condition, Object @object)
+        {
+            if (condition)
+            {
+                cmdlet.WriteObject(@object);
+            }
+        }
+    }
+}
