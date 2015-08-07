3028ac2474c4b9dbc7969832176e19a0c412baac
Jan Hoek <github@uncommonsense.nl>
2015-08-07 19:42:40 +0200

    Renamed CmdletExtensionMethods to ExtensionMethods

---
+++ CBreeze/UncommonSense.CBreeze.Automation/ExtensionMethods.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using UncommonSense.CBreeze.Core;
+
+namespace UncommonSense.CBreeze.Automation
+{
+    public static class ExtensionMethods
+    {
+        public static void WriteObjectIf(this Cmdlet cmdlet, bool condition, System.Object @object)
+        {
+            if (condition)
+            {
+                cmdlet.WriteObject(@object);
+            }
+        }
+
+        public static void AutoCaption(this Table table)
+        {
+            table.Properties.CaptionML.Add("ENU", table.Name);
+        }
+    }
+}
