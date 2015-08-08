2ea5b66e1d18c2e7da9421efb43229b257a2ab95
Jan Hoek <github@uncommonsense.nl>
2015-08-08 17:03:18 +0200

    Added abstract base class AddCmdlet

---
+++ CBreeze/UncommonSense.CBreeze.Automation/AddCmdlet.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+
+namespace UncommonSense.CBreeze.Automation
+{
+    public abstract class AddCmdlet : Cmdlet
+    {
+        [Parameter()]
+        public SwitchParameter PassThru
+        {
+            get;
+            set;
+        }
+
+        protected override void ProcessRecord()
+        {
+            foreach (var addedObject in AddedObjects)
+            {
+                if (PassThru)
+                {
+                    WriteObject(addedObject);
+                }
+            }
+        }
+
+        protected abstract IEnumerable AddedObjects
+        {
+            get;
+        }
+    }
+}
