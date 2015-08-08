7b459509d15e6ab3b75ee1d18069dcbd8fe6cc18
Jan Hoek <github@uncommonsense.nl>
2015-08-08 17:16:22 +0200

    Removed WriteObjectIf method (now implemented by AddCmdlet)

--- CBreeze/UncommonSense.CBreeze.Automation/ExtensionMethods.cs
+++ CBreeze/UncommonSense.CBreeze.Automation/ExtensionMethods.cs
@@ -9,19 +9,27 @@ namespace UncommonSense.CBreeze.Automation
 {
     public static class ExtensionMethods
     {
-        public static void WriteObjectIf(this Cmdlet cmdlet, bool condition, System.Object @object)
-        {
-            if (condition)
-            {
-                cmdlet.WriteObject(@object);
-            }
-        }
-
         public static void AutoCaptionIf(this Table table, bool condition)
         {
             if (condition)
             {
                 table.Properties.CaptionML.Add("ENU", table.Name);
+            }
+        }
+
+        public static void AutoCaptionIf(this Page page, bool condition)
+        {
+            if (condition)
+            {
+                page.Properties.CaptionML.Add("ENU", page.Name);
+            }
+        }
+
+        public static void AutoCaptionIf(this XmlPort xmlPort, bool condition)
+        {
+            if (condition)
+            {
+                xmlPort.Properties.CaptionML.Add("ENU", xmlPort.Name);
             }
         }
     }
