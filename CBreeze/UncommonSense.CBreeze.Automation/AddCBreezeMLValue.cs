86582c090e2f3e37adfd86bfbcd7dbd77dddf258
Jan Hoek <github@uncommonsense.nl>
2015-08-08 17:05:43 +0200

    Added AddCBreezeMLValue cmdlet

---
+++ CBreeze/UncommonSense.CBreeze.Automation/AddCBreezeMLValue.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using UncommonSense.CBreeze.Core;
+
+namespace UncommonSense.CBreeze.Automation
+{
+    [Cmdlet(VerbsCommon.Add, "CBreezeMLValue")]
+    public class AddCBreezeMLValue : Cmdlet
+    {
+        [Parameter(Mandatory=true,ValueFromPipeline=true)]
+        public dynamic InputObject
+        {
+            get;
+            set;
+        }
+
+        [Parameter(Mandatory = true, Position = 0)]
+        [ValidateSet("CaptionML")] // FIXME
+        public string Property
+        {
+            get;
+            set;
+        }
+
+        [Parameter(Mandatory=true,Position=1)]
+        public string LanguageName
+        {
+            get;
+            set;
+        }
+
+        [Parameter(Mandatory=true,Position=2)]
+        public string Value
+        {
+            get;
+            set;
+        }
+
+        protected override void ProcessRecord()
+        {
+            InputObject.Properties[Property].Value.Add(LanguageName, Value);
+        }
+    }
+}
