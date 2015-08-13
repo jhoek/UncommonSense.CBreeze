075f4d0104f3caa470704dc8e1aab6b8ee0052b5
Jan Hoek <github@uncommonsense.nl>
2015-08-13 17:28:50 +0200

    Remove tab50000.ps1 demo script

--- CBreeze/UncommonSense.CBreeze.Automation/tab50000.ps1
+++
@@ -1,6 +0,0 @@
-﻿$Application = New-CBreezeApplication
-
-$Table = $Application | Add-CBreezeTable 50000 'My Test Table' -VersionList NAVJH1.00 -DateTime (Get-Date) -AutoCaption
-$Table | Add-CBreezeCodeTableField 'Primary Key' -AutoCaption
-
-$Application | Export-CBreezeApplication -ServerName '.\NAVDEMO' -Database 'Demo Database NAV (7-0)'
