1a4b0ccf3b8838d85c3390ce69838636af1d0891
Jan Hoek <github@uncommonsense.nl>
2015-08-13 17:26:21 +0200

    Added more properties

--- CBreeze/UncommonSense.CBreeze.Automation/AddCBreezeCodeTableField.cs
+++ CBreeze/UncommonSense.CBreeze.Automation/AddCBreezeCodeTableField.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Management.Automation;
 using System.Text;
+using UncommonSense.CBreeze.Core;
 
 namespace UncommonSense.CBreeze.Automation
 {
@@ -23,6 +24,55 @@ namespace UncommonSense.CBreeze.Automation
         }
 
         [Parameter()]
+        public string AltSearchField
+        {
+            get;
+            set;
+        }
+
+        [Parameter()]
+        public string AutoFormatExpr
+        {
+            get;
+            set;
+        }
+
+        [Parameter()]
+        public AutoFormatType? AutoFormatType
+        {
+            get;
+            set;
+        }
+
+        [Parameter()]
+        public string CalcFormulaFieldName
+        {
+            get;
+            set;
+        }
+
+        [Parameter()]
+        public CalcFormulaMethod? CalcFormulaMethod
+        {
+            get;
+            set;
+        }
+
+        [Parameter()]
+        public SwitchParameter CalcFormulaReverseSign
+        {
+            get;
+            set;
+        }
+
+        [Parameter()]
+        public string CalcFormulaTableName
+        {
+            get;
+            set;
+        }
+
+        [Parameter()]
         public bool? NotBlank
         {
             get;
@@ -36,6 +86,15 @@ namespace UncommonSense.CBreeze.Automation
                 No = AutoAssignNo(No);
 
                 var field = Table.Fields.AddCodeTableField(No, Name, DataLength);
+                field.Properties.AltSearchField = AltSearchField;
+                field.Properties.AutoFormatExpr = AutoFormatExpr;
+                field.Properties.AutoFormatType = AutoFormatType;
+                field.Properties.CalcFormula.FieldName = CalcFormulaFieldName;
+                field.Properties.CalcFormula.Method = CalcFormulaMethod;
+                field.Properties.CalcFormula.ReverseSign = CalcFormulaReverseSign;
+                field.Properties.CalcFormula.TableName = CalcFormulaTableName;
+                
+
                 field.Properties.NotBlank = NotBlank;
 
                 field.AutoCaptionIf(AutoCaption);
