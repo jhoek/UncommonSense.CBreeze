e853700739a9441e848c31bbb64e7856e5759632
Jan Hoek <github@uncommonsense.nl>
2015-08-13 13:05:06 +0200

    Simplified demo script

--- CBreeze/UncommonSense.CBreeze.Automation/tab50000.ps1
+++ CBreeze/UncommonSense.CBreeze.Automation/tab50000.ps1
@@ -1,6 +1,6 @@
 ﻿$Application = New-CBreezeApplication
 
-$Table = $Application | Add-CBreezeTable -ID 50000 -Name 'My Test Table' -VersionList NAVJH1.00 -AutoCaption -PassThru
-$Table | Add-CBreezeCodeTableField -Name 'Primary Key' -DataLength 20 -NotBlank $true -AutoCaption
+$Table = $Application | Add-CBreezeTable 50000 'My Test Table' -VersionList NAVJH1.00 -DateTime (Get-Date) -AutoCaption
+$Table | Add-CBreezeCodeTableField 'Primary Key' -AutoCaption
 
 $Application | Export-CBreezeApplication -ServerName '.\NAVDEMO' -Database 'Demo Database NAV (7-0)'
