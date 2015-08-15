77e6028c33803c8b2e8d16fc31e1b6d019ab25ae
Jan Hoek <github@uncommonsense.nl>
2015-08-15 12:45:50 +0200

    Added more parameter types

---
+++ CBreeze/UncommonSense.CBreeze.Automation/AddCBreezeDecimalParameter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+
+namespace UncommonSense.CBreeze.Automation
+{
+    [Cmdlet(VerbsCommon.Add, "CBreezeDecimalParameter")]
+    public class AddCBreezeDecimalParameter : AddCBreezeParameter
+    {
+        protected override System.Collections.IEnumerable AddedObjects
+        {
+            get
+            {
+                ID = AutoAssignID(ID);
+
+                var decimalParameter = Parameters.AddDecimalParameter(Var, ID, Name);
+                decimalParameter.Dimensions = Dimensions;
+                yield return decimalParameter;
+            }
+        }
+    }
+}
---
+++ CBreeze/UncommonSense.CBreeze.Automation/AddCBreezeDialogParameter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+
+namespace UncommonSense.CBreeze.Automation
+{
+    [Cmdlet(VerbsCommon.Add, "CBreezeDialogParameter")]
+    public class AddCBreezeDialogParameter : AddCBreezeParameter
+    {
+        protected override System.Collections.IEnumerable AddedObjects
+        {
+            get
+            {
+                ID = AutoAssignID(ID);
+
+                var dialogParameter = Parameters.AddDialogParameter(Var, ID, Name);
+                dialogParameter.Dimensions = Dimensions;
+                yield return dialogParameter;
+            }
+        }
+    }
+}
