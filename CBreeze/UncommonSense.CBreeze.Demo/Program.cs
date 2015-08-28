f59f246b0918482fbac3efa16a017a2d8fdc6cd0
Jan Hoek <github@uncommonsense.nl>
2015-08-28 12:51:15 +0200

    Simplified demo project to allow it to run on the Mac.

--- CBreeze/UncommonSense.CBreeze.Demo/Program.cs
+++ CBreeze/UncommonSense.CBreeze.Demo/Program.cs
@@ -3,28 +3,34 @@ using System.Diagnostics;
 using UncommonSense.CBreeze.Core;
 using UncommonSense.CBreeze.Write;
 using UncommonSense.CBreeze.Read;
+using System.IO;
 
 namespace UncommonSense.CBreeze.Demo
 {
-    class MainClass
-    {
-        public static void Main(string[] args)
-        {
-            var commandLine = new CommandLine(args);
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			var programsFolderName = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+			var compareToolFileName = Path.Combine(programsFolderName, "Araxis Merge.app");
 
-            var parserStopwatch = new Stopwatch();
-            parserStopwatch.Start();
-            var application = ApplicationBuilder.FromFile(commandLine.InputFileName);
-            parserStopwatch.Stop();
-            Console.WriteLine(parserStopwatch.Elapsed);
+			var desktopFolderName = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+			var inputFileName = Path.Combine(desktopFolderName, "nl2013.txt");
+			var outputFileName = Path.Combine(desktopFolderName, "nl2013.output.txt");
 
-            var writerStopwatch = new Stopwatch();
-            writerStopwatch.Start();
-            application.Write(commandLine.OutputFileName);
-            writerStopwatch.Stop();
-            Console.WriteLine(writerStopwatch.Elapsed);
+			var parserStopwatch = new Stopwatch();
+			parserStopwatch.Start();
+			var application = ApplicationBuilder.FromFile(inputFileName);
+			parserStopwatch.Stop();
+			Console.WriteLine(parserStopwatch.Elapsed);
 
-            Process.Start(commandLine.CompareToolFileName, string.Format("\"{0}\" \"{1}\"", commandLine.InputFileName, commandLine.OutputFileName));
-        }
-    }
+			var writerStopwatch = new Stopwatch();
+			writerStopwatch.Start();
+			application.Write(outputFileName);
+			writerStopwatch.Stop();
+			Console.WriteLine(writerStopwatch.Elapsed);
+
+			Process.Start(compareToolFileName, string.Format("\"{0}\" \"{1}\"", inputFileName, outputFileName));
+		}
+	}
 }
