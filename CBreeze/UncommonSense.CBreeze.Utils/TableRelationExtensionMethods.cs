e2e115c46bf4e63c3ec7cb4d9e121f3c79d8d82c
Jan Hoek <github@uncommonsense.nl>
2015-08-28 13:51:15 +0200

    Preparations for more utility extension methods

---
+++ CBreeze/UncommonSense.CBreeze.Utils/TableRelationExtensionMethods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UncommonSense.CBreeze.Core;
+
+namespace UncommonSense.CBreeze.Utils
+{
+    public static class TableRelationExtensionMethods
+    {
+        public static TableRelationLine Set(this TableRelationLines tableRelation, string tableName , string fieldName = null)
+        {
+            var tableRelationLine = tableRelation.Add(tableName);
+            tableRelationLine.FieldName = fieldName;
+
+            return tableRelationLine;
+        }
+    }
+}
