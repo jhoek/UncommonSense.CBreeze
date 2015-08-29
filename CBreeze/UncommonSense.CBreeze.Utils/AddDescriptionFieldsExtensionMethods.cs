0534ef718674452a617c01c9483028e3dfd4b024
Jan Hoek <github@uncommonsense.nl>
2015-08-29 10:21:12 +0200

    Added AddDescriptionFieldsExtensionMethods

---
+++ CBreeze/UncommonSense.CBreeze.Utils/AddDescriptionFieldsExtensionMethods.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UncommonSense.CBreeze.Core;
+
+namespace UncommonSense.CBreeze.Utils
+{
+    public enum DescriptionStyle
+    {
+        Name,
+        Description
+    }
+
+    public static class AddDescriptionFieldsExtensionMethods
+    {
+        public static AddDescriptionFieldsManifest AddDescriptionFields(this Table table, DescriptionStyle descriptionStyle = DescriptionStyle.Description,bool description2 = true, bool searchDescription = true, string prefix = null, int fieldNoOffset = 0)
+        {
+            var manifest = new AddDescriptionFieldsManifest();
+
+            fieldNoOffset = fieldNoOffset == 0?table.Fields.Max(f=> f.ID) + 1 : fieldNoOffset;
+            
+            manifest.DescriptionField = table.Fields.Add(new TextTableField(fieldNoOffset++, string.Format("{0}{1}", prefix, descriptionStyle), 50)).AutoCaption();
+
+            if (description2)
+            {
+                manifest.Description2Field = table.Fields.Add(new TextTableField(fieldNoOffset++, string.Format("{0}{1} 2", prefix, descriptionStyle), 50)).AutoCaption();
+            }
+
+            if (searchDescription)
+            {
+                manifest.SearchDescriptionField = table.Fields.Add(new CodeTableField(fieldNoOffset++, string.Format("{0}Search {1}", prefix, descriptionStyle), 50)).AutoCaption();
+                manifest.DescriptionField.Properties.OnValidate.CodeLines.Add("IF ({0} = UPPERCASE(xRec.{1})) OR ({0} = '') THEN", manifest.SearchDescriptionField.Name.QuotedVariableName(), manifest.DescriptionField.Name.QuotedVariableName());
+                manifest.DescriptionField.Properties.OnValidate.CodeLines.Add("  {0} := {1};", manifest.SearchDescriptionField.Name.QuotedVariableName(), manifest.DescriptionField.Name.QuotedVariableName());
+            }
+
+            return manifest;
+        }
+    }
+}
---
+++ CBreeze/UncommonSense.CBreeze.Utils/AddDescriptionFieldsManifest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UncommonSense.CBreeze.Core;
+
+namespace UncommonSense.CBreeze.Utils
+{
+    public class AddDescriptionFieldsManifest
+    {
+        internal AddDescriptionFieldsManifest()
+        {
+        }
+
+        public TextTableField DescriptionField
+        {
+            get;
+            internal set;
+        }
+
+        public TextTableField Description2Field
+        {
+            get;
+            internal set;
+        }
+
+        public CodeTableField SearchDescriptionField
+        {
+            get;
+            internal set;
+        }
+    }
+}
