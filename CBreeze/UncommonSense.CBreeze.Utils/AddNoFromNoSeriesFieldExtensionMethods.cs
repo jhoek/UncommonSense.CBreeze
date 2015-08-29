38e0e6cbd5f0928c6480388237c256179e764a72
Jan Hoek <github@uncommonsense.nl>
2015-08-29 20:58:36 +0200

    Preparations for AddNoFromNoSeriesFieldExtensionMethods

---
+++ CBreeze/UncommonSense.CBreeze.Utils/AddNoFromNoSeriesFieldExtensionMethods.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UncommonSense.CBreeze.Core;
+
+namespace UncommonSense.CBreeze.Utils
+{
+    public static class AddNoFromNoSeriesFieldExtensionMethods
+    {
+        public static AddNoFromNoSeriesFieldManifest AddNoFromNoSeriesField(this Table table, Table setupTable, Page setupPage, params Page[] pages)
+        {
+            var manifest = new AddNoFromNoSeriesFieldManifest();
+
+            manifest.NoField = table.Fields.Add(new CodeTableField(table.NextFieldNo(), "No.", 20)).AutoCaption();
+
+
+            manifest.NoSeriesField = table.Fields.Add(new CodeTableField(table.NextFieldNo(100), "No. Series", 10)).AutoCaption(); // FIXME: No.
+            manifest.NoSeriesField.Properties.Editable = false;
+            manifest.NoSeriesField.Properties.TableRelation.Add("No. Series");
+
+            var primaryKey = table.Keys.Add();
+            primaryKey.Fields.Add(manifest.NoField.Name);
+            primaryKey.Properties.Clustered = true;
+
+            return manifest;
+        }
+    }
+}
---
+++ CBreeze/UncommonSense.CBreeze.Utils/AddNoFromNoSeriesFieldManifest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UncommonSense.CBreeze.Core;
+
+namespace UncommonSense.CBreeze.Utils
+{
+    public class AddNoFromNoSeriesFieldManifest
+    {
+        internal AddNoFromNoSeriesFieldManifest()
+        {
+        }
+
+        public CodeTableField NoField
+        {
+            get;
+            internal set;
+        }
+
+        public CodeTableField NoSeriesField
+        {
+            get;
+            internal set;
+        }
+    }
+}
