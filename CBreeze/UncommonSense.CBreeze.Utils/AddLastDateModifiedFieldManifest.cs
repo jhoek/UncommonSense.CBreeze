651a82594701452bf8556adbdc942992e14b071c
Jan Hoek <github@uncommonsense.nl>
2015-08-29 17:37:44 +0200

    Added AddLastDateModifiedFieldExtensionMethods

---
+++ CBreeze/UncommonSense.CBreeze.Utils/AddLastDateModifiedFieldManifest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UncommonSense.CBreeze.Core;
+
+namespace UncommonSense.CBreeze.Utils
+{
+    public class AddLastDateModifiedFieldManifest
+    {
+        internal AddLastDateModifiedFieldManifest()
+        {
+        }
+
+        public DateTableField LastDateModifiedField
+        {
+            get;
+            internal set;
+        }
+    }
+}
