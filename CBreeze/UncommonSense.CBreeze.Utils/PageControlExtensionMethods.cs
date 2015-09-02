2e9b0c5907dba9047f554195065742046e140374
Jan Hoek <github@uncommonsense.nl>
2015-09-02 11:01:06 +0200

    Added extension method for finding child controls on pages

---
+++ CBreeze/UncommonSense.CBreeze.Utils/PageControlExtensionMethods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UncommonSense.CBreeze.Core;
+
+namespace UncommonSense.CBreeze.Utils
+{
+    public static class PageControlExtensionMethods
+    {
+        public static IEnumerable<PageControl> ChildPageControls(this PageControl pageControl, Page page)
+        {
+            return 
+                page.Controls.
+                Skip(page.Controls.IndexOf(pageControl) + 1).
+                TakeWhile(c => c.IndentationLevel > pageControl.IndentationLevel).
+                Where(c => c.IndentationLevel == pageControl.IndentationLevel + 1);
+        }
+    }
+}
