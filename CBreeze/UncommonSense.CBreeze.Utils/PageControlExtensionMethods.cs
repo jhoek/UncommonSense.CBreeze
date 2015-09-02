d43ffb33fe88724e50310b5160d4d95d29eedba4
Jan Hoek <github@uncommonsense.nl>
2015-09-02 16:36:34 +0200

    Logic for creating standard groups/containers on card pages

--- CBreeze/UncommonSense.CBreeze.Utils/PageControlExtensionMethods.cs
+++ CBreeze/UncommonSense.CBreeze.Utils/PageControlExtensionMethods.cs
@@ -8,13 +8,73 @@ namespace UncommonSense.CBreeze.Utils
 {
     public static class PageControlExtensionMethods
     {
-        public static IEnumerable<PageControl> ChildPageControls(this PageControl pageControl, Page page)
+        public static IEnumerable<PageControl> GetChildPageControls(this PageControl pageControl, Page page)
         {
-            return 
+            return
                 page.Controls.
                 Skip(page.Controls.IndexOf(pageControl) + 1).
                 TakeWhile(c => c.IndentationLevel > pageControl.IndentationLevel).
                 Where(c => c.IndentationLevel == pageControl.IndentationLevel + 1);
+        }
+
+        public static ContainerPageControl GetContentAreaControl(this Page page)
+        {
+            return page.Controls.OfType<ContainerPageControl>().FirstOrDefault(c => c.Properties.ContainerType == ContainerType.ContentArea);
+        }
+
+        public static ContainerPageControl GetOrCreateContentAreaControl(this Page page, IEnumerable<int> range, int index )
+        {
+            var result = GetContentAreaControl(page) ?? page.Controls.Insert(index, new ContainerPageControl(range.GetNextPageControlID(page), 0));
+            result.Properties.ContainerType = ContainerType.ContentArea;
+            return result;
+        }
+
+        public static GroupPageControl GetGeneralGroupControl(this Page page)
+        {
+            var contentArea = GetContentAreaControl(page);
+
+            if (contentArea == null)
+                return null;
+
+            return contentArea.GetChildPageControls(page).OfType<GroupPageControl>().FirstOrDefault(c => c.Properties.CaptionML["ENU"] == "General");
+        }
+
+        public static GroupPageControl GetOrCreateGeneralGroupControl(this Page page,IEnumerable<int> range, int index )
+        {
+            var groupPageControl = GetGeneralGroupControl(page);
+
+            if (groupPageControl == null)
+            {
+                groupPageControl = page.Controls.Insert(index, new GroupPageControl(range.GetNextPageControlID(page), 1));
+                groupPageControl.Properties.CaptionML.Set("ENU", "General");
+                GetOrCreateContentAreaControl(page, range, index);
+            }
+
+            return groupPageControl;
+        }
+
+        public static GroupPageControl GetNumeringGroupControl(this Page page)
+        {
+            var contentArea = GetContentAreaControl(page);
+
+            if (contentArea == null)
+                return null;
+
+            return contentArea.GetChildPageControls(page).OfType<GroupPageControl>().FirstOrDefault(c => c.Properties.CaptionML["ENU"] == "Numbering");
+        }
+
+        public static GroupPageControl GetOrCreateNumberingGroupControl(this Page page, IEnumerable<int> range, int index)
+        {
+            var groupPageControl = GetNumeringGroupControl(page);
+
+            if (groupPageControl == null)
+            {
+                groupPageControl = page.Controls.Insert(index, new GroupPageControl(range.GetNextPageControlID(page), 1));
+                groupPageControl.Properties.CaptionML.Set("ENU", "Numbering");
+                GetOrCreateContentAreaControl(page, range, index);
+            }
+
+            return groupPageControl;
         }
     }
 }
