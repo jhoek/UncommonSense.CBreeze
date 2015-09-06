b396f98f4b8a4307102831d731721290fd9b94f5
Jan Hoek <github@uncommonsense.nl>
2015-09-06 18:34:04 +0200

    Preparations for UserID extension methods

---
+++ CBreeze/UncommonSense.CBreeze.Utils/UserIDExtensionMethods.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UncommonSense.CBreeze.Core;
+
+namespace UncommonSense.CBreeze.Utils
+{
+    public class UserIDFieldsManifest
+    {
+        internal UserIDFieldsManifest()
+        {
+        }
+    }
+
+    public class UserIDControlsManifest
+    {
+        internal UserIDControlsManifest()
+        {
+        }
+    }
+
+    public static class UserIDExtensionMethods
+    {
+        public static UserIDFieldsManifest AddUserIDFields(this Table table, IEnumerable<int> range)
+        {
+            var manifest = new UserIDFieldsManifest();
+
+            return manifest;
+        }
+
+        public static UserIDControlsManifest AddUserIDControls(this Page page, UserIDFieldsManifest fieldsManifest, IEnumerable<int> range)
+        {
+            var manifest = new UserIDControlsManifest();
+
+            return manifest;
+        }
+    }
+}
