336248bef9e2003ea8e09d8418ade7bd0c4c4b9b
Jan Hoek <github@uncommonsense.nl>
2015-09-07 12:52:27 +0200

    Finalized User ID extension methods

--- CBreeze/UncommonSense.CBreeze.Utils/UserIDExtensionMethods.cs
+++ CBreeze/UncommonSense.CBreeze.Utils/UserIDExtensionMethods.cs
@@ -11,12 +11,24 @@ namespace UncommonSense.CBreeze.Utils
         internal UserIDFieldsManifest()
         {
         }
+
+        public CodeTableField UserIDField
+        {
+            get;
+            internal set;
+        }
     }
 
     public class UserIDControlsManifest
     {
         internal UserIDControlsManifest()
         {
+        }
+
+        public FieldPageControl UserIDControl
+        {
+            get;
+            internal set;
         }
     }
 
@@ -26,12 +38,33 @@ namespace UncommonSense.CBreeze.Utils
         {
             var manifest = new UserIDFieldsManifest();
 
+            manifest.UserIDField = table.Fields.Add(new CodeTableField(range.GetNextTableFieldNo(table), "User ID", 50).AutoCaption());
+            manifest.UserIDField.Properties.TableRelation.Set(BaseApp.TableNames.User, "User Name");
+            manifest.UserIDField.Properties.TestTableRelation = false;
+            manifest.UserIDField.Properties.ValidateTableRelation = false;
+
+            var userMgtVariable = manifest.UserIDField.Properties.OnValidate.Variables.Add(new CodeunitVariable(range.GetNextVariableID(manifest.UserIDField.Properties.OnValidate), "UserMgt", 418));
+            manifest.UserIDField.Properties.OnValidate.CodeLines.Add("{0}.ValidateUserID({1});", userMgtVariable.Name, manifest.UserIDField.Name.Quoted());
+
+            userMgtVariable = manifest.UserIDField.Properties.OnLookup.Variables.Add(new CodeunitVariable(range.GetNextVariableID(manifest.UserIDField.Properties.OnLookup), "UserMgt", 418));
+            manifest.UserIDField.Properties.OnLookup.CodeLines.Add("{0}.LookupUserID({1});", userMgtVariable.Name, manifest.UserIDField.Name.Quoted());
+
             return manifest;
         }
 
         public static UserIDControlsManifest AddUserIDControls(this Page page, UserIDFieldsManifest fieldsManifest, IEnumerable<int> range)
         {
             var manifest = new UserIDControlsManifest();
+            var contentArea = page.GetContentArea(range);
+
+            switch(page.Properties.PageType)
+            {
+                case PageType.List:
+                    var listGroup = contentArea.GetGroupByType(GroupType.Repeater, range, Position.FirstWithinContainer);
+                    manifest.UserIDControl = listGroup.AddChildPageControl(page.Controls.Add(new FieldPageControl(range.GetNextPageControlID(page), 2)), Position.LastWithinContainer);
+                    manifest.UserIDControl.Properties.SourceExpr = fieldsManifest.UserIDField.Name.Quoted();
+                    break;
+            }
 
             return manifest;
         }
