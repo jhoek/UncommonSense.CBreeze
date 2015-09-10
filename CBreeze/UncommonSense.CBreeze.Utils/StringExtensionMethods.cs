fcf857aa72935d91a0f021c298fdcbf2d815be9a
Jan Hoek <github@uncommonsense.nl>
2015-09-10 14:09:18 +0200

    Give Write project a reference to Utils; move Quoted extension methods to Utils

--- CBreeze/UncommonSense.CBreeze.Utils/StringExtensionMethods.cs
+++ CBreeze/UncommonSense.CBreeze.Utils/StringExtensionMethods.cs
@@ -13,6 +13,22 @@ namespace UncommonSense.CBreeze.Utils
             if (text.All(c => Char.IsLetterOrDigit(c)))
                 return text;
 
+            return ForceQuoted(text);
+        }
+
+        public static string QuotedExcept(this string text, params char[] exceptions)
+        {
+            // In e.g. CalcFormulas, table names containing no other special characters than a hyphen (e.g. "To-do") should not be quoted
+            // In e.g. TableRelations, table names containing no other special characters than a slash (e.g. "Country/Region") should not be quoted
+            // In e.g. TableRelations, field names containing no other special characters than a dot (e.g. "No.") should not be quoted
+            if (text.All(c => char.IsLetterOrDigit(c) || exceptions.Contains(c)))
+                return text;
+
+            return text.ForceQuoted();
+        }
+
+        public static string ForceQuoted(this string text)
+        {
             return string.Format("\"{0}\"", text);
         }
 
--- CBreeze/UncommonSense.CBreeze.Write/StringExtensionMethods.cs
+++ CBreeze/UncommonSense.CBreeze.Write/StringExtensionMethods.cs
@@ -2,27 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UncommonSense.CBreeze.Utils;
 
 namespace UncommonSense.CBreeze.Write
 {
     public static class StringExtensionMethods
     {
-        public static string Quoted(this string text)
-        {
-            // In e.g. CalcFormulas, table names containing no other special characters than a hyphen (e.g. "To-do") should not be quoted
-            // In e.g. TableRelations, table names containing no other special characters than a slash (e.g. "Country/Region") should not be quoted
-            // In e.g. TableRelations, field names containing no other special characters than a dot (e.g. "No.") should not be quoted
-            if (text.All(c => char.IsLetterOrDigit(c) || c == '/' || c == '-' || c == '.'))
-                return text;
-
-            return text.ForceQuoted();
-        }
-
-        public static string ForceQuoted(this string text)
-        {
-            return string.Format("\"{0}\"", text);
-        }
-
         public static string TextConstantValue(this string text, bool isComment, int noOfValues)
         {
             var needsQuotes = (text.Trim() != text) || (text.Contains(';') || text.Contains("=") || text.StartsWith("\""));
@@ -33,7 +18,7 @@ namespace UncommonSense.CBreeze.Write
             switch (needsQuotes)
             {
                 case true:
-                    return string.Format("\"{0}\"", text);
+                    return text.ForceQuoted();
                 default:
                     return text;
             }
