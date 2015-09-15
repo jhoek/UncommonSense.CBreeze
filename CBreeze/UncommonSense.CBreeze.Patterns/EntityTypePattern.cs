a625845c463ac6e8c3b49e178cf2fe6b25ff2c9a
Jan Hoek <github@uncommonsense.nl>
2015-09-15 12:11:10 +0200

    EntityType patterns now derive from an abstract base class

---
+++ CBreeze/UncommonSense.CBreeze.Patterns/EntityTypePattern.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UncommonSense.CBreeze.Core;
+
+namespace UncommonSense.CBreeze.Patterns
+{
+    public abstract class EntityTypePattern : Pattern
+    {
+        public EntityTypePattern(Application application, IEnumerable<int> range)
+        {
+            Application = application;
+            Range = range;
+        }
+
+        protected override void VerifyRequirements()
+        {
+            if (Application == null)
+                throw new ArgumentNullException("Application");
+
+            if (Range == null)
+                throw new ArgumentNullException("Range");
+        }
+
+        public Application Application
+        {
+            get;
+            set;
+        }
+
+        public IEnumerable<int> Range
+        {
+            get;
+            set;
+        }
+    }
+}
