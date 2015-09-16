544a2bc838ada4a9ae736bc8bd9df563af2042a0
Jan Hoek <github@uncommonsense.nl>
2015-09-16 08:15:26 +0200

    UserIDPattern based on UserIDExtensionMethods

---
+++ CBreeze/UncommonSense.CBreeze.Patterns/UserIDPattern.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UncommonSense.CBreeze.Core;
+using UncommonSense.CBreeze.Utils;
+
+namespace UncommonSense.CBreeze.Patterns
+{
+    public class UserIDPattern : AddFieldsPattern
+    {
+        private Dictionary<Page, FieldPageControl> userIDControls = new Dictionary<Page, FieldPageControl>();
+
+        public UserIDPattern(IEnumerable<int> range, Table table, params Page[] pages)
+            : base(range, table, pages)
+        {
+        }
+
+        protected override void MakeChanges()
+        {
+            UserIDField = Table.Fields.Add(new CodeTableField(Range.GetNextTableFieldNo(Table), "User ID", 50).AutoCaption());
+            UserIDField.Properties.TableRelation.Set(BaseApp.TableNames.User, "User Name");
+            UserIDField.Properties.TestTableRelation = false;
+            UserIDField.Properties.ValidateTableRelation = false;
+
+            var userMgt = UserIDField.Properties.OnValidate.Variables.Add(new CodeunitVariable(Range.GetNextVariableID(UserIDField.Properties.OnValidate), "UserMgt", BaseApp.CodeunitIDs.User_Management));
+            UserIDField.Properties.OnValidate.CodeLines.Add("{0}.ValidateUserID({1});", userMgt.Name, UserIDField.Name.Quoted());
+
+            userMgt = UserIDField.Properties.OnLookup.Variables.Add(new CodeunitVariable(Range.GetNextVariableID(UserIDField.Properties.OnLookup), "UserMgt", BaseApp.CodeunitIDs.User_Management));
+            UserIDField.Properties.OnLookup.CodeLines.Add("{0}.LookupUserID({1});", userMgt.Name, UserIDField.Name.Quoted());
+        }
+
+        public CodeTableField UserIDField
+        {
+            get;
+            protected set;
+        }
+
+        public ReadOnlyDictionary<Page, FieldPageControl> UserIDControls
+        {
+            get
+            {
+                return userIDControls.AsReadOnly();
+            }
+        }
+    }
+}
--- CBreeze/UncommonSense.CBreeze.Utils/UserIDExtensionMethods.cs
+++ CBreeze/UncommonSense.CBreeze.Utils/UserIDExtensionMethods.cs
@@ -6,52 +6,8 @@ using UncommonSense.CBreeze.Core;
 
 namespace UncommonSense.CBreeze.Utils
 {
-    public class UserIDFieldsManifest
-    {
-        internal UserIDFieldsManifest()
-        {
-        }
-
-        public CodeTableField UserIDField
-        {
-            get;
-            internal set;
-        }
-    }
-
-    public class UserIDControlsManifest
-    {
-        internal UserIDControlsManifest()
-        {
-        }
-
-        public FieldPageControl UserIDControl
-        {
-            get;
-            internal set;
-        }
-    }
-
     public static class UserIDExtensionMethods
     {
-        public static UserIDFieldsManifest AddUserIDFields(this Table table, IEnumerable<int> range)
-        {
-            var manifest = new UserIDFieldsManifest();
-
-            manifest.UserIDField = table.Fields.Add(new CodeTableField(range.GetNextTableFieldNo(table), "User ID", 50).AutoCaption());
-            manifest.UserIDField.Properties.TableRelation.Set(BaseApp.TableNames.User, "User Name");
-            manifest.UserIDField.Properties.TestTableRelation = false;
-            manifest.UserIDField.Properties.ValidateTableRelation = false;
-
-            var userMgtVariable = manifest.UserIDField.Properties.OnValidate.Variables.Add(new CodeunitVariable(range.GetNextVariableID(manifest.UserIDField.Properties.OnValidate), "UserMgt", 418));
-            manifest.UserIDField.Properties.OnValidate.CodeLines.Add("{0}.ValidateUserID({1});", userMgtVariable.Name, manifest.UserIDField.Name.Quoted());
-
-            userMgtVariable = manifest.UserIDField.Properties.OnLookup.Variables.Add(new CodeunitVariable(range.GetNextVariableID(manifest.UserIDField.Properties.OnLookup), "UserMgt", 418));
-            manifest.UserIDField.Properties.OnLookup.CodeLines.Add("{0}.LookupUserID({1});", userMgtVariable.Name, manifest.UserIDField.Name.Quoted());
-
-            return manifest;
-        }
-
         public static UserIDControlsManifest AddUserIDControls(this Page page, UserIDFieldsManifest fieldsManifest, IEnumerable<int> range)
         {
             var manifest = new UserIDControlsManifest();
