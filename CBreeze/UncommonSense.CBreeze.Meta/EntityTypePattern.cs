952781dfbbc85bd895eb33c17dbfff8b5927a61d
Jan Hoek <github@uncommonsense.nl>
2015-09-16 14:21:51 +0200

    Files in Meta project were inadvertently ignored

---
+++ CBreeze/UncommonSense.CBreeze.Meta/EntityTypePattern.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UncommonSense.CBreeze.Core;
+using UncommonSense.CBreeze.Patterns;
+
+namespace UncommonSense.CBreeze.Meta    
+{
+    public abstract class EntityTypePattern : Pattern
+    {
+        public EntityTypePattern(Application application, IEnumerable<int> range)
+        {
+            Application = application;
+            Range = range;
+        }
+
+        protected override void VerifyRequirements()
+        {
+            if (Application == null)
+                throw new ArgumentNullException("Application");
+
+            if (Range == null)
+                throw new ArgumentNullException("Range");
+        }
+
+        public Application Application
+        {
+            get;
+            set;
+        }
+
+        public IEnumerable<int> Range
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ CBreeze/UncommonSense.CBreeze.Meta/LedgerEntityTypePattern.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UncommonSense.CBreeze.Core;
+using UncommonSense.CBreeze.Patterns;
+using UncommonSense.CBreeze.Utils;
+
+namespace UncommonSense.CBreeze.Meta
+{
+    public class LedgerEntityTypePattern : EntityTypePattern
+    {
+        private string pluralName;
+        private Dictionary<Page, FieldPageControl> entryNoControls = new Dictionary<Page, FieldPageControl>();
+        private Dictionary<Page, FieldPageControl> masterEntityTypeControls = new Dictionary<Page, FieldPageControl>();
+        private Dictionary<Page, FieldPageControl> descriptionControls = new Dictionary<Page, FieldPageControl>();
+
+        public LedgerEntityTypePattern(Application application, IEnumerable<int> range)
+            : base(application, range)
+        {
+        }
+
+        protected override void MakeChanges()
+        {
+            CreateObjects();
+            LinkObjects();
+
+            AddEntryNo();
+            AddMasterEntityTypeReference();
+            AddDescription();
+        }
+
+        protected void CreateObjects()
+        {
+            Table = Application.Tables.Add(new Table(Range.GetNextTableID(Application), Name).AutoCaption());
+            Page = Application.Pages.Add(new Page(Range.GetNextPageID(Application), PluralName).AutoCaption());
+
+            Page.Properties.PageType = PageType.List;
+        }
+
+        protected void LinkObjects()
+        {
+            Table.Properties.DrillDownPageID = Page.ID;
+            Table.Properties.LookupPageID = Page.ID;
+            Page.Properties.SourceTable = Table.ID;
+        }
+
+        protected void AddEntryNo()
+        {
+            var entryNoPattern = new EntryNoPattern(Range, Table, Page);
+            entryNoPattern.Apply();
+
+            EntryNoField = entryNoPattern.EntryNoField;
+            entryNoControls.FromReadOnly(entryNoPattern.EntryNoControls);
+        }
+
+        protected void AddMasterEntityTypeReference()
+        {
+            if (MasterEntityTypeTable != null)
+            {
+                MasterEntityTypeField = Table.Fields.Add(new CodeTableField(Range.GetNextTableFieldNo(Table), string.Format("{0} No.", MasterEntityTypeTable.Name), 20).AutoCaption());
+                MasterEntityTypeField.Properties.TableRelation.Add(MasterEntityTypeTable.Name);
+
+                var contentArea = Page.GetContentArea(Range);
+                var group = contentArea.GetGroupByType(GroupType.Repeater, Range, Position.FirstWithinContainer);
+                masterEntityTypeControls.Add(Page, group.AddFieldPageControl(Range.GetNextPageControlID(Page), Position.LastWithinContainer, MasterEntityTypeField.Name));
+            }
+        }
+
+        protected void AddDescription()
+        {
+            var descriptionPattern = new DescriptionPattern(Range, Table, Page);
+            descriptionPattern.HasDescription2 = false;
+            descriptionPattern.HasSearchDescription = false;
+            descriptionPattern.Apply();
+
+            DescriptionField = descriptionPattern.DescriptionField;
+            descriptionControls.FromReadOnly(descriptionPattern.DescriptionControls);
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public string PluralName
+        {
+            get
+            {
+                return this.pluralName ?? string.Format("{0}s", Name);
+            }
+            set
+            {
+                this.pluralName = value;
+            }
+        }
+
+        public Table MasterEntityTypeTable
+        {
+            get;
+            set;
+        }
+
+        public Table Table
+        {
+            get;
+            protected set;
+        }
+
+        public Page Page
+        {
+            get;
+            protected set;
+        }
+
+        public IntegerTableField EntryNoField
+        {
+            get;
+            protected set;
+        }
+
+        public CodeTableField MasterEntityTypeField
+        {
+            get;
+            protected set;
+        }
+
+        public TextTableField DescriptionField
+        {
+            get;
+            protected set;
+        }
+
+        public ReadOnlyDictionary<Page, FieldPageControl> EntryNoControls
+        {
+            get
+            {
+                return entryNoControls.AsReadOnly();
+            }
+        }
+
+        public ReadOnlyDictionary<Page, FieldPageControl> MasterEntityTypeControls
+        {
+            get
+            {
+                return masterEntityTypeControls.AsReadOnly();
+            }
+        }
+
+        public ReadOnlyDictionary<Page, FieldPageControl> DescriptionControls
+        {
+            get
+            {
+                return descriptionControls.AsReadOnly();
+            }
+        }
+    }
+}
