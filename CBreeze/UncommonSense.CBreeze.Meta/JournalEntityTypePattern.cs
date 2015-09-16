bc061838d820cef80ced51e1d8ba9a35a09a6401
Jan Hoek <github@uncommonsense.nl>
2015-09-16 21:38:54 +0200

    Input properties for JournalEntityTypePattern

--- CBreeze/UncommonSense.CBreeze.Meta/JournalEntityTypePattern.cs
+++ CBreeze/UncommonSense.CBreeze.Meta/JournalEntityTypePattern.cs
@@ -8,10 +8,111 @@ namespace UncommonSense.CBreeze.Meta
 {
     public class JournalEntityTypePattern : EntityTypePattern
     {
+        private string templateName;
+        private string templatePluralName;
+        private string batchName;
+        private string batchPluralName;
+        private string lineName;
+
         public JournalEntityTypePattern(Application application, IEnumerable<int> range, string name)
             : base(application, range, name)
         {
+        }
 
+        public Table MasterEntityTypeTable
+        {
+            get;
+            set;
+        }
+
+        public string TemplateName
+        {
+            get
+            {
+                return templateName ?? string.Format("{0} Template", Name);
+            }
+            set
+            {
+                templateName = value;
+            }
+        }
+
+        public string TemplatePluralName
+        {
+            get
+            {
+                return templatePluralName ?? string.Format("{0}s", TemplateName);
+            }
+            set
+            {
+                templatePluralName = value;
+            }
+        }
+
+        public string BatchName
+        {
+            get
+            {
+                return batchName ?? string.Format("{0} Batch", Name);
+            }
+            set
+            {
+                batchName = value;
+            }
+        }
+
+        public string BatchPluralName
+        {
+            get
+            {
+                return batchPluralName ?? string.Format("{0}es", BatchName);
+            }
+            set
+            {
+                batchPluralName = value;
+            }
+        }
+
+        public string LineName
+        {
+            get
+            {
+                return lineName ?? string.Format("{0} Line", Name);
+            }
+            set
+            {
+                lineName = value;
+            }
+        }
+
+        public bool HasTestReportID
+        {
+            get;
+            set;
+        }
+
+        public bool HasPostingReportID
+        {
+            get;
+            set;
+        }
+
+        public bool HasSourceCode
+        {
+            get;
+            set;
+        }
+
+        public bool HasReasonCode
+        {
+            get;
+            set;
+        }
+
+        public bool HasPostingDate
+        {
+            get;
+            set;
         }
     }
 }
