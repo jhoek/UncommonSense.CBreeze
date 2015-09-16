0e072ee5117ba7665c119fed6bf1da9445f41b4a
Jan Hoek <github@uncommonsense.nl>
2015-09-16 17:24:19 +0200

    Moved DifferentiatorType to Meta project

---
+++ CBreeze/UncommonSense.CBreeze.Meta/DifferentiatorType.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace UncommonSense.CBreeze.Meta
+{
+    public enum DifferentiatorType
+    {
+        None,   // e.g. Item Vendor
+        LineNo, // e.g. ???
+        Code	// e.g. Ship-To Address
+    }
+}
+
