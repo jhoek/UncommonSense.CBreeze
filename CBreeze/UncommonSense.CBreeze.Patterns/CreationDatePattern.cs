0699955e0c8f60ef9dbe8c8c52bde3a7e6b667f6
Jan Hoek <github@uncommonsense.nl>
2015-09-17 14:45:28 +0200

    Introduced CreationDatePattern and SourceCodePattern

---
+++ CBreeze/UncommonSense.CBreeze.Patterns/CreationDatePattern.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UncommonSense.CBreeze.Core;
+using UncommonSense.CBreeze.Utils;
+
+namespace UncommonSense.CBreeze.Patterns
+{
+    public class CreationDatePattern : AddFieldsPattern
+    {
+        public CreationDatePattern(IEnumerable<int> range, Table table, params Page[] pages)
+            : base(range, table, pages)
+        {
+            CreationDateControls = new FieldPageControls();
+        }
+
+        protected override void VerifyRequirements()
+        {
+            base.VerifyRequirements();
+
+            if (!Table.Keys.Any())
+            {
+                throw new ApplicationException("The table doesn't have a primary key yet.");
+            }
+        }
+
+        protected override void MakeChanges()
+        {
+            base.MakeChanges();
+
+            CreateKey();
+        }
+
+        protected override void CreateFields()
+        {
+            CreationDateField = Table.Fields.Add(new DateTableField(Range.GetNextTableFieldNo(Table), "Creation Date").AutoCaption());
+        }
+
+        protected override void CreateListPageControls(Page page)
+        {
+            CreationDateControls.Add(page, CreateListPageControl(page, Position.LastWithinContainer, CreationDateField.Name));
+        }
+
+        protected void CreateKey()
+        {
+            if (CreateKeyOnCreationDate)
+            {
+                KeyOnCreationDate = Table.Keys.Add();
+                KeyOnCreationDate.Fields.Add(CreationDateField.Name);
+            }
+        }
+
+        public bool CreateKeyOnCreationDate
+        {
+            get;
+            set;
+        }
+
+        public DateTableField CreationDateField
+        {
+            get;
+            protected set;
+        }
+
+        public FieldPageControls CreationDateControls
+        {
+            get;protected set;
+        }
+
+        public TableKey KeyOnCreationDate
+        {
+            get;
+            protected set;
+        }
+    }
+}
---
+++ CBreeze/UncommonSense.CBreeze.Patterns/SourceCodePattern.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UncommonSense.CBreeze.Core;
+using UncommonSense.CBreeze.Utils;
+
+namespace UncommonSense.CBreeze.Patterns
+{
+    public class SourceCodePattern : AddFieldsPattern
+    {
+        public SourceCodePattern(IEnumerable<int> range, Table table, params Page[] pages)
+            : base(range, table, pages)
+        {
+            SourceCodeControls = new FieldPageControls();
+        }
+
+        protected override void CreateFields()
+        {
+            SourceCodeField = Table.Fields.Add(new CodeTableField(Range.GetNextTableFieldNo(Table), "Source Code", 10).AutoCaption());
+            SourceCodeField.Properties.TableRelation.Add(BaseApp.TableNames.Source_Code);
+        }
+
+        protected override void CreateListPageControls(Page page)
+        {
+            var contentArea = page.GetContentArea(Range);
+            var group = contentArea.GetGroupByType(GroupType.Repeater, Range, Position.FirstWithinContainer);
+
+            SourceCodeControls.Add(page, group.AddFieldPageControl(Range.GetNextPageControlID(page), Position.LastWithinContainer, SourceCodeField.Name));
+        }
+
+        public CodeTableField SourceCodeField
+        {
+            get;
+            protected set;
+        }
+
+        public FieldPageControls SourceCodeControls
+        {
+            get;
+            protected set;
+        }
+    }
+}
