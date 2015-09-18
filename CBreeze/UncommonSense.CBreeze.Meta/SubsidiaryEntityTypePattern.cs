0ff6e433b31125fd65e9eacae9ca4065c94ec7fe
Jan Hoek <github@uncommonsense.nl>
2015-09-18 15:00:43 +0200

    Replace SubsidiaryEntityTypeRenderer with SubsidiaryEntityTypePattern

--- CBreeze/UncommonSense.CBreeze.Meta/SubsidiaryEntityTypePattern.cs
+++ CBreeze/UncommonSense.CBreeze.Meta/SubsidiaryEntityTypePattern.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UncommonSense.CBreeze.Core;
+using UncommonSense.CBreeze.Patterns;
+using UncommonSense.CBreeze.Utils;
 
 namespace UncommonSense.CBreeze.Meta
 {
@@ -14,6 +16,7 @@ namespace UncommonSense.CBreeze.Meta
         public SubsidiaryEntityTypePattern(Application application, IEnumerable<int> range, string name, params Table[] subsidiaryTo)
             : base(application, range, name)
         {
+            SubsidiaryToFields = new MappedResults<Core.Table, TableField>();
             DifferentiatorType = DifferentiatorType.None;
             this.subsidiaryTo.AddRange(subsidiaryTo);
         }
@@ -22,6 +25,74 @@ namespace UncommonSense.CBreeze.Meta
         {
             if (!SubsidiaryTo.Any())
                 throw new ArgumentOutOfRangeException("SubsidiaryTo");
+
+            foreach (var subsidiaryTo in SubsidiaryTo)
+            {
+                GetSubsidiaryToPrimaryKeyField(subsidiaryTo);
+            }
+        }
+
+        protected override void CreateObjects()
+        {
+            Table = Application.Tables.Add(new Table(Range.GetNextTableID(Application), Name).AutoCaption());
+            Page = Application.Pages.Add(new Page(Range.GetNextPageID(Application), PluralName).AutoCaption());
+        }
+
+        protected override void LinkObjects()
+        {
+            Page.Properties.SourceTable = Table.ID;
+            Table.Properties.LookupPageID = Page.ID;
+            Table.Properties.DrillDownPageID = Page.ID;
+        }
+
+        protected override void CreateFields()
+        {
+            var myPrimaryKey = Table.Keys.Add();
+            myPrimaryKey.Properties.Clustered = true;
+
+            foreach (var subsidiaryTo in SubsidiaryTo)
+            {
+                var theirPrimaryKeyField = GetSubsidiaryToPrimaryKeyField(subsidiaryTo);
+                var myPrimaryKeyField = Table.Fields.Add(new CodeTableField(Range.GetNextPrimaryKeyFieldNo(Table), string.Format("{0} {1}", subsidiaryTo.Name, theirPrimaryKeyField.Name), theirPrimaryKeyField.DataLength).AutoCaption());
+                myPrimaryKeyField.Properties.TableRelation.Add(subsidiaryTo.Name);
+
+                myPrimaryKey.Fields.Add(myPrimaryKeyField.Name);
+                SubsidiaryToFields.Add(subsidiaryTo, myPrimaryKeyField);               
+                // FIXME: add to page
+            }
+
+            switch (DifferentiatorType)
+            {
+                case Meta.DifferentiatorType.Code:
+                    CodeField = Table.Fields.Add(new CodeTableField(Range.GetNextPrimaryKeyFieldNo(Table), "Code", 10).AutoCaption());
+                    myPrimaryKey.Fields.Add(CodeField.Name);
+                    // FIXME: Add to page?
+                    break;
+                case Meta.DifferentiatorType.LineNo:
+                    LineNoField = Table.Fields.Add(new IntegerTableField(Range.GetNextPrimaryKeyFieldNo(Table), "Line No.").AutoCaption());
+                    myPrimaryKey.Fields.Add(LineNoField.Name);
+                    // FIXME: Add to page?
+                    break;
+            }
+        }
+
+        protected CodeTableField GetSubsidiaryToPrimaryKeyField(Table subsidiaryTo)
+        {
+            var primaryKey = subsidiaryTo.Keys.First();
+
+            if (primaryKey.Fields.Count() != 1)
+                throw new ArgumentOutOfRangeException("Primary key must consist of exactly one field of type Code with length 10 or 20.");
+
+            var primaryKeyFieldName = primaryKey.Fields.First().FieldName;
+            var primaryKeyField = subsidiaryTo.Fields[primaryKeyFieldName] as CodeTableField;
+
+            if (primaryKeyField == null)
+                throw new ArgumentOutOfRangeException("Primary key must consist of exactly one field of type Code with length 10 or 20.");
+
+            if (primaryKeyField.DataLength != 10 && primaryKeyField.DataLength != 20)
+                throw new ArgumentOutOfRangeException("Primary key must consist of exactly one field of type Code with length 10 or 20.");
+
+            return primaryKeyField;
         }
 
         public string PluralName
@@ -41,13 +112,43 @@ namespace UncommonSense.CBreeze.Meta
             get;
             set;
         }
-        
+
         public IEnumerable<Table> SubsidiaryTo
         {
             get
             {
                 return subsidiaryTo.AsEnumerable();
             }
+        }
+
+        public Table Table
+        {
+            get;
+            protected set;
+        }
+
+        public Page Page
+        {
+            get;
+            protected set;
+        }
+
+        public MappedResults<Table, TableField> SubsidiaryToFields
+        {
+            get;
+            protected set;
+        }
+
+        public IntegerTableField LineNoField
+        {
+            get;
+            protected set;
+        }
+
+        public CodeTableField CodeField
+        {
+            get;
+            protected set;
         }
     }
 }
