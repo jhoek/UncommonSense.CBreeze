d02e8a888a4136cb9c47ae44e86a01c582474d60
Jan Hoek <github@uncommonsense.nl>
2015-09-18 10:34:17 +0200

    Added HasStatisticsPage for DocumentEntityTypePattern

--- CBreeze/UncommonSense.CBreeze.Meta/DocumentEntityTypePattern.Inputs.cs
+++ CBreeze/UncommonSense.CBreeze.Meta/DocumentEntityTypePattern.Inputs.cs
@@ -17,6 +17,12 @@ namespace UncommonSense.CBreeze.Meta
             set;
         }
 
+        public bool HasStatisticsPage
+        {
+            get;
+            set;
+        }
+
         public Table SetupTable
         {
             get;
