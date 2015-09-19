91d0f0a928f644edd800ded8b1df6c8834b45a51
Jan Hoek <github@uncommonsense.nl>
2015-09-19 11:29:19 +0200

    Simplified parameter for languageID

--- CBreeze/UncommonSense.CBreeze.IO/RoleTailoredClient.cs
+++ CBreeze/UncommonSense.CBreeze.IO/RoleTailoredClient.cs
@@ -9,13 +9,13 @@ namespace UncommonSense.CBreeze.IO
 {
     public static class RoleTailoredClient
     {
-        public static void Run(string fileName, string hyperlink, bool hideNavigationPage = false, bool fullScreen = false, bool consoleMode = false, CultureInfo languageID = null)
+        public static void Run(string fileName, string hyperlink, bool hideNavigationPage = false, bool fullScreen = false, bool consoleMode = false, int? languageID = null)
         {
             var arguments = new StringBuilder();
             arguments.AppendIf(hideNavigationPage, " -shownavigationpane:0");
             arguments.AppendIf(fullScreen, " -fullscreen");
             arguments.AppendIf(consoleMode, " -consolemode");
-            // FIXME: arguments.AppendIf(languageID != null, String.Format(" -language:{0}", languageID.LCID));
+            arguments.AppendIf(languageID.HasValue, String.Format(" -language:{0}", languageID));
             arguments.AppendFormat(" \"{0}\"", hyperlink);
 
             Process.Start(fileName, arguments.ToString());
