45393e6a0c68bc4a0e135552c155c2cdac79f54f
Jan Hoek <github@uncommonsense.nl>
2015-09-24 12:16:56 +0200

    Null values were causing issues; corrected

--- CBreeze/UncommonSense.CBreeze.IO/Arguments.cs
+++ CBreeze/UncommonSense.CBreeze.IO/Arguments.cs
@@ -21,7 +21,7 @@ namespace UncommonSense.CBreeze.IO
 
         public override string ToString()
         {
-            return string.Join(",", this.Where(a => !string.IsNullOrEmpty(a.Value.ToString())).Select(a => string.Format("{0}={1}", a.Key, a.Value)));
+            return string.Join(",", this.Where(a => !string.IsNullOrEmpty((a.Value ?? string.Empty).ToString())).Select(a => string.Format("{0}={1}", a.Key, a.Value)));
         }
     }
 }
