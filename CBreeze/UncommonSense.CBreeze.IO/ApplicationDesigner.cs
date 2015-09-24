caa4a738ba9e67a8d340c40f9bc80bdb6b2bdbff
Jan Hoek <github@uncommonsense.nl>
2015-09-24 12:16:29 +0200

    Added Design extension method for objects

---
+++ CBreeze/UncommonSense.CBreeze.IO/ApplicationDesigner.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UncommonSense.CBreeze.Core;
+
+namespace UncommonSense.CBreeze.IO
+{
+    public static class ApplicationDesigner
+    {
+        public static void Design(this UncommonSense.CBreeze.Core.Object @object, string devClient, string serverName, string database)
+        {
+            var arguments = new Arguments(null, serverName, database);
+            arguments.Add("designobject", string.Format("{0} {1}", @object.Type, @object.ID));
+
+            DevClient.Run(devClient, arguments);
+        }
+    }
+}
