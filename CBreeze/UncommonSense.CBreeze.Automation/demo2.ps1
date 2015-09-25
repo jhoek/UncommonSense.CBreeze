5bc3ce8188e8812be6dd5510bf2347fdaa2b8f76
Jan Hoek <github@uncommonsense.nl>
2015-09-25 15:11:59 +0200

    Added second demo script

---
+++ CBreeze/UncommonSense.CBreeze.Automation/demo2.ps1
@@ -0,0 +1,10 @@
+﻿Import-Module C:\Users\jhoek\GitHub\UncommonSense.CBreeze\CBreeze\UncommonSense.CBreeze.Automation\bin\Debug\UncommonSense.CBreeze.Automation.dll
+
+$Application = New-CBreezeApplication
+$Table = $Application | Add-CBreezeTable -Name Demo
+$Table.Properties.DataCaptionFields.Add(($Table | Add-CBreezeCodeTableField -Name Code -NotBlank $true -PrimaryKeyFieldNoRange -PassThru))
+$Table.Properties.DataCaptionFields.Add(($Table | Add-CBreezeTextTableField -Name Description -DataLength 50 -PassThru))
+
+$Table2 = $Application | Add-CBreezeTable -Name 'Demo Translation'
+
+$Application | Export-CBreezeApplication -TextWriter ([Console]::Out)
