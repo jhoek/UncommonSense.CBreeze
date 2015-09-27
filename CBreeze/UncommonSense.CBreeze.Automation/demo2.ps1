7c948134dfc3b24998200c42cb7eb80efd672671
Jan Hoek <github@uncommonsense.nl>
2015-09-27 18:03:53 +0200

    Added more logic in demo2.ps1

--- CBreeze/UncommonSense.CBreeze.Automation/demo2.ps1
+++ CBreeze/UncommonSense.CBreeze.Automation/demo2.ps1
@@ -1,10 +1,13 @@
 ﻿Import-Module C:\Users\jhoek\GitHub\UncommonSense.CBreeze\CBreeze\UncommonSense.CBreeze.Automation\bin\Debug\UncommonSense.CBreeze.Automation.dll
 
 $Application = New-CBreezeApplication
-$Table = $Application | Add-CBreezeTable -Name Demo
-$Table.Properties.DataCaptionFields.Add(($Table | Add-CBreezeCodeTableField -Name Code -NotBlank $true -PrimaryKeyFieldNoRange -PassThru))
-$Table.Properties.DataCaptionFields.Add(($Table | Add-CBreezeTextTableField -Name Description -DataLength 50 -PassThru))
 
-$Table2 = $Application | Add-CBreezeTable -Name 'Demo Translation'
+$Table = $Application | Add-CBreezeTable -Name Demo -PassThru 
+$Table | Add-CBreezeCodeTableField -Name Code -NotBlank $true -PrimaryKeyFieldNoRange 
+$Table | Add-CBreezeTextTableField -Name Description -DataLength 50
+$Table | Add-CBreezeVariable -Record -Name Cust -SubType ([UncommonSense.CBreeze.Core.BaseApp+TableIDs]::Customer) 
+$Table | Add-CBreezeVariable -Option -Name MyOption -OptionString 'Foo,Baz'
+
+$Page = $Application | Add-CBreezePage -Name Demo -ID 50000 -SourceTable $Table.ID -PageType Card -PassThru
 
 $Application | Export-CBreezeApplication -TextWriter ([Console]::Out)
