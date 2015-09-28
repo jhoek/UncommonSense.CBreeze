4278928c63e8a0b15457aeb7ca5c9de9476303c8
Jan Hoek <github@uncommonsense.nl>
2015-09-28 13:13:03 +0200

    Added missing parameter names, added Import-Module

--- CBreeze/UncommonSense.CBreeze.Automation/demo.ps1
+++ CBreeze/UncommonSense.CBreeze.Automation/demo.ps1
@@ -4,6 +4,9 @@ $CompareToolFileName = 'c:\Program Files\Araxis\Araxis Merge\compare.exe'
 $OutputFileName = 'c:\users\jhoek\desktop\tab7.generated.txt'
 $ReferenceFileName = 'c:\users\jhoek\desktop\tab7.txt'
 
+Clear-Host
+Import-Module .\Bin\Debug\UncommonSense.CBreeze.Automation.dll
+
 Write-Progress -Activity $Activity -CurrentOperation 'Exporting reference file'
 Import-CBreezeApplication -ServerName '.\NAVDEMO' -Database 'Demo Database NAV (7-0)' -Filter 'Type=Table;ID=7|8' | Export-CBreezeApplication -Path $ReferenceFileName
 
@@ -12,7 +15,7 @@ $Application = New-CBreezeApplication
 $DateTime = Get-Date -Year 2012 -Month 9 -Day 7 -Hour 12 -Minute 0 -Second 0
 $VersionList = 'NAVW17.00'
 
-$Table = $Application | Add-CBreezeTable 7 'Standard Text' -DateTime $DateTime -VersionList $VersionList -LookupPageID 8 
+$Table = $Application | Add-CBreezeTable -ID 7 -Name 'Standard Text' -DateTime $DateTime -VersionList $VersionList -LookupPageID 8 -PassThru
 $Table | Add-CBreezeMLValue CaptionML NLD Standaardtekst
 $Table | Add-CBreezeVariable -ID 1000 -Record ExtTextHeader -SubType 279 -Verbose
 $Table | Add-CBreezeCodeTableField -Name Code -DataLength 20 -NotBlank $true -PassThru | Add-CBreezeMLValue CaptionML NLD Code
@@ -22,7 +25,7 @@ $Table.Properties.OnDelete | Add-CBreezeCodeLine 'ExtTextHeader.SETRANGE("Table 
 $Table.Properties.OnDelete | Add-CBreezeCodeLine 'ExtTextHeader.SETRANGE("No.",Code);'
 $Table.Properties.OnDelete | Add-CBreezeCodeLine 'ExtTextHeader.DELETEALL(TRUE);'
 
-$Table = $Application | Add-CBreezeTable 8 Language -DateTime $DateTime -VersionList $VersionList -LookupPageID 9 
+$Table = $Application | Add-CBreezeTable -ID 8 -Name Language -DateTime $DateTime -VersionList $VersionList -LookupPageID 9 -PassThru
 $Table | Add-CBreezeMLValue CaptionML NLD Taal
 $Table | Add-CBreezeCodeTableField -Name Code -NotBlank $true -PassThru | Add-CBreezeMLValue CaptionML NLD Code
 $Table | Add-CBreezeTextTableField -Name Name -DataLength 50 -PassThru | Add-CBreezeMLValue CaptionML NLD Naam
