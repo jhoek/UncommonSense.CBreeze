05be8668e5e080d0726029d8ff9fca2ff1dbb505
Jan Hoek <github@uncommonsense.nl>
2015-09-29 15:34:59 +0200

    Added AddCBreezeReportParameter function

---
+++ CBreeze/UncommonSense.CBreeze.Automation/AddCBreezeReportParameter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using UncommonSense.CBreeze.Core;
+
+namespace UncommonSense.CBreeze.Automation
+{
+    [Cmdlet(VerbsCommon.Add, "CBreezeReportParameter")]
+    public class AddCBreezeReportParameter : AddCBreezeParameter<ReportParameter>
+    {
+        [Parameter(Mandatory = true)]
+        [ValidateRange(1, int.MaxValue)]
+        public int SubType
+        {
+            get;
+            set;
+        }
+
+        protected override ReportParameter CreateParameter()
+        {
+            return Parameters.Add(new ReportParameter(Var, GetParameterID(), Name, SubType));
+        }
+    }
+}
