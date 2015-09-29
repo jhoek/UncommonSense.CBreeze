60887820201a0770a3e2c9ceeb05298906b78367
Jan Hoek <github@uncommonsense.nl>
2015-09-29 20:24:28 +0200

    Added list page to script

--- CBreeze/UncommonSense.CBreeze.Automation/demo2.ps1
+++ CBreeze/UncommonSense.CBreeze.Automation/demo2.ps1
@@ -3,11 +3,13 @@
 $Application = New-CBreezeApplication
 
 $Table = $Application | Add-CBreezeTable -Name Demo -PassThru 
-$Table | Add-CBreezeCodeTableField -Name Code -NotBlank $true -PrimaryKeyFieldNoRange 
-$Table | Add-CBreezeTextTableField -Name Description -DataLength 50
-$Table | Add-CBreezeVariable -Record -Name Cust -SubType ([UncommonSense.CBreeze.Core.BaseApp+TableIDs]::Customer) 
-$Table | Add-CBreezeVariable -Option -Name MyOption -OptionString 'Foo,Baz'
+$CodeField = $Table | Add-CBreezeCodeTableField -Name Code -NotBlank $true -PrimaryKeyFieldNoRange -PassThru
+$DescriptionField = $Table | Add-CBreezeTextTableField -Name Description -DataLength 50 -PassThru
+$Table.Properties.DataCaptionFields.AddRange($CodeField.Name, $DescriptionField.Name);
 
 $Page = $Application | Add-CBreezePage -Name Demo -ID 50000 -SourceTable $Table.ID -PageType Card -PassThru
+$ContentArea = $Page | Add-CBreezeContainerPageControl -Type ContentArea -PassThru
+$Group = $ContentArea | Add-CBreezeGroupPageControl -PassThru
+
 
 $Application | Export-CBreezeApplication -TextWriter ([Console]::Out)
