82c68e0f61e65d67798ea84c5cb7286064dca35e
Jan Hoek <github@uncommonsense.nl>
2015-09-29 18:50:07 +0200

    Added AddCBreezeContainerPageControl and its base class AddCBreezePageControl

---
+++ CBreeze/UncommonSense.CBreeze.Automation/AddCBreezeContainerPageControl.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using UncommonSense.CBreeze.Core;
+
+namespace UncommonSense.CBreeze.Automation
+{
+    [Cmdlet(VerbsCommon.Add, "CBreezeContainerPageControl")]
+    public class AddCBreezeContainerPageControl : AddCBreezePageControl
+    {
+        [Parameter(Mandatory = true)]
+        public ContainerType Type
+        {
+            get;
+            set;
+        }
+
+        [Parameter()]
+        public string Description
+        {
+            get;
+            set;
+        }
+
+        [Parameter()]
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        protected override void ProcessRecord()
+        {
+            var control = new ContainerPageControl(GetPageControlID(), 0);
+            control.Properties.ContainerType = Type;
+            control.Properties.Description = Description;
+            control.Properties.Name = Name;
+
+            switch (Type)
+            {
+                case ContainerType.ContentArea:
+                    Page.Controls.Insert(0, control);
+                    break;
+                case ContainerType.FactBoxArea:
+                    Page.Controls.Add(control);
+                    break;
+                case ContainerType.RoleCenterArea:
+                    Page.Controls.Insert(0, control);
+                    break;
+            }
+
+            if (PassThru)
+                WriteObject(control);
+        }
+
+    }
+}
---
+++ CBreeze/UncommonSense.CBreeze.Automation/AddCBreezePageControl.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using UncommonSense.CBreeze.Core;
+using UncommonSense.CBreeze.Utils;
+
+namespace UncommonSense.CBreeze.Automation
+{
+    public abstract class AddCBreezePageControl : Cmdlet
+    {
+        [Parameter(Mandatory=true, ValueFromPipeline=true)]
+        public Page Page
+        {
+            get;
+            set;
+        }
+
+        [Parameter(ParameterSetName = "Range")]
+        public IEnumerable<int> Range
+        {
+            get;
+            set;
+        }
+
+        [Parameter(ParameterSetName = "ID")]
+        [ValidateRange(1, int.MaxValue)]
+        public int ID
+        {
+            get;
+            set;
+        }
+
+        [Parameter]
+        public SwitchParameter PassThru
+        {
+            get;
+            set;
+        }
+
+        protected int GetPageControlID()
+        {
+            if (ID != 0)
+                return ID;
+
+            var range = Range;
+
+            if (Range.Contains(Page.ID))
+                range = 1.To(int.MaxValue);
+
+            return range.Except(Page.Controls.Select(c => c.ID)).First();
+        }
+    }
+}
