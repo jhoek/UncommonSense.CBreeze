16623c5e321d3a9c16f84428c3016f132dd70769
Jan Hoek <github@uncommonsense.nl>
2015-09-30 21:22:10 +0200

    Introducing DynamicParameter class

---
+++ CBreeze/UncommonSense.CBreeze.Automation/DynamicParameter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+
+namespace UncommonSense.CBreeze.Automation
+{
+    public class DynamicParameter<T>
+    {
+        public DynamicParameter(string name, bool mandatory, T minRange, T maxRange)
+            : this(name, mandatory)
+        {
+            var validateRangeAttribute = new ValidateRangeAttribute(minRange, maxRange);
+            RuntimeDefinedParameter.Attributes.Add(validateRangeAttribute);
+        }
+
+        public DynamicParameter(string name, bool mandatory)
+        {
+            var attributes = new Collection<Attribute>();
+
+            var parameterAttribute = new ParameterAttribute();
+            parameterAttribute.Mandatory = mandatory;
+            attributes.Add(parameterAttribute);
+
+            RuntimeDefinedParameter = new RuntimeDefinedParameter(name, typeof(T), attributes);
+        }
+
+        public RuntimeDefinedParameter RuntimeDefinedParameter
+        {
+            get;
+            protected set;
+        }
+
+        public T Value
+        {
+            get
+            {
+                return (T)RuntimeDefinedParameter.Value;
+            }
+        }
+    }
+}
