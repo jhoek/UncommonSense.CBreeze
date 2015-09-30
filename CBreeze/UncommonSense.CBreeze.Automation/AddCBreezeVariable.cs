146acea3637d673500a74a13a1c923a945333586
Jan Hoek <github@uncommonsense.nl>
2015-09-30 17:10:46 +0200

    Added one single cmdlet for handling all variable types
    If this works, apply same principle to other objects/subobjects

--- CBreeze/UncommonSense.CBreeze.Automation/AddCBreezeVariable.cs
+++ CBreeze/UncommonSense.CBreeze.Automation/AddCBreezeVariable.cs
@@ -8,10 +8,24 @@ using UncommonSense.CBreeze.Core;
 
 namespace UncommonSense.CBreeze.Automation
 {
-    public abstract class AddCBreezeVariable : Cmdlet
+    [Cmdlet(VerbsCommon.Add, "CBreezeVariable")]
+    public class AddCBreezeVariable : CmdletWithDynamicParams
     {
+        private RuntimeDefinedParameter integerSubType = DynamicParameterMgt.IntegerSubType();
+        private RuntimeDefinedParameter recordSecurityFiltering = DynamicParameterMgt.RecordSecurityFiltering();
+        private RuntimeDefinedParameter stringSubType = DynamicParameterMgt.StringSubType();
+        private RuntimeDefinedParameter temporary = DynamicParameterMgt.Temporary();
+        private RuntimeDefinedParameter withEvents = DynamicParameterMgt.WithEvents();
+
         [Parameter(Mandatory = true, ValueFromPipeline = true)]
         public PSObject[] InputObject
+        {
+            get;
+            set;
+        }
+
+        [Parameter(Mandatory = true)]
+        public VariableType Type
         {
             get;
             set;
@@ -44,9 +58,60 @@ namespace UncommonSense.CBreeze.Automation
         {
             get;
             set;
+        } // ???
+
+        [Parameter()]
+        public SwitchParameter PassThru
+        {
+            get;
+            set;
         }
 
-        private Variables GetVariables(PSObject inputObject)
+        protected override void ProcessRecord()
+        {
+            foreach (var inputObject in InputObject)
+            {
+                switch (Type)
+                {
+                    case VariableType.Action:
+                        var actionVariable = GetVariables(inputObject).Add(new ActionVariable(GetVariableID(inputObject), Name));
+                        actionVariable.Dimensions = Dimensions;
+                        WriteVariable(actionVariable);
+                        break;
+
+                    case VariableType.Automation:
+                        var automationVariable = GetVariables(inputObject).Add(new AutomationVariable(GetVariableID(inputObject), Name, StringSubType));
+                        automationVariable.Dimensions = Dimensions;
+                        automationVariable.WithEvents = WithEvents;
+                        WriteVariable(automationVariable);
+                        break;
+
+                    case VariableType.Record:                        
+                        var recordVariable = GetVariables(inputObject).Add(new RecordVariable(GetVariableID(inputObject), Name, IntegerSubType));
+                        recordVariable.Dimensions = Dimensions;
+                        recordVariable.SecurityFiltering = RecordSecurityFiltering;
+                        recordVariable.Temporary = Temporary;
+                        WriteVariable(recordVariable);
+                        break;
+                }
+            }
+        }
+
+        protected void WriteVariable(Variable variable)
+        {
+            if (PassThru)
+                WriteObject(variable);
+        }
+
+        protected int GetVariableID(PSObject inputObject)
+        {
+            if (ID != 0)
+                return ID;
+
+            return Range.Except(GetVariables(inputObject).Select(v => v.ID)).First();
+        }
+
+        protected Variables GetVariables(PSObject inputObject)
         {
             if (inputObject.BaseObject is Table)
                 return (inputObject.BaseObject as Table).Code.Variables;
@@ -73,5 +138,65 @@ namespace UncommonSense.CBreeze.Automation
 
             throw new ApplicationException("Cannot add variables to this object.");
         }
+
+        public override IEnumerable<RuntimeDefinedParameter> DynamicParameters
+        {
+            get
+            {
+                switch (Type)
+                {
+                    case VariableType.Record:
+                        yield return integerSubType;
+                        yield return recordSecurityFiltering;
+                        yield return temporary;
+                        break;
+                    case VariableType.Automation:
+                        yield return stringSubType;
+                        yield return withEvents;
+                        break;
+                }
+            }
+        }
+
+        public int IntegerSubType
+        {
+            get
+            {
+                return (int)this.integerSubType.Value;
+            }
+        }
+
+        public RecordSecurityFiltering? RecordSecurityFiltering
+        {
+            get
+            {
+                return (RecordSecurityFiltering?)this.recordSecurityFiltering.Value;
+            }
+        }
+
+        public string StringSubType
+        {
+            get
+            {
+                return (string)this.stringSubType.Value;
+            }
+        }
+
+        public bool? Temporary
+        {
+            get
+            {
+                return (bool?)this.temporary.Value;
+            }
+        }
+
+        public bool? WithEvents
+        {
+            get
+            {
+                return (bool?)this.withEvents.Value;
+            }
+        }
+
     }
 }
