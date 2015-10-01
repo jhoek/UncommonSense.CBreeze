266e18f159879c6e0f0c873412174c87e5a010b7
Jan Hoek <github@uncommonsense.nl>
2015-10-01 16:17:09 +0200

    Moved retrieving collections to new ExtensionMethods class

---
+++ CBreeze/UncommonSense.CBreeze.Automation/ExtensionMethods.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using UncommonSense.CBreeze.Core;
+
+namespace UncommonSense.CBreeze.Automation
+{
+    public static class ExtensionMethods
+    {
+        public static Parameters GetParameters(this PSObject inputObject)
+        {
+            if (inputObject.BaseObject is Parameters)
+                return (inputObject.BaseObject as Parameters);
+            if (inputObject.BaseObject is Function)
+                return (inputObject.BaseObject as Function).Parameters;
+            if (inputObject.BaseObject is Event)
+                return (inputObject.BaseObject as Event).Parameters;
+
+            throw new ApplicationException("Cannot add parameters to this object.");
+        }
+
+        public static Variables GetVariables(this PSObject inputObject)
+        {
+            if (inputObject.BaseObject is Table)
+                return (inputObject.BaseObject as Table).Code.Variables;
+            if (inputObject.BaseObject is Page)
+                return (inputObject.BaseObject as Page).Code.Variables;
+            if (inputObject.BaseObject is Report)
+                return (inputObject.BaseObject as Report).Code.Variables;
+            if (inputObject.BaseObject is Codeunit)
+                return (inputObject.BaseObject as Codeunit).Code.Variables;
+            if (inputObject.BaseObject is Query)
+                return (inputObject.BaseObject as Query).Code.Variables;
+            if (inputObject.BaseObject is XmlPort)
+                return (inputObject.BaseObject as XmlPort).Code.Variables;
+
+            if (inputObject.BaseObject is Code)
+                return (inputObject.BaseObject as Code).Variables;
+
+            if (inputObject.BaseObject is Function)
+                return (inputObject.BaseObject as Function).Variables;
+            if (inputObject.BaseObject is Trigger)
+                return (inputObject.BaseObject as Trigger).Variables;
+            if (inputObject.BaseObject is Event)
+                return (inputObject.BaseObject as Event).Variables;
+
+            throw new ApplicationException("Cannot add variables to this object.");
+        }
+    }
+}
