0261c12e06b6d38c82a136adef157cf5cb36b40d
Jan Hoek <github@uncommonsense.nl>
2015-10-02 19:12:21 +0200

    Preparations for AddCBreezeFilter

---
+++ CBreeze/UncommonSense.CBreeze.Automation/AddCBreezeFilter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+
+namespace UncommonSense.CBreeze.Automation
+{
+    [Cmdlet(VerbsCommon.Add, "CBreezeFilter")]
+    public class AddCBreezeFilter : CmdletWithDynamicParams
+    {
+        [Parameter(Mandatory = true, ValueFromPipeline = true)]
+        public PSObject InputObject
+        {
+            get;
+            set;
+        }
+
+
+
+
+
+        public override IEnumerable<RuntimeDefinedParameter> DynamicParameters
+        {
+            get
+            {
+                
+            }
+        }
+    }
+}
