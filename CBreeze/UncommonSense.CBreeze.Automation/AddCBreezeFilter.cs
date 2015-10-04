0ab5bd18178eaea673940ce05f2160d70a3794d7
Jan Hoek <github@uncommonsense.nl>
2015-10-04 22:14:34 +0200

    Simplified AddCBreezeFilter

--- CBreeze/UncommonSense.CBreeze.Automation/AddCBreezeFilter.cs
+++ CBreeze/UncommonSense.CBreeze.Automation/AddCBreezeFilter.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Management.Automation;
 using System.Text;
+using UncommonSense.CBreeze.Core;
 
 namespace UncommonSense.CBreeze.Automation
 {
-    [Cmdlet(VerbsCommon.Add, "CBreezeFilter")]
-    public class AddCBreezeFilter : CmdletWithDynamicParams
+    [Cmdlet(VerbsCommon.Add, "CBreezeFilter", DefaultParameterSetName = "Const")]
+    public class AddCBreezeFilter : Cmdlet
     {
         [Parameter(Mandatory = true, ValueFromPipeline = true)]
         public PSObject InputObject
@@ -16,15 +17,84 @@ namespace UncommonSense.CBreeze.Automation
             set;
         }
 
+        [Parameter(Mandatory = true, Position = 0)]
+        public string FieldName
+        {
+            get;
+            set;
+        }
 
+        [Parameter(Mandatory = true, Position = 1, ParameterSetName = "Const")]
+        public SwitchParameter Const
+        {
+            get;
+            set;
+        }
 
+        [Parameter(Mandatory = true, Position = 1, ParameterSetName = "Filter")]
+        public SwitchParameter Filter
+        {
+            get;
+            set;
+        }
 
+        [Parameter(Mandatory = true, Position = 1, ParameterSetName = "Field")]
+        public SwitchParameter Field
+        {
+            get;
+            set;
+        }
 
-        public override IEnumerable<RuntimeDefinedParameter> DynamicParameters
+        [Parameter(Mandatory = true, Position = 2)]
+        [AllowEmptyString]
+        public string Value
+        {
+            get;
+            set;
+        }
+
+        protected override void ProcessRecord()
+        {
+            WriteVerbose(InputObject.BaseObject.GetType().FullName);
+
+            if (InputObject.BaseObject is CalcFormula)
+            {
+                (InputObject.BaseObject as CalcFormula).TableFilter.Add(new CalcFormulaTableFilterLine(FieldName, ExtendedTableFilterType, Value));
+                return;
+            }
+
+            throw new ArgumentOutOfRangeException("InputObject"); // FIXME
+        }
+
+        public bool SupportsFieldFilters
         {
             get
             {
-                
+                if (InputObject == null)
+                    return false;
+
+                if (InputObject.BaseObject is CalcFormula)
+                    return true;
+
+                if (InputObject.BaseObject is TableRelationLines)
+                    return true;
+
+                return false;
+            }
+        }
+
+        protected ExtendedTableFilterType ExtendedTableFilterType
+        {
+            get
+            {
+                if (Const.IsPresent)
+                    return Core.ExtendedTableFilterType.Const;
+                if (Filter.IsPresent)
+                    return Core.ExtendedTableFilterType.Filter;
+                if (Field.IsPresent)
+                    return Core.ExtendedTableFilterType.Field;
+
+                throw new ArgumentOutOfRangeException("ExtendedTableFilterType");
             }
         }
     }
