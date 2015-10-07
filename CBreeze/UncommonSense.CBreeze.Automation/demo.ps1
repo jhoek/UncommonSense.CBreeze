30678eb254a3ac81ebfd4531ea00458d17c87989
Jan Hoek <github@uncommonsense.nl>
2015-10-07 15:43:41 +0200

    Changes to test scripts

--- CBreeze/UncommonSense.CBreeze.Automation/demo.ps1
+++ CBreeze/UncommonSense.CBreeze.Automation/demo.ps1
@@ -8,27 +8,29 @@ Clear-Host
 Import-Module .\Bin\Debug\UncommonSense.CBreeze.Automation.dll
 
 Write-Progress -Activity $Activity -CurrentOperation 'Exporting reference file'
-Import-CBreezeApplication -ServerName '.\NAVDEMO' -Database 'Demo Database NAV (7-0)' -Filter 'Type=Table;ID=7|8' | Export-CBreezeApplication -Path $ReferenceFileName
+$Application = Import-CBreezeApplication -ServerName '.\NAVDEMO' -Database 'Demo Database NAV (7-0)' -Filter 'Type=Table;ID=7|8'
+Export-CBreezeApplication -Application $Application -Path $ReferenceFileName
 
 Write-Progress -Activity $Activity -CurrentOperation 'Building C/Breeze application'
 $Application = New-CBreezeApplication
+
 $DateTime = Get-Date -Year 2012 -Month 9 -Day 7 -Hour 12 -Minute 0 -Second 0
 $VersionList = 'NAVW17.00'
 
-$Table = $Application | Add-CBreezeTable -ID 7 -Name 'Standard Text' -DateTime $DateTime -VersionList $VersionList -LookupPageID 8 -PassThru
-$Table | Add-CBreezeMLValue CaptionML NLD Standaardtekst
-$Table | Add-CBreezeVariable -ID 1000 -Record ExtTextHeader -SubType 279 -Verbose
-$Table | Add-CBreezeCodeTableField -Name Code -DataLength 20 -NotBlank $true -PassThru | Add-CBreezeMLValue CaptionML NLD Code
-$Table | Add-CBreezeTextTableField -Name Description -DataLength 50 -AutoCaption -PassThru | Add-CBreezeMLValue CaptionML NLD Omschrijving
+$Table = Add-CBreezeObject -Application $application -Type Table -ID 7 -Name 'Standard Text' -DateTime $DateTime -VersionList $VersionList -LookupPageID 8 -PassThru
+$Table | Add-CBreezeMLValue -Property CaptionML -LanguageName 1043 -Value Standaardtekst
+$Table | Add-CBreezeVariable -ID 1000 -Type Record ExtTextHeader -SubType 279 -Verbose
+$Table | Add-CBreezeTableField -Type Code -Name Code -DataLength 20 -NotBlank $true -PassThru | Add-CBreezeMLValue CaptionML NLD Code
+$Table | Add-CBreezeTableField -Type Text -Name Description -DataLength 50 -AutoCaption -PassThru | Add-CBreezeMLValue CaptionML NLD Omschrijving
 $Table | Add-CBreezeTableKey -Fields Code -Clustered $true
 $Table.Properties.OnDelete | Add-CBreezeCodeLine 'ExtTextHeader.SETRANGE("Table Name",ExtTextHeader."Table Name"::"Standard Text");'
 $Table.Properties.OnDelete | Add-CBreezeCodeLine 'ExtTextHeader.SETRANGE("No.",Code);'
 $Table.Properties.OnDelete | Add-CBreezeCodeLine 'ExtTextHeader.DELETEALL(TRUE);'
 
-$Table = $Application | Add-CBreezeTable -ID 8 -Name Language -DateTime $DateTime -VersionList $VersionList -LookupPageID 9 -PassThru
+$Table = Add-CBreezeObject -Application $Application -Type Table -ID 8 -Name Language -DateTime $DateTime -VersionList $VersionList -LookupPageID 9 -PassThru
 $Table | Add-CBreezeMLValue CaptionML NLD Taal
-$Table | Add-CBreezeCodeTableField -Name Code -NotBlank $true -PassThru | Add-CBreezeMLValue CaptionML NLD Code
-$Table | Add-CBreezeTextTableField -Name Name -DataLength 50 -PassThru | Add-CBreezeMLValue CaptionML NLD Naam
+$Table | Add-CBreezeTableField -Type Code -Name Code -NotBlank $true -PassThru | Add-CBreezeMLValue CaptionML NLD Code
+$Table | Add-CBreezeTableField -Type Text -Name Name -DataLength 50 -PassThru | Add-CBreezeMLValue CaptionML NLD Naam
 $Table | Add-CBreezeTableKey -Fields Code -Clustered $true
 
 $Function = $Table | Add-CBreezeFunction -Name GetUserLanguage -ReturnValueType Code -PassThru
@@ -39,7 +41,7 @@ $Function | Add-CBreezeCodeLine 'SETRANGE("Windows Language ID");'
 $Function | Add-CBreezeCodeLine 'EXIT(Code);'
 
 $Function = $Table | Add-CBreezeFunction -Name GetLanguageID -ReturnValueType Integer -PassThru
-$Function | Add-CBreezeCodeParameter -ID 1000 -Name LanguageCode -DataLength 10
+$Function | Add-CBreezeParameter -Type Code -ID 1000 -Name LanguageCode -DataLength 10
 $Function | Add-CBreezeCodeLine 'CLEAR(Rec);'
 $Function | Add-CBreezeCodeLine 'IF LanguageCode <> '''' THEN'
 $Function | Add-CBreezeCodeLine '  IF GET(LanguageCode) THEN'
@@ -48,7 +50,7 @@ $Function | Add-CBreezeCodeLine '"Windows Language ID" := GLOBALLANGUAGE;'
 $Function | Add-CBreezeCodeLine 'EXIT("Windows Language ID");'
 
 Write-Progress -Activity $Activity -CurrentOperation 'Exporting C/Breeze application'
-$Application | Export-CBreezeApplication -Path $OutputFileName
+Export-CBreezeApplication -Path $OutputFileName -Application $Application
 
 Write-Progress -Activity $Activity -CurrentOperation 'Starting compare tool'
 & $CompareToolFileName $ReferenceFileName $OutputFileName
--- CBreeze/UncommonSense.CBreeze.Automation/demo2.ps1
+++ CBreeze/UncommonSense.CBreeze.Automation/demo2.ps1
@@ -2,14 +2,11 @@
 
 $Application = New-CBreezeApplication
 
-$Table = $Application | Add-CBreezeTable -Name Demo -PassThru 
-$CodeField = $Table | Add-CBreezeCodeTableField -Name Code -NotBlank $true -PrimaryKeyFieldNoRange -PassThru
-$DescriptionField = $Table | Add-CBreezeTextTableField -Name Description -DataLength 50 -PassThru
+$Table = $Application | Add-CBreezeObject -Type Table -Name Demo -PassThru 
+$CodeField = $Table | Add-CBreezeTableField -Type Code -Name Code -NotBlank $true -PrimaryKeyFieldNoRange -PassThru
+$DescriptionField = $Table | Add-CBreezeTableField -Type Text -Name Description -DataLength 50 -PassThru
 $Table.Properties.DataCaptionFields.AddRange($CodeField.Name, $DescriptionField.Name);
 
-$Page = $Application | Add-CBreezePage -Name Demo -ID 50000 -SourceTable $Table.ID -PageType Card -PassThru
-$ContentArea = $Page | Add-CBreezeContainerPageControl -Type ContentArea -PassThru
-$Group = $ContentArea | Add-CBreezeGroupPageControl -PassThru
-
+$Page = $Application | Add-CBreezeObject -Type Page -Name Demo -ID 50000 -SourceTable $Table.ID -PageType Card -PassThru
 
 $Application | Export-CBreezeApplication -TextWriter ([Console]::Out)
