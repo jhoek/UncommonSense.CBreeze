b13f5fbdb98dbcaa98eb6bddb7525d760a256df4
Jan Hoek <github@uncommonsense.nl>
2015-10-11 10:26:26 +0200

    Cmdlet for adding table relations to table fields and page controls

---
+++ CBreeze/UncommonSense.CBreeze.Automation/AddCBreezeTableRelation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using UncommonSense.CBreeze.Core;
+
+namespace UncommonSense.CBreeze.Automation
+{
+    [Cmdlet(VerbsCommon.Add, "CBreezeTableRelation")]
+    public class AddCBreezeTableRelation : Cmdlet 
+    {
+        [Parameter(Mandatory = true, ValueFromPipeline = true)]
+        public dynamic InputObject
+        {
+            get;
+            set;
+        }
+
+        [Parameter(Mandatory=true)]
+        public string TableName
+        {
+            get;
+            set;
+        }
+
+        [Parameter()]
+        public string FieldName
+        {
+            get;
+            set;
+        }
+
+        [Parameter()]
+        public SwitchParameter PassThru
+        {
+            get;
+            set;
+        }
+
+        protected override void ProcessRecord()
+        {
+            var tableRelationLine = InputObject.Properties.TableRelation.Add(TableName);
+            tableRelationLine.FieldName = FieldName;
+
+            if (PassThru)
+                WriteObject(tableRelationLine);
+        }
+    }
+}
