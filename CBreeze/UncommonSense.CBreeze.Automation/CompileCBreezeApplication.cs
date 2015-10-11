e88dedf25d9b866f5130b2405c3480e7d1b55bd2
Jan Hoek <github@uncommonsense.nl>
2015-10-11 05:47:38 +0200

    Added CompileCBreezeApplication cmdlet; requires IO namespace

---
+++ CBreeze/UncommonSense.CBreeze.Automation/CompileCBreezeApplication.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using UncommonSense.CBreeze.Core;
+using UncommonSense.CBreeze.IO;
+
+namespace UncommonSense.CBreeze.Automation
+{
+    [Cmdlet("Compile", "CBreezeApplication")]
+    public class CompileCBreezeApplication : Cmdlet
+    {
+        public CompileCBreezeApplication()
+        {
+            DevClientPath = @"C:\Program Files (x86)\Microsoft Dynamics NAV\70\RoleTailored Client\finsql.exe";
+            ServerName = ".";
+        }
+
+        [Parameter(Mandatory = true, ValueFromPipeline = true)]
+        public Application Application
+        {
+            get;
+            set;
+        }
+
+        [Parameter()]
+        public string DevClientPath
+        {
+            get;
+            set;
+        }
+
+        [Parameter()]
+        public string ServerName
+        {
+            get;
+            set;
+        }
+
+        [Parameter(Mandatory = true)]
+        public string Database
+        {
+            get;
+            set;
+        }
+
+        protected override void ProcessRecord()
+        {
+            ApplicationCompiler.Compile(Application, DevClientPath, ServerName, Database);
+        }
+    }
+}
