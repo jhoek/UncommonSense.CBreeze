715df337584415f7f597974d5b447e77c5631d3e
Jan Hoek <github@uncommonsense.nl>
2015-10-11 19:48:48 +0200

    Expose more Utils methods to PowerShell

---
+++ CBreeze/UncommonSense.CBreeze.Automation/GetCBreezePageGroup.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using UncommonSense.CBreeze.Core;
+using UncommonSense.CBreeze.Utils;
+
+namespace UncommonSense.CBreeze.Automation
+{
+    [Cmdlet(VerbsCommon.Get, "CBreezePageGroup")]
+    public class GetCBreezePageGroup : PSCmdlet
+    {
+        [Parameter(Mandatory = true, ValueFromPipeline = true)]
+        public Page Page
+        {
+            get;
+            set;
+        }
+
+        [Parameter(Mandatory = true)]
+        public IEnumerable<int> Range
+        {
+            get;
+            set;
+        }
+
+        [Parameter(Mandatory = true, ParameterSetName = "ByGroupType")]
+        public GroupType GroupType
+        {
+            get;
+            set;
+        }
+
+        [Parameter(Mandatory = true, ParameterSetName = "ByGroupCaption")]
+        public string GroupCaption
+        {
+            get;
+            set;
+        }
+
+        [Parameter()]
+        public Position? Position
+        {
+            get;
+            set;
+        }
+
+        protected override void ProcessRecord()
+        {
+            var contentArea = Page.GetContentArea(Range);
+
+            switch (ParameterSetName)
+            {
+                case "ByGroupType":
+                    WriteObject(contentArea.GetGroupByType(GroupType, Range, Position ?? UncommonSense.CBreeze.Utils.Position.FirstWithinContainer));
+                    break;
+                case "ByGroupCaption":
+                    WriteObject(contentArea.GetGroupByCaption(GroupCaption, Range, Position ?? UncommonSense.CBreeze.Utils.Position.LastWithinContainer));
+                    break;
+            }
+        }
+    }
+}
