3ba986fd83c847ce3a6cd73cfac0ec3c31360ed5
Jan Hoek <github@uncommonsense.nl>
2015-10-14 10:42:36 +0200

    Quoted() now lives in Core, not in Utils

--- CBreeze/UncommonSense.CBreeze.Write/StringExtensionMethods.cs
+++ CBreeze/UncommonSense.CBreeze.Write/StringExtensionMethods.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using UncommonSense.CBreeze.Utils;
+using UncommonSense.CBreeze.Core;
 
 namespace UncommonSense.CBreeze.Write
 {
