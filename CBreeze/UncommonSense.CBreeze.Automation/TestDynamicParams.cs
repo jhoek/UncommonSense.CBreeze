11168ad01a2b14e9fca635930c522b61768332f4
Jan Hoek <github@uncommonsense.nl>
2015-10-19 15:19:37 +0200

    (Temporarily) adding Test-DynamicParams

---
+++ CBreeze/UncommonSense.CBreeze.Automation/TestDynamicParams.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using UncommonSense.CBreeze.Core;
+
+namespace UncommonSense.CBreeze.Automation
+{
+    [Cmdlet(VerbsDiagnostic.Test, "DynamicParams")]
+    public class TestDynamicParams : CmdletWithDynamicParams
+    {
+        public TestDynamicParams()
+        {
+            Application = new DynamicParameter<Core.Application>("Application", true);
+            Range = new DynamicParameter<IEnumerable<int>>("Range", true);
+            Dynamic = new DynamicParameter<int>("Dynamic", "Foo");
+        }
+
+        protected DynamicParameter<Application> Application
+        {
+            get;
+            set;
+        }
+
+        protected DynamicParameter<IEnumerable<int>> Range
+        {
+            get;
+            set;
+        }
+
+        protected DynamicParameter<int> Dynamic
+        {
+            get;
+            set;
+        }
+
+        protected override void ProcessRecord()
+        {
+            WriteObject(Dynamic.Value);
+        }
+
+        public override IEnumerable<RuntimeDefinedParameter> DynamicParameters
+        {
+            get
+            {
+                yield return Application.RuntimeDefinedParameter;
+                yield return Range.RuntimeDefinedParameter;
+                yield return Dynamic.RuntimeDefinedParameter;
+            }
+        }
+    }
+}
