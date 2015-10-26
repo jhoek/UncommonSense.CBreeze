d618574f876b59a2f29bb1fc0f1d07a9bff442de
Jan Hoek <github@uncommonsense.nl>
2015-10-26 12:56:43 +0100

    Control IDs and action IDs come from same range

--- CBreeze/UncommonSense.CBreeze.Automation/AddCBreezePageAction.cs
+++ CBreeze/UncommonSense.CBreeze.Automation/AddCBreezePageAction.cs
@@ -260,7 +260,10 @@ namespace UncommonSense.CBreeze.Automation
             if (Range.Contains(Page.ID))
                 range = 1.To(int.MaxValue);
 
-            return range.Except(Page.Properties.ActionList.Select(a => a.ID)).First();
+            var controlIDs = Page.Controls.Select(c => c.ID);
+            var actionIDs = Page.Properties.ActionList.Select(a => a.ID);
+
+            return range.Except(controlIDs).Except(actionIDs).First();
         }
 
         public override IEnumerable<RuntimeDefinedParameter> DynamicParameters
