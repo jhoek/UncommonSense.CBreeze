85b0908b130105af981dc946f61db26f339dd352
Jan Hoek <github@uncommonsense.nl>
2015-10-26 12:56:58 +0100

    Added Get-CBreezePageActionItems

---
+++ CBreeze/UncommonSense.CBreeze.Automation/GetCBreezePageActionItems.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using UncommonSense.CBreeze.Core;
+using UncommonSense.CBreeze.Utils;
+
+namespace UncommonSense.CBreeze.Automation
+{
+    [Cmdlet(VerbsCommon.Get, "CBreezePageActionItems")]
+    public class GetCBreezePageActionItems : Cmdlet
+    {
+        [Parameter(Mandatory = true, ValueFromPipeline = true)]
+        public Page Page
+        {
+            get;
+            set;
+        }
+
+        [Parameter(Mandatory=true)]
+        public IEnumerable<int> Range
+        {
+            get;
+            set;
+        }
+
+        protected override void ProcessRecord()
+        {
+            WriteObject(Page.GetActionItems(Range));
+        }
+    }
+}
