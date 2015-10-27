047bb8035fa8eaa2799e073e00b3b2ec4054d8a3
Jan Hoek <github@uncommonsense.nl>
2015-10-27 14:31:51 +0100

    ExportCBreezeApplication now uses ApplicationImporter

--- CBreeze/UncommonSense.CBreeze.Automation/ExportCBreezeApplication.cs
+++ CBreeze/UncommonSense.CBreeze.Automation/ExportCBreezeApplication.cs
@@ -6,11 +6,12 @@ using System.Linq;
 using System.Management.Automation;
 using System.Text;
 using UncommonSense.CBreeze.Core;
+using UncommonSense.CBreeze.IO;
 using UncommonSense.CBreeze.Write;
 
 namespace UncommonSense.CBreeze.Automation
 {
-    [Cmdlet(VerbsData.Export, "CBreezeApplication", DefaultParameterSetName="ToPath")]
+    [Cmdlet(VerbsData.Export, "CBreezeApplication", DefaultParameterSetName = "ToPath")]
     public class ExportCBreezeApplication : PSCmdlet
     {
         public ExportCBreezeApplication()
@@ -20,14 +21,14 @@ namespace UncommonSense.CBreeze.Automation
             ImportAction = "Skip";
         }
 
-        [Parameter(Mandatory=true,ValueFromPipeline=true)]
+        [Parameter(Mandatory = true, ValueFromPipeline = true)]
         public Application Application
         {
             get;
             set;
         }
 
-        [Parameter(Mandatory = true, ParameterSetName = "ToPath", Position=0)]
+        [Parameter(Mandatory = true, ParameterSetName = "ToPath", Position = 0)]
         public string Path
         {
             get;
@@ -62,7 +63,7 @@ namespace UncommonSense.CBreeze.Automation
             set;
         }
 
-        [Parameter(Mandatory=true,ParameterSetName="ToDatabase")]
+        [Parameter(Mandatory = true, ParameterSetName = "ToDatabase")]
         public string Database
         {
             get;
@@ -91,53 +92,7 @@ namespace UncommonSense.CBreeze.Automation
                     Application.Write(Stream);
                     break;
                 case "ToDatabase":
-                    var tempFileName = System.IO.Path.ChangeExtension(System.IO.Path.GetTempFileName(), "txt");
-                    var logFileName = System.IO.Path.GetTempFileName();
-                    var resultFileName = System.IO.Path.Combine(System.IO.Path.GetTempPath(), "navcommandresult.txt");
-
-                    var arguments = new Dictionary<string, string>();
-                    arguments.Add("command", "importobjects");
-                    arguments.Add("file", tempFileName);
-                    arguments.Add("servername", ServerName);
-                    arguments.Add("database", Database);
-                    arguments.Add("logfile", logFileName);
-                    arguments.Add("importaction", ImportAction);
-                    arguments.Add("ntauthentication", "1");
-
-                    var processStartInfo = new ProcessStartInfo();
-                    processStartInfo.FileName = DevClientPath;
-                    processStartInfo.Arguments = string.Join(", ", arguments.Where(k=>!string.IsNullOrEmpty(k.Value)).Select(k => string.Format("{0}={1}", k.Key, k.Value)));
-                    processStartInfo.UseShellExecute = false;
-
-                    try
-                    {
-                        Application.Write(tempFileName);
-
-                        Process.Start(processStartInfo).WaitForExit();
-
-                        if (File.Exists(logFileName))
-                        {
-                            throw new ApplicationException(File.ReadAllText(logFileName));
-                        }
-                    }
-                    finally
-                    {
-                        if (File.Exists(tempFileName))
-                        {
-                            File.Delete(tempFileName);
-                        }
-
-                        if (File.Exists(logFileName))
-                        {
-                            File.Delete(logFileName);
-                        }
-
-                        if (File.Exists(resultFileName))
-                        {
-                            File.Delete(resultFileName);
-                        }
-                    }
-
+                    ApplicationImporter.Import(Application, DevClientPath, ServerName, Database);
                     break;
             }
         }
