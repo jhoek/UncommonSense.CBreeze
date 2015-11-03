2ba2b34edbb564fd1cd804971b1e84f1200434a6
Jan Hoek <github@uncommonsense.nl>
2015-11-03 08:09:12 +0100

    Overridden ToString()

--- CBreeze/UncommonSense.CBreeze.Core/TableField.cs
+++ CBreeze/UncommonSense.CBreeze.Core/TableField.cs
@@ -25,6 +25,11 @@ namespace UncommonSense.CBreeze.Core
 			Name = name;
 		}
 
+        public override string ToString()
+        {
+            return string.Format("{0} {1} {2}", Type, ID, Name);
+        }
+
 		public abstract TableFieldType Type
 		{
 			get;
