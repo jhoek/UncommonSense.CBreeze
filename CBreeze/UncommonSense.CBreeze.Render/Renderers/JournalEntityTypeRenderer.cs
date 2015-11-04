0ae42fb08497766e2fd91caf1abe54eac528f361
Jan Hoek <github@uncommonsense.nl>
2015-11-04 12:32:30 +0100

    Remove reference to no longer existing namespace

--- CBreeze/UncommonSense.CBreeze.Render/Renderers/JournalEntityTypeRenderer.cs
+++ CBreeze/UncommonSense.CBreeze.Render/Renderers/JournalEntityTypeRenderer.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UncommonSense.CBreeze.Core;
-using UncommonSense.CBreeze.Model;
 using UncommonSense.CBreeze.Utils;
 using UncommonSense.CBreeze.Write;
 
