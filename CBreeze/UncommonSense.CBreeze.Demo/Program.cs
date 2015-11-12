07c8435dd6ab30a077a3d003167b3eb57a43d88d
Jan Hoek <github@uncommonsense.nl>
2015-11-12 08:56:46 +0100

    Report elapsed time for reading (in addition to parsing and writing)

--- CBreeze/UncommonSense.CBreeze.Demo/Program.cs
+++ CBreeze/UncommonSense.CBreeze.Demo/Program.cs
@@ -4,6 +4,7 @@ using UncommonSense.CBreeze.Core;
 using UncommonSense.CBreeze.Write;
 using UncommonSense.CBreeze.Read;
 using System.IO;
+using System.Text;
 
 namespace UncommonSense.CBreeze.Demo
 {
@@ -17,17 +18,23 @@ namespace UncommonSense.CBreeze.Demo
 			var inputFileName = Path.Combine(desktopFolderName, "nl2013.txt");
 			var outputFileName = Path.Combine(desktopFolderName, "nl2013.output.txt");
 
+            var readerStopWatch = new Stopwatch();
+            readerStopWatch.Start();
+            var lines = File.ReadLines(inputFileName, Encoding.GetEncoding("ibm850"));
+            readerStopWatch.Stop();
+            Console.WriteLine("Reading took {0} ms.", readerStopWatch.ElapsedMilliseconds);
+            
 			var parserStopwatch = new Stopwatch();
 			parserStopwatch.Start();
-			var application = ApplicationBuilder.FromFile(inputFileName);
+			var application = ApplicationBuilder.FromLines(lines);
 			parserStopwatch.Stop();
-			Console.WriteLine(parserStopwatch.Elapsed);
+			Console.WriteLine("Parsing took {0} ms.", parserStopwatch.ElapsedMilliseconds);
 
 			var writerStopwatch = new Stopwatch();
 			writerStopwatch.Start();
 			application.Write(outputFileName);
 			writerStopwatch.Stop();
-			Console.WriteLine(writerStopwatch.Elapsed);
+			Console.WriteLine("Writing took {0} ms.", writerStopwatch.ElapsedMilliseconds);
 
 			Process.Start(compareToolFileName, string.Format("\"{0}\" \"{1}\"", inputFileName, outputFileName));
 		}
