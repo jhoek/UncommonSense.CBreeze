011ab5c4ff41e25c23bb003376eb5dc3d561bd98
Jan Hoek <github@uncommonsense.nl>
2015-11-17 15:48:33 +0100

    Using new Container property to retrieve Index

---
+++ CBreeze/UncommonSense.CBreeze.Utils/MenuSuiteNodeExtensionMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UncommonSense.CBreeze.Core;
+
+namespace UncommonSense.CBreeze.Utils
+{
+    public static class MenuSuiteNodeExtensionMethods
+    {
+        public static int Index(this MenuSuiteNode menuSuiteNode)
+        {
+            return menuSuiteNode.Container.IndexOf(menuSuiteNode);
+        }
+    }
+}
