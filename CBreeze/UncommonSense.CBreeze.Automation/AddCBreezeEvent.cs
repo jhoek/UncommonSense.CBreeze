f40a3e39554600ca74fdd53691073534ee5934d1
Jan Hoek <github@uncommonsense.nl>
2015-11-18 09:57:29 +0100

    Adding AddCBreezeEvent

---
+++ CBreeze/UncommonSense.CBreeze.Automation/AddCBreezeEvent.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using UncommonSense.CBreeze.Core;
+
+namespace UncommonSense.CBreeze.Automation
+{
+    [Cmdlet(VerbsCommon.Add, "CBreezeEvent")]
+    public class AddCBreezeEvent : Cmdlet
+    {
+        [Parameter(Mandatory = true, ValueFromPipeline = true)]
+        public PSObject InputObject
+        {
+            get;
+            set;
+        }
+
+        [Parameter(Mandatory = true, ParameterSetName = "ID")]
+        [ValidateRange(1, int.MaxValue)]
+        public int ID
+        {
+            get;
+            set;
+        }
+
+        [Parameter(Mandatory = true, ParameterSetName = "Range")]
+        public IEnumerable<int> Range
+        {
+            get;
+            set;
+        }
+
+        [Parameter(Mandatory = true)]
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        [Parameter(Mandatory = true)]
+        public int SourceID
+        {
+            get;
+            set;
+        }
+
+        [Parameter(Mandatory = true)]
+        public string SourceName
+        {
+            get;
+            set;
+        }
+
+        [Parameter()]
+        public SwitchParameter PassThru
+        {
+            get;
+            set;
+        }
+
+        protected override void ProcessRecord()
+        {
+            var @event = GetEvents(InputObject).Add(new Event(SourceID, SourceName, GetEventID(InputObject), Name));
+            
+            if (PassThru)
+                WriteObject(@event);
+        }
+
+        protected int GetEventID(PSObject inputObject)
+        {
+            if (ID != 0)
+                return ID;
+
+            return Range.Except(GetEvents(inputObject).Select(e => e.ID)).First();
+        }
+
+        protected Events GetEvents(PSObject inputObject)
+        {
+            if (inputObject.BaseObject is Events)
+                return (inputObject.BaseObject as Events);
+
+            if (inputObject.BaseObject is Code)
+                return (inputObject.BaseObject as Code).Events;
+
+            if (inputObject.BaseObject is Table)
+                return (inputObject.BaseObject as Table).Code.Events;
+            if (inputObject.BaseObject is Page)
+                return (inputObject.BaseObject as Page).Code.Events;
+            if (inputObject.BaseObject is Report)
+                return (inputObject.BaseObject as Report).Code.Events;
+            if (inputObject.BaseObject is Codeunit)
+                return (inputObject.BaseObject as Codeunit).Code.Events;
+            if (inputObject.BaseObject is Query)
+                return (inputObject.BaseObject as Query).Code.Events;
+            if (inputObject.BaseObject is XmlPort)
+                return (inputObject.BaseObject as XmlPort).Code.Events;
+
+            throw new ApplicationException("Cannot add events to this object.");
+        }
+    }
+}
