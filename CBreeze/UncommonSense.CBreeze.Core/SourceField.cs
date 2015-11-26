63c9abc6a92c4490dc1683bdb41310badfe06b21
Jan Hoek <github@uncommonsense.nl>
2015-11-26 14:30:21 +0100

    Replaced backing fields with auto-implemented properties

--- CBreeze/UncommonSense.CBreeze.Core/SourceField.cs
+++ CBreeze/UncommonSense.CBreeze.Core/SourceField.cs
@@ -17,35 +17,14 @@ namespace UncommonSense.CBreeze.Core
     [Serializable]
     public partial class SourceField
     {
-        private String fieldName;
-        private String tableVariableName;
-
-        public SourceField()
-        {
-        }
-
         public String FieldName
         {
-            get
-            {
-                return this.fieldName;
-            }
-            set
-            {
-                this.fieldName = value;
-            }
+            get;set;
         }
 
         public String TableVariableName
         {
-            get
-            {
-                return this.tableVariableName;
-            }
-            set
-            {
-                this.tableVariableName = value;
-            }
+            get;set;
         }
 
     }
