49466d6f5508dc142ab39fd86726ab0c3fbf16f4
Jan Hoek <github@uncommonsense.nl>
2015-11-27 13:07:46 +0100

    Moved logic for summarizing ID ranges into separate class

--- CBreeze/UncommonSense.CBreeze.IO/ApplicationCompiler.cs
+++ CBreeze/UncommonSense.CBreeze.IO/ApplicationCompiler.cs
@@ -10,19 +10,6 @@ namespace UncommonSense.CBreeze.IO
 {
     public static class ApplicationCompiler
     {
-        public static string Summarize(this IEnumerable<int> ids)
-        {
-            var groups = ids.Select((n, i) => new
-            {
-                ID = n,
-                Group = n - i
-            }).GroupBy(n => n.Group);
-
-            var ranges = groups.Select(g => g.Count() >= 3 ? string.Format("{0}..{1}", g.First().ID, g.Last().ID) : string.Join("|", g.Select(x => x.ID)));
-
-            return string.Join("|", ranges);
-        }
-
         public static void Compile(this Application application, string devClient, string serverName, string database)
         {
             Compile("Table", application.Tables.Select(t => t.ID), devClient, serverName, database);
---
+++ CBreeze/UncommonSense.CBreeze.IO/RangeExtensionMethods.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UncommonSense.CBreeze.IO
+{
+    public static class RangeExtensionMethods
+    {
+        public static string Summarize(this IEnumerable<int> ids)
+        {
+            var groups = ids.Select((n, i) => new
+            {
+                ID = n,
+                Group = n - i
+            }).GroupBy(n => n.Group);
+
+            var ranges = groups.Select(g => g.Count() >= 3 ? string.Format("{0}..{1}", g.First().ID, g.Last().ID) : string.Join("|", g.Select(x => x.ID)));
+
+            return string.Join("|", ranges);
+        }
+    }
+}
