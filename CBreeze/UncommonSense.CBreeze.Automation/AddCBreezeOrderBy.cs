857e59c10f31a3cc4a0cd0b354cc5b633de38c56
Jan Hoek <github@uncommonsense.nl>
2015-11-29 18:52:23 +0100

    Introduced AddCBreezeOrderBy

---
+++ CBreeze/UncommonSense.CBreeze.Automation/AddCBreezeOrderBy.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using UncommonSense.CBreeze.Core;
+
+namespace UncommonSense.CBreeze.Automation
+{
+    [Cmdlet(VerbsCommon.Add, "CBreezeOrderBy")]
+    public class AddCBreezeOrderBy : Cmdlet
+    {
+        [Parameter(Mandatory=true, ValueFromPipeline=true)]
+        public Query InputObject
+        {
+            get;
+            set;
+        }
+
+        [Parameter(Mandatory = true)]
+        public string Column
+        {
+            get;
+            set;
+        }
+
+        [Parameter()]
+        public QueryOrderByDirection Direction
+        {
+            get;
+            set;
+        }
+
+        protected override void ProcessRecord()
+        {
+            InputObject.Properties.OrderBy.Add(Column, Direction);
+        }
+    }
+}
