8c9ceca10ffec118a1940c74ec5692e74eea4c19
Jan Hoek <github@uncommonsense.nl>
2015-11-30 12:01:16 +0100

    line.Type is no longer nullable
    In fact, it never was - the only way to set is was through the ctor,
    which did not accept nulls.

--- CBreeze/UncommonSense.CBreeze.Write/PropertyWriter.cs
+++ CBreeze/UncommonSense.CBreeze.Write/PropertyWriter.cs
@@ -606,7 +606,7 @@ namespace UncommonSense.CBreeze.Write
             foreach (var line in property.Value)
             {
                 var isLastLine = (line == property.Value.Last());
-                writer.WriteLine("{0}={1}({2}){3}", line.Column, line.Type.Value.AsString(), line.Value, isLastLine ? ";" : ",");
+                writer.WriteLine("{0}={1}({2}){3}", line.Column, line.Type.AsString(), line.Value, isLastLine ? ";" : ",");
             }
 
             writer.Unindent();
