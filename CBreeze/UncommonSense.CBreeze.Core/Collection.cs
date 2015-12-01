3790afb8cc1d0dc37183b3065d4ed9d44edccec3
Jan Hoek <github@uncommonsense.nl>
2015-12-01 09:41:38 +0100

    Introduced generic Collection<T> class
    Add and Insert return added item; AddRange adds multiple items at once

---
+++ CBreeze/UncommonSense.CBreeze.Core/Collection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UncommonSense.CBreeze.Core
+{
+    public abstract class Collection<T> : System.Collections.ObjectModel.Collection<T>
+    {
+        public new T Add(T value)
+        {
+            base.Add(value);
+            return value;
+        }
+
+        public void AddRange(IEnumerable<T> values)
+        {
+            foreach (var value in values)
+            {
+                base.Add(value);
+            }
+        }
+
+        public new T Insert(int index, T value)
+        {
+            base.Insert(index, value);
+            return value;
+        }
+    }
+}
