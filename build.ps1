f5b3159e7a9e51df02eee9834247c679d2abc476
Jan Hoek <github@uncommonsense.nl>
2015-12-10 14:27:42 +0100

    Added build support for remaining assemblies

--- build.ps1
+++ build.ps1
@@ -1,13 +1,66 @@
-﻿cd C:\Users\jhoek\GitHub\UncommonSense.CBreeze\CBreeze\UncommonSense.CBreeze.Core
+﻿function Invoke-Build
+{
+    Param
+    (
+        [ValidateSet('Core','Parse','Read','Write','Automation', 'IO')]
+        [string]$Assembly,
 
-Write-Host 'Building Core 2013...' -ForegroundColor DarkYellow
-& C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe /out:bin/debug/UncommonSense.CBreeze.2013.Core.dll /target:library *.cs .\Properties\AssemblyInfo.cs
+        [ValidateSet('2013', '2013R2', '2015', '2016')]
+        [string]$Version,
 
-Write-Host 'Building Core 2013R2...' -ForegroundColor DarkYellow
-& C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe /define:NAV2013R2 /target:library /out:bin/debug/UncommonSense.CBreeze.2013R2.Core.dll *.cs .\Properties\AssemblyInfo.cs
+        [ValidateSet('Core','Parse','Read','Write','Automation', 'IO')]
+        [string[]]$Reference,
 
-Write-Host 'Building Core 2015...' -ForegroundColor DarkYellow
-& C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe /define:NAV2015 /define:NAV2013R2 /target:library /out:bin/debug/UncommonSense.CBreeze.2015.Core.dll *.cs .\Properties\AssemblyInfo.cs
+        [string[]]$ExternalReference,
 
-Write-Host 'Building Core 2016...' -ForegroundColor DarkYellow
-& C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe /define:NAV2016 /define:NAV2015 /define:NAV2013R2 /target:library /out:bin/debug/UncommonSense.CBreeze.2016.Core.dll *.cs .\Properties\AssemblyInfo.cs
+        [string[]]$Constant
+    )
+
+    Write-Host "Building $Assembly $Version..." -ForegroundColor DarkYellow
+
+    cd C:\Users\jhoek\GitHub\UncommonSense.CBreeze\CBreeze\UncommonSense.CBreeze.$Assembly
+    $Out = "C:\Users\jhoek\GitHub\UncommonSense.CBreeze\CBreeze\UncommonSense.CBreeze.$Assembly\bin\Debug\UncommonSense.CBreeze.$Version.$Assembly.dll"
+
+    $References = if ($Reference) { $Reference | ForEach-Object { "/reference:C:\Users\jhoek\GitHub\UncommonSense.CBreeze\CBreeze\UncommonSense.CBreeze.$_\bin\Debug\UncommonSense.CBreeze.$Version.$_.dll" } }
+    $ExternalReferences = if ($ExternalReference) { $ExternalReference | ForEach-Object { "/reference:$_" } }
+    $Constants = if ($Constant) { $Constant | ForEach-Object { "/define:$_" } }
+
+    & C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe /nologo /out:$Out /target:library $References $ExternalReferences $Constants *.cs .\Properties\AssemblyInfo.cs 
+}
+
+Clear-Host
+
+Invoke-Build -Assembly Core -Version 2013
+Invoke-Build -Assembly Core -Version 2013R2 -Constant NAV2013R2
+Invoke-Build -Assembly Core -Version 2015 -Constant NAV2013R2,NAV2015
+Invoke-Build -Assembly Core -Version 2016 -Constant NAV2013R2,NAV2015,NAV2016
+
+Invoke-Build -Assembly Parse -Version 2013 -Reference Core
+Invoke-Build -Assembly Parse -Version 2013R2 -Constant NAV2013R2 -Reference Core
+Invoke-Build -Assembly Parse -Version 2015 -Constant NAV2013R2,NAV2015 -Reference Core
+Invoke-Build -Assembly Parse -Version 2016 -Constant NAV2013R2,NAV2015,NAV2016 -Reference Core
+
+Invoke-Build -Assembly Read -Version 2013 -Reference Core,Parse
+Invoke-Build -Assembly Read -Version 2013R2 -Constant NAV2013R2 -Reference Core,Parse
+Invoke-Build -Assembly Read -Version 2015 -Constant NAV2013R2,NAV2015 -Reference Core,Parse
+Invoke-Build -Assembly Read -Version 2016 -Constant NAV2013R2,NAV2015,NAV2016 -Reference Core,Parse
+
+Invoke-Build -Assembly Write -Version 2013 -Reference Core
+Invoke-Build -Assembly Write -Version 2013R2 -Constant NAV2013R2 -Reference Core
+Invoke-Build -Assembly Write -Version 2015 -Constant NAV2013R2,NAV2015 -Reference Core
+Invoke-Build -Assembly Write -Version 2016 -Constant NAV2013R2,NAV2015,NAV2016 -Reference Core
+
+Invoke-Build -Assembly IO -Version 2013 -Reference Core,Read,Write,Parse
+Invoke-Build -Assembly IO -Version 2013R2 -Constant NAV2013R2 -Reference Core,Read,Write,Parse
+Invoke-Build -Assembly IO -Version 2015 -Constant NAV2013R2,NAV2015 -Reference Core,Read,Write,Parse
+Invoke-Build -Assembly IO -Version 2016 -Constant NAV2013R2,NAV2015,NAV2016 -Reference Core,Read,Write,Parse
+
+Invoke-Build -Assembly Automation -Version 2013 -Reference Core,Write,IO,Read,Parse -ExternalReference (SystemManagementAutomation)
+Invoke-Build -Assembly Automation -Version 2013R2 -Constant NAV2013R2 -Reference Core,Write,IO,Read,Parse -ExternalReference (SystemManagementAutomation)
+Invoke-Build -Assembly Automation -Version 2015 -Constant NAV2013R2,NAV2015 -Reference Core,Write,IO,Read,Parse -ExternalReference (SystemManagementAutomation)
+Invoke-Build -Assembly Automation -Version 2016 -Constant NAV2013R2,NAV2015,NAV2016 -Reference Core,Write,IO,Read,Parse -ExternalReference (SystemManagementAutomation)
+
+function SystemManagementAutomation()
+{
+    "C:\Users\jhoek\GitHub\UncommonSense.CBreeze\CBreeze\packages\System.Management.Automation.6.1.7601.17515\lib\net40\System.Management.Automation.dll"
+}
