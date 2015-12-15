561414eeddd011bf5dd9edafdd919dcaf672ec06
Jan Hoek <github@uncommonsense.nl>
2015-12-15 11:33:14 +0100

    Adding positional arguments

--- CBreeze/UncommonSense.CBreeze.Automation/AddCBreezeOrderBy.cs
+++ CBreeze/UncommonSense.CBreeze.Automation/AddCBreezeOrderBy.cs
@@ -17,14 +17,14 @@ namespace UncommonSense.CBreeze.Automation
             set;
         }
 
-        [Parameter(Mandatory = true)]
+        [Parameter(Mandatory = true, Position = 0)]
         public string Column
         {
             get;
             set;
         }
 
-        [Parameter()]
+        [Parameter(Position = 1)]
         public QueryOrderByDirection Direction
         {
             get;
