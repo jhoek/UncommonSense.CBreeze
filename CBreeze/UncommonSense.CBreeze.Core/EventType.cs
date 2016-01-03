f0089f0d41fe920c6a2ecc9f08502e338729345c
Jan Hoek <github@uncommonsense.nl>
2016-01-03 09:48:39 +0100

    Started work on support for NAV 2016 eventing

---
+++ CBreeze/UncommonSense.CBreeze.Core/EventType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UncommonSense.CBreeze.Core
+{
+    public enum EventType
+    {
+        No,
+        Publisher,
+        Subscriber
+    }
+}
