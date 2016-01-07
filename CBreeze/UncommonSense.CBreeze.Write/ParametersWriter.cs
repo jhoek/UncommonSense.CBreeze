5768c86674eca7fc44c50ea86fe57e3f4501825e
Jan Hoek <github@uncommonsense.nl>
2016-01-07 11:55:46 +0100

    Renamed classes

---
+++ CBreeze/UncommonSense.CBreeze.Write/ParametersWriter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UncommonSense.CBreeze.Core;
+
+namespace UncommonSense.CBreeze.Write
+{
+    public static class ParametersWriter
+    {
+        public static void Write(this Parameters parameters, CSideWriter writer)
+        {
+            foreach (var parameter in parameters)
+            {
+                parameter.Write(writer);
+
+                if (parameter != parameters.Last())
+                    writer.Write(";");
+            }
+        }
+    }
+}
---
+++ CBreeze/UncommonSense.CBreeze.Write/ReturnValueWriter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UncommonSense.CBreeze.Core;
+
+namespace UncommonSense.CBreeze.Write
+{
+    public static class ReturnValueWriter
+    {
+        public static void Write(this FunctionReturnValue functionReturnValue, CSideWriter writer)
+        {
+            if (!functionReturnValue.Type.HasValue)
+                return;
+
+            if (!string.IsNullOrEmpty(functionReturnValue.Name))
+                writer.Write(" {0}", functionReturnValue.Name);
+
+            writer.Write(" : ");
+
+            if (!string.IsNullOrEmpty(functionReturnValue.Dimensions))
+                writer.Write("ARRAY [{0}] OF ", functionReturnValue.Dimensions);
+
+            writer.Write(GetTypeName(functionReturnValue));
+        }
+
+        public static string GetTypeName(FunctionReturnValue functionReturnValue)
+        {
+            switch (functionReturnValue.Type)
+            {
+                case FunctionReturnValueType.Text:
+                case FunctionReturnValueType.Code:
+                    return functionReturnValue.DataLength.HasValue ? string.Format("{0}[{1}]", functionReturnValue.Type, functionReturnValue.DataLength) : functionReturnValue.Type.ToString();
+                case FunctionReturnValueType.Guid:
+                    return "GUID";
+                default:
+                    return functionReturnValue.Type.ToString();
+            }
+        }
+    }
+}
