b3f740586a9c9f5d208feb5969cd1a18d5ca737b
Jan Hoek <github@uncommonsense.nl>
2016-01-10 22:03:18 +0100

    Added temporary test project

---
+++ CBreeze/UncommonSense.CBreeze.Temp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UncommonSense.CBreeze.Core;
+
+namespace UncommonSense.CBreeze.Temp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var xmlport = new XmlPort(50000, "Test");
+
+            xmlport.Properties.Namespaces.Set("Foo", "Baz");
+            xmlport.Properties.Namespaces.Set("Oink", "Boink");
+
+            Console.WriteLine(xmlport.Properties.Namespaces.Count());
+            Console.WriteLine(xmlport.Properties.Namespaces["Foo"]);
+        }
+    }
+}
