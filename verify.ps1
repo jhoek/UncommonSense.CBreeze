ba66fb84d8413b91f2f424a25e667496c8196dd8
Jan Hoek <github@uncommonsense.nl>
2016-01-13 20:43:46 +0100

    Adding verification script
    Run shell per version for best results

---
+++ verify.ps1
@@ -0,0 +1,37 @@
+﻿function Verify-Build
+{
+    Param
+    (
+        [Parameter(Mandatory)]
+        [ValidateSet('2013', '2013R2', '2015', '2016')]
+        [string]$Version
+    )
+
+    Add-Type -Path "c:\users\jhoek\GitHub\UncommonSense.CBreeze\CBreeze\UncommonSense.CBreeze.Core\bin\Debug\UncommonSense.CBreeze.$Version.Core.dll"
+    Add-Type -Path "c:\users\jhoek\GitHub\UncommonSense.CBreeze\CBreeze\UncommonSense.CBreeze.Parse\bin\Debug\UncommonSense.CBreeze.$Version.Parse.dll"
+    Add-Type -Path "C:\Users\jhoek\GitHub\UncommonSense.CBreeze\CBreeze\UncommonSense.CBreeze.Read\bin\Debug\UncommonSense.CBreeze.$Version.Read.dll"
+    Add-Type -Path "C:\Users\jhoek\GitHub\UncommonSense.CBreeze\CBreeze\UncommonSense.CBreeze.Write\bin\Debug\UncommonSense.CBreeze.$Version.Write.dll"
+
+    $InputFileName = "$Home\Desktop\nl$Version.txt"
+    $OutputFileName = "$Home\Desktop\nl$Version.output.txt"
+    
+    $Reading = Measure-Command { $Lines = [System.IO.File]::ReadLines($InputFileName, [System.Text.Encoding]::GetEncoding('ibm850')) }
+    $Parsing = Measure-Command { $Application = [UncommonSense.CBreeze.Read.ApplicationBuilder]::FromLines($Lines) }
+    $Writing = Measure-Command { [UncommonSense.CBreeze.Write.ApplicationWriter]::Write($Application, $OutputFileName) }
+
+    $Result = [Ordered]@{}
+    $Result.Version = $Version
+    $Result.Reading = $Reading.TotalMilliseconds
+    $Result.Parsing = $Parsing.TotalMilliseconds
+    $Result.Writing = $Writing.TotalMilliseconds
+    $Result
+
+    # Start-Process -FilePath 'C:\Program Files\Araxis\Araxis Merge\Compare.exe' -ArgumentList $InputFileName, $OutputFileName
+}
+
+Clear-Host
+
+#Verify-Build -Version 2013 | Format-Table
+#Verify-Build -Version 2013R2 | Format-Table
+#Verify-Build -Version 2015 | Format-Table 
+Verify-Build -Version 2016 | Format-Table
