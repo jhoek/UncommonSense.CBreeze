3c71e7c5c39d3c43a67f61a1365b4edf82b9beda
Jan Hoek <github@uncommonsense.nl>
2016-08-16 15:41:10 +0200

    If IndentationLevel = null, assume zero in calculations

--- CBreeze/UncommonSense.CBreeze.Core/PageControl.cs
+++ CBreeze/UncommonSense.CBreeze.Core/PageControl.cs
@@ -6,85 +6,85 @@ using UncommonSense.CBreeze.Common;
 
 namespace UncommonSense.CBreeze.Core
 {
-		public abstract class PageControl : KeyedItem<int>, IHasName, IHasProperties
-	{
-		internal PageControl(int id, int? indentationLevel)
-		{
-			ID = id;
-			IndentationLevel = indentationLevel;
-		}
+    public abstract class PageControl : KeyedItem<int>, IHasName, IHasProperties
+    {
+        internal PageControl(int id, int? indentationLevel)
+        {
+            ID = id;
+            IndentationLevel = indentationLevel;
+        }
 
-		public virtual PageControls Container
-		{
-			get;
-			internal set;
-		}
+        public virtual PageControls Container
+        {
+            get;
+            internal set;
+        }
 
-		public abstract PageControlType Type
-		{
-			get;
-		}
+        public abstract PageControlType Type
+        {
+            get;
+        }
 
-		public int? IndentationLevel
-		{
-			get;
-			protected set;
-		}
+        public int? IndentationLevel
+        {
+            get;
+            protected set;
+        }
 
-		public abstract string GetName();
+        public abstract string GetName();
 
-		public abstract Properties AllProperties
-		{
-			get;
-		}
+        public abstract Properties AllProperties
+        {
+            get;
+        }
 
-		public int Index
-		{
-			get
-			{
-				return Container.IndexOf(this);
-			}
-		}
+        public int Index
+        {
+            get
+            {
+                return Container.IndexOf(this);
+            }
+        }
 
-		public IEnumerable<PageControl> DescendantPageControls
-		{
-			get
-			{
-				return Container.Skip(Index + 1).TakeWhile(c => c.IndentationLevel > IndentationLevel);
-			}
-		}
+        public IEnumerable<PageControl> DescendantPageControls
+        {
+            get
+            {
+                return Container.Skip(Index + 1).TakeWhile(c => c.IndentationLevel > (IndentationLevel ?? 0));
+            }
+        }
 
-		public IEnumerable<PageControl> ChildPageControls
-		{
-			get
-			{
-				return DescendantPageControls.Where(c => c.IndentationLevel == IndentationLevel + 1);
-			}
-		}
+        public IEnumerable<PageControl> ChildPageControls
+        {
+            get
+            {
+                return DescendantPageControls.Where(c => c.IndentationLevel == (IndentationLevel ?? 0) + 1);
+            }
+        }
 
-		public PageControl ParentPageControl
-		{
-			get
-			{
-				return Container.Where(c => c.IndentationLevel == IndentationLevel - 1).Where(c => c.Index < Index).Last();
-			}
-		}
+        public PageControl ParentPageControl
+        {
+            get
+            {
+                return Container.Where(c => c.IndentationLevel == (IndentationLevel ?? 0) - 1).Where(c => c.Index < Index).Last();
+            }
+        }
 
-		public T AddChildPageControl<T>(T child, Position position) where T : PageControl
-		{
-			switch (position)
-			{
-				case Position.FirstWithinContainer:
-					Container.Insert(Index + 1, child);
-					break;
-				case Position.LastWithinContainer:
-					var descendantPageControls = DescendantPageControls;
-					var lastIndex = descendantPageControls.Any() ? descendantPageControls.Last().Index : Index;
-					Container.Insert(lastIndex + 1, child);
-					break;
-			}
+        public T AddChildPageControl<T>(T child, Position position) where T : PageControl
+        {
+            switch (position)
+            {
+                case Position.FirstWithinContainer:
+                    Container.Insert(Index + 1, child);
+                    break;
+                case Position.LastWithinContainer:
+                    var descendantPageControls = DescendantPageControls;
+                    var lastIndex = descendantPageControls.Any() ? descendantPageControls.Last().Index : Index;
+                    Container.Insert(lastIndex + 1, child);
+                    break;
+            }
 
-			return child;
-		}
-	}
+            return child;
+        }
+    }
 }
