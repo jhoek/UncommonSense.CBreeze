54d71f4b05a762b28680590d3b1ee7cada39d38c
Jan Hoek <github@uncommonsense.nl>
2016-10-27 13:01:56 +0200

    Override of ToString(): add data length for binary, code and text fields

--- CBreeze/UncommonSense.CBreeze.Core/TextTableField.cs
+++ CBreeze/UncommonSense.CBreeze.Core/TextTableField.cs
@@ -23,6 +23,11 @@ namespace UncommonSense.CBreeze.Core
             }
         }
 
+        public override string ToString()
+        {
+            return string.Format("{0}[{1}]", base.ToString(), DataLength);
+        }
+
         public int DataLength
         {
             get;
