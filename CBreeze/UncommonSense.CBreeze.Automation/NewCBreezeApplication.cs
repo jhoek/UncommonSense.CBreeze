3faff1373d456a013b6929d0d801a33db1d9b4af
Jan Hoek <github@uncommonsense.nl>
2016-11-23 22:24:28 +0100

    NewCBreezeApplication now accepts a scriptblock for defining its contents

--- CBreeze/UncommonSense.CBreeze.Automation/NewCBreezeApplication.cs
+++ CBreeze/UncommonSense.CBreeze.Automation/NewCBreezeApplication.cs
@@ -10,9 +10,23 @@ namespace UncommonSense.CBreeze.Automation
     [Cmdlet(VerbsCommon.New, "CBreezeApplication")]
     public class NewCBreezeApplication : Cmdlet
     {
+        [Parameter(Position=0)]
+        public ScriptBlock Objects
+        {
+            get;
+            set;
+        }
+
         protected override void EndProcessing()
         {
-            WriteObject(new Application());
+            var application = new Application();
+
+            if (Objects != null)
+            {
+                application.Add(Objects.Invoke().Select(o=>o.BaseObject).Cast<Core.Object>());
+            }
+
+            WriteObject( application);
         }
     }
 }
