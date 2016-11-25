1ca52763e983ea2bb61a7df0b448798d9a209e6c
Jan Hoek <github@uncommonsense.nl>
2016-11-25 16:29:49 +0100

    Added default parameter set

--- CBreeze/UncommonSense.CBreeze.Automation/AddCBreezeXmlPort.cs
+++ CBreeze/UncommonSense.CBreeze.Automation/AddCBreezeXmlPort.cs
@@ -7,7 +7,7 @@ using UncommonSense.CBreeze.Core;
 
 namespace UncommonSense.CBreeze.Automation
 {
-    [Cmdlet(VerbsCommon.Add, "CBreezeXmlPort")]
+    [Cmdlet(VerbsCommon.Add, "CBreezeXmlPort", DefaultParameterSetName = "ManualObjectProperties")]
     public class AddCBreezeXmlPort : NewCBreezeXmlPort
     {
         public AddCBreezeXmlPort()
