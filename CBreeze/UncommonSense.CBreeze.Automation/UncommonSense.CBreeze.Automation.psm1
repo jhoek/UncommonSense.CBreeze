8f9e7428a9761b580e2478d14f88b8f258109413
Jan Hoek <github@uncommonsense.nl>
2016-12-26 17:20:00 +0100

    Add aliases for New-CBreezeTableKey and New-CBreezeParameter

--- CBreeze/UncommonSense.CBreeze.Automation/UncommonSense.CBreeze.Automation.psm1
+++ CBreeze/UncommonSense.CBreeze.Automation/UncommonSense.CBreeze.Automation.psm1
@@ -53,7 +53,9 @@ Set-Alias -Name Codeunit -Value New-CBreezeCodeunit
 Set-Alias -Name Query -Value New-CBreezeQuery
 Set-Alias -Name MenuSuite -Value New-CBreezeMenuSuite
 Set-Alias -Name Field -Value New-CBreezeTableField
+Set-Alias -Name Key -Value New-CBreezeTableKey
 Set-Alias -Name Procedure -Value New-CBreezeFunction
+Set-Alias -Name Parameter -Value New-CBreezeParameter
 Set-Alias -Name Variable -Value New-CBreezeVariable
 
 # Export members
