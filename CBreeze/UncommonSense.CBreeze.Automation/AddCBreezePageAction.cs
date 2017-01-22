de03a75f0e1fc3fa322bd0826e928b44abdb2c6c
Jan Hoek <github@uncommonsense.nl>
2017-01-22 18:06:59 +0100

    Preparations for AddCBreezePageAction* derived from NewCBreezePageAction*

--- CBreeze/UncommonSense.CBreeze.Automation/AddCBreezePageAction.cs
+++ CBreeze/UncommonSense.CBreeze.Automation/AddCBreezePageAction.cs
@@ -12,11 +12,6 @@ namespace UncommonSense.CBreeze.Automation
     [Cmdlet(VerbsCommon.Add, "CBreezePageAction")]
     public class AddCBreezePageAction : NewCBreezePageAction
     {
-        public AddCBreezePageAction()
-        {
-            Position = new DynamicParameter<Position?>("Position");
-        }
-
         [Parameter(Mandatory = true, ValueFromPipeline = true)]
         public PSObject InputObject
         {
@@ -39,16 +34,25 @@ namespace UncommonSense.CBreeze.Automation
             set;
         }
 
-        protected DynamicParameter<Position?> Position
+        [Parameter()]
+        public Position? Position
         {
             get;
             set;
         }
 
+        protected override int GetID()
+        {
+            var page = InputObject.GetIPage();
+            return ID.GetID(page.GetPageUIDsInUse(), page.ObjectID);
+        }
+
+        protected override int GetIndentation() => GetParentIndentationLevel() + 1;
+
         protected override void ProcessRecord()
         {
-            var pageAction = CreatePageAction(GetID(), GetParentIndentationLevel() + 1);
-            var position = Position.Value.GetValueOrDefault(Core.Position.LastWithinContainer);
+            var pageAction = CreatePageAction();
+            var position = Position.GetValueOrDefault(Core.Position.LastWithinContainer);
 
             TypeSwitch.Do(
                 InputObject.BaseObject,
@@ -80,67 +84,6 @@ namespace UncommonSense.CBreeze.Automation
 
             return result;
         }
-
-        protected int GetID()
-        {
-            var page = InputObject.GetIPage();
-            return ID.GetID(page.GetPageUIDsInUse(), page.ObjectID);
-        }
-
-        public override IEnumerable<RuntimeDefinedParameter> DynamicParameters
-        {
-            get
-            {
-                switch (Type.Value)
-                {
-                    case PageActionBaseType.ActionContainer:
-                        yield return ContainerType.RuntimeDefinedParameter;
-                        yield return Description.RuntimeDefinedParameter;
-                        yield return Name.RuntimeDefinedParameter;
-                        break;
-
-                    case PageActionBaseType.ActionGroup:
-                        yield return Position.RuntimeDefinedParameter;
-                        yield return Caption.RuntimeDefinedParameter;
-                        yield return Description.RuntimeDefinedParameter;
-                        yield return Enabled.RuntimeDefinedParameter;
-                        yield return Image.RuntimeDefinedParameter;
-                        yield return Name.RuntimeDefinedParameter;
-                        yield return Visible.RuntimeDefinedParameter;
-                        break;
-
-                    case PageActionBaseType.Action:
-                        yield return Position.RuntimeDefinedParameter;
-                        yield return Caption.RuntimeDefinedParameter;
-                        yield return Description.RuntimeDefinedParameter;
-                        yield return Ellipsis.RuntimeDefinedParameter;
-                        yield return Enabled.RuntimeDefinedParameter;
-                        yield return Image.RuntimeDefinedParameter;
-                        yield return InFooterBar.RuntimeDefinedParameter;
-                        yield return Name.RuntimeDefinedParameter;
-                        yield return Promoted.RuntimeDefinedParameter;
-                        yield return PromotedCategory.RuntimeDefinedParameter;
-                        yield return PromotedIsBig.RuntimeDefinedParameter;
-                        yield return RunObjectType.RuntimeDefinedParameter;
-                        yield return RunObjectID.RuntimeDefinedParameter;
-                        yield return RunPageMode.RuntimeDefinedParameter;
-                        yield return RunPageOnRec.RuntimeDefinedParameter;
-                        yield return RunPageViewKey.RuntimeDefinedParameter;
-                        yield return RunPageViewOrder.RuntimeDefinedParameter;
-#if NAV2015
-                        yield return Scope.RuntimeDefinedParameter;
-#endif
-                        yield return ShortcutKey.RuntimeDefinedParameter;
-                        yield return Visible.RuntimeDefinedParameter;
-                        break;
-
-                    case PageActionBaseType.Separator:
-                        yield return Position.RuntimeDefinedParameter;
-                        yield return Caption.RuntimeDefinedParameter;
-                        yield return IsHeader.RuntimeDefinedParameter;
-                        break;
-                }
-            }
-        }
     }
 }
+
