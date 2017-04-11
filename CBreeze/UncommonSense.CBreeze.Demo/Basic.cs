fb4cc206c4d8b39abb26fae68af9252784da1af4
Jan Hoek <github@uncommonsense.nl>
2017-04-11 22:01:58 +0200

    Testing with generated documentation

---
+++ CBreeze/UncommonSense.CBreeze.Demo/Basic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UncommonSense.CBreeze.Core;
+
+namespace UncommonSense.CBreeze.Demo
+{
+    // # Basic
+    public static class Basic
+    {
+        // ## Adding a table
+        public static void AddingATable()
+        {
+            var application = new Application();
+            var table = application.Tables.Add(new Table(50000, "My Table"));
+        }
+
+        // ## Adding a page
+        public static void AddingAPage()
+        {
+            var application = new Application();
+            var page = application.Pages.Add(new Page(50000, "My Page"));
+        }
+    }
+}
---
+++ howto.ps1
@@ -0,0 +1,22 @@
+﻿$DemoPath = Join-Path -Path $PSScriptRoot -ChildPath .\CBreeze\UncommonSense.CBreeze.Demo
+
+Get-ChildItem -Path $DemoPath -Filter *.cs |
+    Where-Object Name -ne Program.cs |
+    ForEach-Object { 
+        $FileName = $_.FullName
+
+        Get-Content -Path $FileName |
+        Where-Object { $_ -notmatch '^using' } |
+        Where-Object { $_ -notmatch '^namespace' } |
+        Where-Object { $_ -notmatch '^\s*\{' } | 
+        Foreach-Object { if ($_ -match '^\s{8}}') { '```' } else { $_} } |
+        Where-Object { $_ -notmatch '^\s*}' } | 
+        Where-Object { $_ -notmatch '^\s*public static class' } | 
+        ForEach-Object { if ($_ -match '^\s*public static void') { '```c#' } else { $_ } } |
+        ForEach-Object { $_ -replace '^\s*//', '' } | 
+        Set-Content -Path ($FileName + '.md')
+    }
+
+
+    
+    
