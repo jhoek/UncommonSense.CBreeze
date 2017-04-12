221ba8bf79e32ec594e03adef5276ed90e7cdfb1
Jan Hoek <github@uncommonsense.nl>
2017-04-12 11:07:20 +0200

    Test with XMLports

--- CBreeze/UncommonSense.CBreeze.Demo/Program.cs
+++ CBreeze/UncommonSense.CBreeze.Demo/Program.cs
@@ -17,100 +17,14 @@ namespace UncommonSense.CBreeze.Demo
             DefaultRanges.UID = Enumerable.Range(1000000000, 100);
 
             var application = new Application();
+            var xmlport = application.XmlPorts.Add(new XmlPort("My XMLport"));
 
-            //var table = SetupTable(application.Tables.Add(new Table("MyTable")));
-            //var table2 = SetupTable(application.Tables.Add(new Table(1, "MyOtherTable")));
+            xmlport.Nodes.Add(new XmlPortTableElement("Customer")).Properties.SourceTable = BaseApp.TableIDs.Customer;
+            xmlport.Nodes.Add(new XmlPortFieldElement("No", 1));
+            xmlport.Nodes.Add(new XmlPortFieldElement("Balance", 1));
+            xmlport.Nodes.Add(new XmlPortTextAttribute("Currency", 2));
 
-            var page = SetupPage(application.Pages.Add(new Page("MyPage")));
-
-            //var report = SetupReport(application.Reports.Add(new Report("MyReport")));
-            //var xmlPort = SetupXmlPort(application.XmlPorts.Add(new XmlPort("MyXmlPort")));
-            //var query = SetupQuery(application.Queries.Add(new Query("MyQuery")));
-            //var codeunit = SetupCodeunit(application.Codeunits.Add(new Codeunit("MyCodeunit")));
-            //var menuSuite = SetupMenuSuite(application.MenuSuites.Add(new MenuSuite("MyMenuSuite")));
-
-            // FIXME: ParentID in zelfde range => dan nummeren van 1
             application.Write();
-        }
-
-        private static Codeunit SetupCodeunit(Codeunit codeunit)
-        {
-            codeunit.Code.Variables.Add(new ActionVariable("MyActionVariable"));
-            return codeunit;
-        }
-
-        private static MenuSuite SetupMenuSuite(MenuSuite menuSuite)
-        {
-            return menuSuite;
-        }
-
-        private static Page SetupPage(Page page)
-        {
-            page.Actions.Add(new PageActionContainer());
-            page.Actions.Add(new PageActionGroup(indentationLevel: 1, caption: "FOo"));
-            page.Actions.Add(new PageAction(caption: "Oink", indentationLevel: 2));
-            page.Actions.Add(new PageActionSeparator(indentationLevel: 2));
-
-            page.Controls.Add(new ContainerPageControl());
-            page.Controls.Add(new GroupPageControl(indentationLevel: 1, groupType: GroupType.Repeater));
-            page.Controls.Add(new FieldPageControl("MyField", indentationLevel: 2));
-
-            var function = page.Code.Functions.Add(new Function(0, "Foo"));
-            function.Parameters.Add(new IntegerParameter("MyParam"));
-            function.Parameters.Add(new BigTextParameter("MyBigText"));
-            function.Parameters.Add(new TextParameter("MyText", dataLength: 80, var: true));
-            function.Variables.Add(new IntegerVariable(0, "Oink"));
-            function.Variables.Add(new IntegerVariable(0, "Boink"));
-
-            var function2 = page.Code.Functions.Add(new Function("Baz"));
-
-            return page;
-        }
-
-        private static Query SetupQuery(Query query)
-        {
-            query.Elements.Add(new DataItemQueryElement(BaseApp.TableIDs.Customer));
-            query.Elements.Add(new FilterQueryElement("No.", indentationLevel: 1));
-            query.Elements.Add(new ColumnQueryElement("Name", indentationLevel: 1));
-
-            return query;
-        }
-
-        private static Report SetupReport(Report report)
-        {
-            report
-                .Labels
-                .Add(new ReportLabel("MyLabel"))
-                .Properties
-                .CaptionML
-                .Set("ENU", "Foo")
-                .Set("NLD", "Baz");
-
-            report.Elements.Add(new DataItemReportElement(BaseApp.TableIDs.Customer));
-            report.Elements.Add(new ColumnReportElement("No", "No."));
-
-            return report;
-        }
-
-        private static Table SetupTable(Table table)
-        {
-            table.Fields.Add(new IntegerTableField("Foo"));
-            table.Fields.Add(new CodeTableField("Baz"));
-
-            table.Keys.Add(new TableKey("Foo"));
-
-            table.FieldGroups.Add(new TableFieldGroup(TableFieldGroup.DropDown, "Foo", "Baz"));
-            table.FieldGroups.Add(new TableFieldGroup(TableFieldGroup.Brick, "Oink", "BOink"));
-
-            var function = table.Code.Functions.Add(new Function("MyFunction"));
-            function.Parameters.Add(new TextParameter("MyTextParameter", true));
-
-            return table;
-        }
-
-        private static XmlPort SetupXmlPort(XmlPort xmlPort)
-        {
-            return xmlPort;
         }
     }
 }
