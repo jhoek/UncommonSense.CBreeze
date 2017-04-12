15537e4d0dbd35f7c0e9eff9f817a82477df2884
Jan Hoek <github@uncommonsense.nl>
2017-04-12 09:23:13 +0200

    Added more demo functions

--- CBreeze/UncommonSense.CBreeze.Demo/Basic.cs
+++ CBreeze/UncommonSense.CBreeze.Demo/Basic.cs
@@ -3,12 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UncommonSense.CBreeze.Core;
+using UncommonSense.CBreeze.Read;
 
 namespace UncommonSense.CBreeze.Demo
 {
     // # Basic operations
     public static class Basic
     {
+        // ## Creating a new application
+        public static void _00100CreatingAnApplication()
+        {
+            var application = new Application();
+        }
+
+        // ## Initializing an application with objects
+        public static void _00200InitializingAnApplicationWithObjects()
+        {
+            var application = new Application(
+                new Table(50000, "My Table"),
+                new Table(50001, "My Other Table"),
+                new Page(50000, "My Page"),
+                new Codeunit(50000, "My Codeunit"));
+        }
+
+        // ## Importing an existing application from a single disk file
+        public static void _00300ImportingAnExistingApplicationFromASingleDiskFile()
+        {
+            var application = ApplicationBuilder.FromFile("mytest.txt");
+        }
+
         // ## Adding a table
         public static void _01000AddingATable()
         {
