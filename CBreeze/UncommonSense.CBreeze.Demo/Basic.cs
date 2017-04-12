980ee256e785d3dc2a3919bff1f72b009ecb7abd
Jan Hoek <github@uncommonsense.nl>
2017-04-12 08:44:19 +0200

    Number samples to keep sort order (even when using e.g. CodeMaid); added another sample

--- CBreeze/UncommonSense.CBreeze.Demo/Basic.cs
+++ CBreeze/UncommonSense.CBreeze.Demo/Basic.cs
@@ -6,18 +6,26 @@ using UncommonSense.CBreeze.Core;
 
 namespace UncommonSense.CBreeze.Demo
 {
-    // # Basic
+    // # Basic operations
     public static class Basic
     {
         // ## Adding a table
-        public static void AddingATable()
+        public static void _01000AddingATable()
         {
             var application = new Application();
             var table = application.Tables.Add(new Table(50000, "My Table"));
         }
 
+        // ## Adding a table field
+        public static void _01100AddingATableField()
+        {
+            var application = new Application();
+            var table = application.Tables.Add(new Table(50000, "My Table"));
+            var field = table.Fields.Add(new IntegerTableField(1, "Entry No."));
+        }
+
         // ## Adding a page
-        public static void AddingAPage()
+        public static void _02000AddingAPage()
         {
             var application = new Application();
             var page = application.Pages.Add(new Page(50000, "My Page"));
