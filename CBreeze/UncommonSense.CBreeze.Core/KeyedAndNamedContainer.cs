72f67ad409cfe6d37582e72305b25fb2344f522b
Jan Hoek <github@uncommonsense.nl>
2017-04-12 10:58:47 +0200

    Unique name check for KeyedAndNamedContainer should ignore name case

--- CBreeze/UncommonSense.CBreeze.Core/KeyedAndNamedContainer.cs
+++ CBreeze/UncommonSense.CBreeze.Core/KeyedAndNamedContainer.cs
@@ -6,39 +6,40 @@ using System.Threading.Tasks;
 
 namespace UncommonSense.CBreeze.Core
 {
-	public abstract class KeyedAndNamedContainer<TKey, TItem> : KeyedContainer<TKey, TItem>
-		where TItem : KeyedItem<TKey>, IHasName
+    public abstract class KeyedAndNamedContainer<TKey, TItem> : KeyedContainer<TKey, TItem>
+        where TItem : KeyedItem<TKey>, IHasName
         where TKey : struct
-	{
-		protected override void InsertItem(int index, TItem item)
-		{
-			ValidateName(item);
-			base.InsertItem(index, item);
-		}
+    {
+        protected override void InsertItem(int index, TItem item)
+        {
+            ValidateName(item);
+            base.InsertItem(index, item);
+        }
 
-		public TItem this [string name]
-		{
-			get
-			{
-				return this.Items.FirstOrDefault(i => i.GetName() == name);
-			}
-		}
+        public TItem this[string name]
+        {
+            get
+            {
+                return this.Items.FirstOrDefault(i => i.GetName() == name);
+            }
+        }
 
-		public abstract void ValidateName(TItem item);
+        public abstract void ValidateName(TItem item);
 
-		protected void TestNameNotNullOrEmpty(TItem item)
-		{
-			if (string.IsNullOrEmpty(item.GetName()))
-				throw new ArgumentOutOfRangeException("Collection items must have a name.");
-		}
+        protected void TestNameNotNullOrEmpty(TItem item)
+        {
+            if (string.IsNullOrEmpty(item.GetName()))
+                throw new ArgumentOutOfRangeException("Collection items must have a name.");
+        }
 
-		protected void TestNameUnique(TItem item)
-		{
-			if (!string.IsNullOrEmpty(item.GetName()))
-			{
-				if (this.Any(i => i.GetName() == item.GetName()))
-					throw new ArgumentOutOfRangeException("Collection item names must be unique.");
-			}
-		}
-	}
+        protected void TestNameUnique(TItem item)
+        {
+            if (!string.IsNullOrEmpty(item.GetName()))
+            {
+                if (this.Any(i => i.GetName().Equals(item.GetName(), StringComparison.InvariantCultureIgnoreCase)))
+
+                    throw new ArgumentOutOfRangeException("Collection item names must be unique.");
+            }
+        }
+    }
 }
