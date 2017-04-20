1177087c2af03ee20cbaa8d7a71690a2ee5d939c
Jan Hoek <github@uncommonsense.nl>
2017-04-20 13:13:44 +0200

    Default (U)ID ranges; match file name with class name

--- CBreeze/UncommonSense.CBreeze.Core/DefaultRanges.cs
+++ CBreeze/UncommonSense.CBreeze.Core/DefaultRanges.cs
@@ -7,6 +7,12 @@ namespace UncommonSense.CBreeze.Core
 {
     public static class DefaultRanges
     {
+        static DefaultRanges()
+        {
+            ID = Enumerable.Range(50000, 1000);
+            UID = Enumerable.Range(1000000000, 1000);
+        }
+
         public static IEnumerable<int> ID { get; set; }
         public static IEnumerable<int> UID { get; set; }
     }
