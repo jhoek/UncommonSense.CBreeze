e1524e992141d5ae3bc88b5805d274c4fd3b8157
Jan Hoek <github@uncommonsense.nl>
2017-04-24 11:25:59 +0200

    Get-HelpAsMarkdown is now a proper module; no need to dot-source it. Updated README.md.

--- CBreeze/UncommonSense.CBreeze.Automation/Update-README.ps1
+++ CBreeze/UncommonSense.CBreeze.Automation/Update-README.ps1
@@ -1,7 +1,5 @@
-﻿. C:\users\jhoek\GitHub\Scripts\Get-HelpAsMarkDown.ps1
-Set-Location C:\Users\jhoek\Documents\WindowsPowerShell\Modules\UncommonSense.CBreeze.Automation
+﻿Import-Module UncommonSense.CBreeze.Automation -Force
 
-Import-Module UncommonSense.CBreeze.Automation -Force
 Get-Command `
     -Module UncommonSense.CBreeze.Automation | `
     Sort-Object -Property Noun,Verb | `
