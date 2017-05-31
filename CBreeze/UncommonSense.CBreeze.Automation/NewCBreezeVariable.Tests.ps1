3bbd226a8ca3e6b39fc2f6e3b1eb5756e2189cd4
Jan Hoek <github@uncommonsense.nl>
2017-05-31 13:02:08 +0200

    First check-in for generated New-CBreezeVariable cmdlets

---
+++ CBreeze/UncommonSense.CBreeze.Automation/NewCBreezeVariable.Tests.ps1
@@ -0,0 +1,10 @@
+﻿Describe "New-CBreezeVariable" {
+    It "Correctly creates variables" {
+        $Codeunit = Codeunit Test {
+            ActionVariable MyActionVariable -Dimensions '1,2,3'
+            AutomationVariable MyAutomationVariable MySubType -Dimensions '1,2,3'
+        } 
+
+        Write-Host ($Codeunit | Export-CBreezeApplication)
+    }
+}
