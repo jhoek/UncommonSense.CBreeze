7a4bd52e6ebc13be731589c71617ece87c5aec0f
Jan Hoek <github@uncommonsense.nl>
2017-07-05 12:27:07 +0200

    More work on New-CBreezeQueryElement

--- CBreeze/UncommonSense.CBreeze.Automation/NewCBreezeQueryElement.cs
+++ CBreeze/UncommonSense.CBreeze.Automation/NewCBreezeQueryElement.cs
@@ -3,102 +3,104 @@ using System.Collections.Generic;
 using System.Management.Automation;
 using UncommonSense.CBreeze.Core;
 
-namespace UncommonSense.CBreeze.Automation 
+namespace UncommonSense.CBreeze.Automation
 {
-	// FIXME: Combine methods into one property
+    // FIXME: Combine methods into one property
 
-	[Cmdlet(VerbsCommon.New, "ColumnQueryElement")]
-	[OutputType(typeof(QueryElement))]
-	[Alias("Column")]
-	public class NewCBreezeColumnQueryElement : NewItemWithIDCmdlet<QueryElement, int, PSObject>
-	{
-		protected override IEnumerable<QueryElement> CreateItems()
-		{
-			// FIXME
-			throw new NotImplementedException();
-		}
+    [Cmdlet(VerbsCommon.New, "ColumnQueryElement")]
+    [OutputType(typeof(QueryElement))]
+    [Alias("Column")]
+    public class NewCBreezeColumnQueryElement : NewItemWithIDCmdlet<QueryElement, int, PSObject>
+    {
+        protected override void AddItemToInputObject(QueryElement item, PSObject InputObject)
+        {
+            // FIXME
+        }
 
-		protected override void AddItemToInputObject(QueryElement item, PSObject InputObject)
-		{
-			// FIXME
-		}
+        protected override IEnumerable<QueryElement> CreateItems()
+        {
+            var columnQueryElement = new ColumnQueryElement();
 
-	[Parameter()]
-	public String DataSource { get; set; }
+            return columnQueryElement;
+        }
 
-	[Parameter()]
-	public Nullable<DateMethod> DateMethod { get; set; }
+        [Parameter()]
+        public String DataSource { get; set; }
 
-	[Parameter()]
-	public String Description { get; set; }
+        [Parameter()]
+        public String Description { get; set; }
 
-	[Parameter()]
-	public Nullable<MethodType> MethodType { get; set; }
+        [Parameter()]
+        [ValidateSet("Day", "Month", "Year", "Sum", "Count", "Average", "Min", "Max")]
+        public string Method { get; set; }
 
-	[Parameter()]
-	public Nullable<Boolean> ReverseSign { get; set; }
+        [Parameter()]
+        public string Name { get; set; }
 
-	[Parameter()]
-	public Nullable<TotalsMethod> TotalsMethod { get; set; }
+        [Parameter()]
+        public Nullable<Boolean> ReverseSign { get; set; }
+    }
 
-	}
+    [Cmdlet(VerbsCommon.New, "DataItemQueryElement")]
+    [OutputType(typeof(QueryElement))]
+    [Alias("DataItem")]
+    public class NewCBreezeDataItemQueryElement : NewItemWithIDCmdlet<QueryElement, int, PSObject>
+    {
+        protected override void AddItemToInputObject(QueryElement item, PSObject InputObject)
+        {
+            // FIXME
+        }
 
-	[Cmdlet(VerbsCommon.New, "DataItemQueryElement")]
-	[OutputType(typeof(QueryElement))]
-	[Alias("DataItem")]
-	public class NewCBreezeDataItemQueryElement : NewItemWithIDCmdlet<QueryElement, int, PSObject>
-	{
-		protected override IEnumerable<QueryElement> CreateItems()
-		{
-			// FIXME
-			throw new NotImplementedException();
-		}
+        protected override IEnumerable<QueryElement> CreateItems()
+        {
+            var dataItemQueryElement = new DataItemQueryElement();
 
-		protected override void AddItemToInputObject(QueryElement item, PSObject InputObject)
-		{
-			// FIXME
-		}
+            return dataItemQueryElement;
+        }
 
-	[Parameter()]
-	public Nullable<DataItemLinkType> DataItemLinkType { get; set; }
+        [Parameter()]
+        public Nullable<DataItemLinkType> DataItemLinkType { get; set; }
 
-	[Parameter(Mandatory=true)]
-	public Nullable<Int32> DataItemTable { get; set; }
+        [Parameter(Mandatory = true)]
+        public Nullable<Int32> DataItemTable { get; set; }
 
-	[Parameter()]
-	public DataItemQueryElementTableFilter DataItemTableFilter { get; set; }
+        [Parameter()]
+        public DataItemQueryElementTableFilter DataItemTableFilter { get; set; }
 
-	[Parameter()]
-	public String Description { get; set; }
+        [Parameter()]
+        public String Description { get; set; }
 
-	[Parameter()]
-	public Nullable<SqlJoinType> SQLJoinType { get; set; }
+        [Parameter()]
+        public string Name { get; set; }
 
-	}
+        [Parameter()]
+        public Nullable<SqlJoinType> SQLJoinType { get; set; }
+    }
 
-	[Cmdlet(VerbsCommon.New, "FilterQueryElement")]
-	[OutputType(typeof(QueryElement))]
-	[Alias("Filter")]
-	public class NewCBreezeFilterQueryElement : NewItemWithIDCmdlet<QueryElement, int, PSObject>
-	{
-		protected override IEnumerable<QueryElement> CreateItems()
-		{
-			// FIXME
-			throw new NotImplementedException();
-		}
+    [Cmdlet(VerbsCommon.New, "FilterQueryElement")]
+    [OutputType(typeof(QueryElement))]
+    [Alias("Filter")]
+    public class NewCBreezeFilterQueryElement : NewItemWithIDCmdlet<QueryElement, int, PSObject>
+    {
+        protected override void AddItemToInputObject(QueryElement item, PSObject InputObject)
+        {
+            // FIXME
+        }
 
-		protected override void AddItemToInputObject(QueryElement item, PSObject InputObject)
-		{
-			// FIXME
-		}
+        protected override IEnumerable<QueryElement> CreateItems()
+        {
+            var filterQueryElement = new FilterQueryElement();
 
-	[Parameter()]
-	public String DataSource { get; set; }
+            return filterQueryElement;
+        }
 
-	[Parameter()]
-	public String Description { get; set; }
+        [Parameter()]
+        public String DataSource { get; set; }
 
-	}
+        [Parameter()]
+        public String Description { get; set; }
 
+        [Parameter()]
+        public string Name { get; set; }
+    }
 }
-
