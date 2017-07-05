e92ce70a4078191a5617dddbd65f20e8fd394f58
Jan Hoek <github@uncommonsense.nl>
2017-07-05 17:02:23 +0200

    Argument completers for TableName

--- CBreeze/UncommonSense.CBreeze.Automation/UncommonSense.CBreeze.Automation.psm1
+++ CBreeze/UncommonSense.CBreeze.Automation/UncommonSense.CBreeze.Automation.psm1
@@ -1,1 +1,23 @@
-﻿Export-ModuleMember -Alias * -Function * -Cmdlet *
+﻿Export-ModuleMember -Alias * -Function *-* -Cmdlet *
+
+function TableNameCompleter {
+    param
+    (
+        $commandName, 
+        $parameterName, 
+        $wordToComplete, 
+        $commandAst, 
+        $fakeBoundParameter
+    )
+
+    Write-Host $wordToComplete
+
+    [UncommonSense.CBreeze.Core.BaseApp+TableNames].GetFields('Static,Public') | 
+        ForEach-Object { $_.GetValue($null) } |
+        Where-Object { $_ -like "$WordToComplete*" } |
+        Sort-Object |
+        ForEach-Object { "'$_'" }
+}
+
+Register-ArgumentCompleter -CommandName 'New-CBreezeCalcFormula' -ParameterName TableName -ScriptBlock ${function:TableNameCompleter}
+Register-ArgumentCompleter -CommandName 'Add-CBreezeTableRelation' -ParameterName TableName -ScriptBlock ${function:TableNameCompleter}
