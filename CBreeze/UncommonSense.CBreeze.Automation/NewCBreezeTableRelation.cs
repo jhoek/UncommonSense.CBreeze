b2b877a2b1b793c07da13b097a185090179b73d3
Jan Hoek <github@uncommonsense.nl>
2017-07-22 13:54:11 +0200

    Renamed AddCBreezeTableRelation to NewCBreezeTableRelation

---
+++ CBreeze/UncommonSense.CBreeze.Automation/NewCBreezeTableRelation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using UncommonSense.CBreeze.Core;
+using UncommonSense.CBreeze.Write;
+
+namespace UncommonSense.CBreeze.Automation
+{
+    [Cmdlet(VerbsCommon.New, "CBreezeTableRelation", DefaultParameterSetName = ParameterSetNames.NewWithoutID)]
+    [Alias("TableRelation")]
+    public class NewCBreezeTableRelation : NewItemCmdlet<TableRelationLine, PSObject>
+    {
+        [Parameter(Position = 1)]
+        public string FieldName
+        {
+            get;
+            set;
+        }
+
+        [Parameter(Mandatory = true, Position = 0)]
+        public string TableName
+        {
+            get;
+            set;
+        }
+
+        protected override void AddItemToInputObject(TableRelationLine item, PSObject inputObject)
+        {
+            switch (inputObject.BaseObject)
+            {
+                case TableField f: (f.AllProperties["TableRelation"] as TableRelationProperty).Value.Add(item); break;
+                case FieldPageControl c: c.Properties.TableRelation.Add(item); break;
+                default: throw new ArgumentOutOfRangeException("Don't know how to add a table relation to this InputObject."); ;
+            }
+        }
+
+        protected override IEnumerable<TableRelationLine> CreateItems()
+        {
+            yield return new TableRelationLine(TableName) { FieldName = FieldName };
+        }
+    }
+}
