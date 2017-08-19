cbc910ee11343b031a51b2baa3ab5acc5346a3b8
Jan Hoek <github@uncommonsense.nl>
2017-08-19 11:12:59 +0200

    ApplicationImporter now has conditional compilation sections for different NAV versions

--- CBreeze/UncommonSense.CBreeze.IO/ApplicationImporter.cs
+++ CBreeze/UncommonSense.CBreeze.IO/ApplicationImporter.cs
@@ -11,13 +11,13 @@ using UncommonSense.CBreeze.Write;
 namespace UncommonSense.CBreeze.IO
 {
     // Reads from Application object, imports into database
+    // https://msdn.microsoft.com/nl-nl/library/hh165287(v=nav.90).aspx
+
     public static class ApplicationImporter
     {
-        public static void Import(this Application application, string devClient, string serverName, string database)
+        private static void DoImport(Application application, string devClient, Arguments arguments)
         {
             var fileName = Path.ChangeExtension(Path.GetTempFileName(), "txt");
-
-            var arguments = new Arguments("importobjects", serverName, database);
             arguments.Add("file", fileName);
 
             try
@@ -30,5 +30,47 @@ namespace UncommonSense.CBreeze.IO
                 File.Delete(fileName);
             }
         }
+
+#if (NAV2015 || NAV2016)
+
+        public static void Import(this Application application, string devClient, string serverName, string database, ImportAction importAction = ImportAction.Default, SynchronizeSchemaChanges? synchronizeSchemaChanges = null, string navServerName = null, string navServerInstance = null, int? navServerMgtPort = null, string tenant = null)
+        {
+            var arguments = new Arguments("importobjects", serverName, database);
+            arguments.AddIf(importAction != ImportAction.Default, "importaction", (int)importAction);
+            arguments.Add("synchronizeschemachanges", synchronizeSchemaChanges?.ToString().ToLowerInvariant());
+            arguments.Add("navservername", navServerName);
+            arguments.Add("navserverinstance", navServerInstance);
+            arguments.Add("navservermanagementport", navServerMgtPort);
+            arguments.Add("tenant", tenant);
+
+            DoImport(application, devClient, arguments);
+        }
+
+#elif NAV2013R2
+
+        public static void Import(this Application application, string devClient, string serverName, string database, ImportAction importAction = ImportAction.Default, bool? validateTableChanges = null, string navServerName = null, string navServerInstance = null, int? navServerMgtPort = null, string tenant = null)
+        {
+            var arguments = new Arguments("importobjects", serverName, database);
+            arguments.AddIf(importAction != ImportAction.Default, "importaction", (int)importAction);
+            arguments.AddIf(validateTableChanges.HasValue, "validatetablechanges", validateTableChanges.Value ? 1 : 0);
+            arguments.Add("navservername", navServerName);
+            arguments.Add("navserverinstance", navServerInstance);
+            arguments.Add("navservermanagementport", navServerMgtPort);
+            arguments.Add("tenant", tenant);
+
+            DoImport(application, devClient, arguments);
+        }
+
+#else
+
+        public static void Import(this Application application, string devClient, string serverName, string database, ImportAction importAction = ImportAction.Default)
+        {
+            var arguments = new Arguments("importobjects", serverName, database);
+            arguments.AddIf(importAction != ImportAction.Default, "importaction", (int)importAction);
+
+            DoImport(application, devClient, arguments);
+        }
+
+#endif
     }
 }
