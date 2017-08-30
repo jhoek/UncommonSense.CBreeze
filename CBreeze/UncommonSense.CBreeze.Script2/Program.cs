8fc336671a1d0c7f868022c5f86a0e79c9a66c3c
Jan Hoek <github@uncommonsense.nl>
2017-08-30 10:26:06 +0200

    Read reference file

--- CBreeze/UncommonSense.CBreeze.Script2/Program.cs
+++ CBreeze/UncommonSense.CBreeze.Script2/Program.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UncommonSense.CBreeze.Core;
+using UncommonSense.CBreeze.Read;
 
 namespace UncommonSense.CBreeze.Script2
 {
@@ -13,32 +14,13 @@ namespace UncommonSense.CBreeze.Script2
     {
         private static void Main(string[] args)
         {
-            var application = new Application();
+            foreach (var arg in args)
+            {
+                var scriptFileName = Path.ChangeExtension(arg, ".ps1");
+                var application = ApplicationBuilder.FromFile(arg);
 
-            var table = new Table(50000, "Foo");
-            table.ObjectProperties.DateTime = DateTime.Now;
-            table.ObjectProperties.VersionList = "NAVJH1.00";
-
-            var codeField = new CodeTableField("Code", 20);
-            codeField.Properties.NotBlank = true;
-            table.Fields.Add(codeField);
-
-            var key = new TableKey("Code");
-            table.Keys.Add(key);
-
-            var function = new Function("Foo");
-            function.Local = true;
-            table.Code.Functions.Add(function);
-
-            application.Tables.Add(table);
-
-            var table2 = new Table(50001, "Baz");
-            application.Tables.Add(table2);
-
-            Console.WriteLine(application.ToInvocation().ToString());
-            File.WriteAllText(
-                Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "script.ps1"), 
-                application.ToInvocation().ToString());
+                File.WriteAllText(scriptFileName, application.ToInvocation().ToString());
+            }
         }
     }
 }
