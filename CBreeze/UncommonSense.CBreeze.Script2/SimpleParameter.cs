2602b208ea2e3e57221aecabcd7ff2f0761904a5
Jan Hoek <github@uncommonsense.nl>
2017-08-30 10:27:17 +0200

    More special cases for ValueAsString()

--- CBreeze/UncommonSense.CBreeze.Script2/SimpleParameter.cs
+++ CBreeze/UncommonSense.CBreeze.Script2/SimpleParameter.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UncommonSense.CBreeze.Core;
 
 namespace UncommonSense.CBreeze.Script2
 {
@@ -17,22 +18,38 @@ namespace UncommonSense.CBreeze.Script2
 
         public override bool HasValue => Value != null;
 
-        public Object Value { get; protected set; }
+        public System.Object Value { get; protected set; }
 
-        public string ValueAsString {
+        public string ValueAsString
+        {
             get
             {
                 switch (Value)
                 {
-                    case bool b: return (b ? "$true" : "$false");
-                    case string s: return $"'{s}'";
-                    case DateTime d: return $"'{d}'";
-                    case IEnumerable<string> e: return string.Join(", ", e.Select(s => $"'{s}'"));
-                    default: return Value.ToString();
+                    case bool b:
+                        return (b ? "$true" : "$false");
+
+                    case string s:
+                        return $"'{s.Replace("'", "''")}'";
+
+                    case DateTime d:
+                        return $"'{d}'";
+
+                    case IEnumerable<string> e:
+                        return string.Join(", ", e.Select(s => $"'{s}'"));
+
+                    case MultiLanguageValue m:
+                        return $"@{{ {string.Join("; ", m.Select(e => $"'{e.LanguageID}' = '{e.Value.Replace("'", "''") }'")) } }}";
+
+                    case DecimalPlaces d:
+                        return $"{d.AtLeast}:{d.AtMost}";
+
+                    default:
+                        return Value.ToString();
                 }
             }
         }
-        
+
         public override string ToString(int indentation) => $"{Indentation(indentation)}-{Name} {ValueAsString}";
     }
 }
