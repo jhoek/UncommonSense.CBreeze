5934d864196402aab839e3365e439452eddc5330
Jan Hoek <github@uncommonsense.nl>
2017-09-06 14:52:20 +0200

    Add-CBreezeOrderBy is now called New-CBreezeQueryOrderBy

---
+++ CBreeze/UncommonSense.CBreeze.Automation/NewCBreezeQueryOrderBy.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using UncommonSense.CBreeze.Core;
+
+namespace UncommonSense.CBreeze.Automation
+{
+    [Cmdlet(VerbsCommon.New, "CBreezeQueryOrderBy", DefaultParameterSetName = ParameterSetNames.NewWithoutID)]
+    [OutputType(typeof(QueryOrderByLine))]
+    [Alias("OrderBy")]
+    public class NewCBreezeQueryOrderBy : NewItemCmdlet<QueryOrderByLine, Query>
+    {
+        protected override void AddItemToInputObject(QueryOrderByLine item, Query inputObject)
+        {
+            inputObject.Properties.OrderBy.Add(item);
+        }
+
+        protected override IEnumerable<QueryOrderByLine> CreateItems()
+        {
+            yield return new QueryOrderByLine(Column, Direction);
+        }
+
+        [Parameter(Mandatory = true, Position = 0)]
+        public string Column
+        {
+            get;
+            set;
+        }
+
+        [Parameter(Position = 1)]
+        public QueryOrderByDirection Direction
+        {
+            get;
+            set;
+        }
+    }
+}
