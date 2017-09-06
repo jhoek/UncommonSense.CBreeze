f4d69b5c5017e29a484b327eac1458a66f7d43cd
Jan Hoek <github@uncommonsense.nl>
2017-09-06 14:38:52 +0200

    Replace generic AddCBreezeLink with specialized classes for queries and pages

---
+++ CBreeze/UncommonSense.CBreeze.Automation/NewCBreezeQueryLink.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+using UncommonSense.CBreeze.Core;
+
+namespace UncommonSense.CBreeze.Automation
+{
+    [Cmdlet(VerbsCommon.New, "CBreezeQueryLink", DefaultParameterSetName = ParameterSetNames.NewWithoutID)]
+    [Alias("QueryLink")]
+    [OutputType(typeof(QueryDataItemLinkLine))]
+    public class NewCBreezeQueryLink : NewItemCmdlet<QueryDataItemLinkLine, DataItemQueryElement>
+    {
+        protected override void AddItemToInputObject(QueryDataItemLinkLine item, DataItemQueryElement inputObject)
+        {
+            inputObject.Properties.DataItemLink.Add(item);
+        }
+
+        protected override IEnumerable<QueryDataItemLinkLine> CreateItems()
+        {
+            yield return new QueryDataItemLinkLine(FieldName, ReferenceTableName, ReferenceFieldName);
+        }
+
+        [Parameter(Mandatory = true, Position = 1)]
+        public string FieldName { get; set; }
+
+        [Parameter(Mandatory = true, Position = 3)]
+        public string ReferenceFieldName { get; set; }
+
+        [Parameter(Mandatory = true, Position = 2)]
+        public string ReferenceTableName { get; set; }
+    }
+}
---
+++ CBreeze/UncommonSense.CBreeze.Automation/NewCBreezeRunObjectLink.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using UncommonSense.CBreeze.Common;
+using UncommonSense.CBreeze.Core;
+using UncommonSense.CBreeze.Write;
+
+namespace UncommonSense.CBreeze.Automation
+{
+    [Cmdlet(VerbsCommon.New, "CBreezeRunObjectLink", DefaultParameterSetName = ParameterSetNames.NewWithoutID)]
+    [Alias("RunObjectLink")]
+    [OutputType(typeof(RunObjectLinkLine))]
+    public class NewCBreezeRunObjectLink : NewItemCmdlet<RunObjectLinkLine, PSObject>
+    {
+        protected override void AddItemToInputObject(RunObjectLinkLine item, PSObject inputObject)
+        {
+            switch (inputObject.BaseObject)
+            {
+                case PageAction a:
+                    a.Properties.RunPageLink.Add(item);
+                    break;
+
+                case PartPageControl c:
+                    c.Properties.SubPageLink.Add(item);
+                    break;
+
+                default:
+                    throw new ApplicationException("Cannot add links to this object.");
+            }
+        }
+
+        protected override IEnumerable<RunObjectLinkLine> CreateItems()
+        {
+            yield return new RunObjectLinkLine(FieldName, Type, Value)
+            {
+                ValueIsFilter = ValueIsFilter,
+                OnlyMaxLimit = OnlyMaxLimit
+            };
+        }
+
+        [Parameter(Mandatory = true, Position = 1)] public string FieldName { get; set; }
+        [Parameter()] public SwitchParameter OnlyMaxLimit { get; set; }
+        [Parameter(Mandatory = true, Position = 2)] public TableFilterType Type { get; set; }
+        [Parameter(Mandatory = true, Position = 3)] [AllowEmptyString()] public string Value { get; set; }
+        [Parameter()] public SwitchParameter ValueIsFilter { get; set; }
+    }
+}
