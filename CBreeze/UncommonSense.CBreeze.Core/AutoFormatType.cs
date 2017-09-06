57f642a50a4f07d4190213978ace9ae86451bea9
Jan Hoek <github@uncommonsense.nl>
2017-09-06 10:32:26 +0200

    Added missing Custom AutoFormatType

--- CBreeze/UncommonSense.CBreeze.Core/AutoFormatType.cs
+++ CBreeze/UncommonSense.CBreeze.Core/AutoFormatType.cs
@@ -2,8 +2,9 @@ namespace UncommonSense.CBreeze.Core
 {
     public enum AutoFormatType
     {
-        Other,
-        Amount,
-        UnitAmount,
+        Other = 0,
+        Amount = 1,
+        UnitAmount = 2,
+        Custom = 10
     }
 }
