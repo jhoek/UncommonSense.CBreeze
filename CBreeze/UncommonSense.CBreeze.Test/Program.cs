68a45fd4e50e3a89ca615703495dfcaed89ed64d
Jan Hoek <github@uncommonsense.nl>
2017-09-21 16:28:23 +0200

    Changed test code

--- CBreeze/UncommonSense.CBreeze.Test/Program.cs
+++ CBreeze/UncommonSense.CBreeze.Test/Program.cs
@@ -13,20 +13,9 @@ namespace UncommonSense.CBreeze.Test
     {
         private static void Main(string[] args)
         {
-            //var tagList = new TagList();
-            //tagList.Add("#Foo");
-            //tagList.Add("#Baz");
-            //Console.WriteLine(tagList);
-
-            //var application = new Application();
-            //var page = application.Pages.Add(new Page("Foo"));
-            //var action = page.Properties.ActionList.Add(new PageAction(caption: "Foo"));
-            //action.Properties.ApplicationArea.Add("#oink");
-            //action.Properties.ApplicationArea.Add("#boink");
-
-            var application = ApplicationBuilder.FromFile(@"c:\users\jhoek\Dropbox\BaseAppExports\w12017.txt");
-            application.Write(@"c:\users\jhoek\Desktop\output.txt");
-            //var application = ApplicationBuilder.FromFile(@"c:\users\jhoek\Desktop\cod455.txt");
+            ApplicationBuilder
+                .FromFile(@"c:\users\jhoek\Dropbox\BaseAppExports\nl2016.txt")
+                .Write(@"c:\users\jhoek\Desktop\output.txt");
         }
     }
 }
