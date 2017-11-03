7c3ead65a9d552d5ddffec82cd02cda2b054a063
Jan Hoek <github@uncommonsense.nl>
2017-11-03 21:36:48 +0100

    Run the test script automatically

--- CBreeze/UncommonSense.CBreeze.Script/Program.cs
+++ CBreeze/UncommonSense.CBreeze.Script/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.CodeDom.Compiler;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -20,6 +21,14 @@ namespace UncommonSense.CBreeze.Script
                 var application = ApplicationBuilder.ReadFromFiles(arg);
 
                 File.WriteAllText(scriptFileName, application.ToInvocation().ToString(), Encoding.UTF8);
+
+                var processStartInfo = new ProcessStartInfo() {
+                    FileName = @"C:\WINDOWS\System32\WindowsPowerShell\v1.0\powershell.exe",
+                    Arguments = $"-NoExit -File test.ps1",
+                    WorkingDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
+                };
+                
+                Process.Start(processStartInfo);
             }
         }
     }
