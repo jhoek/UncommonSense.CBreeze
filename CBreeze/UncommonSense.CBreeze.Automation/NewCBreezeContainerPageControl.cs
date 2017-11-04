ab4859fa87c8af2e665d94da73a6224414101fd1
Jan Hoek <github@uncommonsense.nl>
2017-11-04 11:26:38 +0100

    Cmdlets for new page controls (container and group) - work in progress

---
+++ CBreeze/UncommonSense.CBreeze.Automation/NewCBreezeContainerPageControl.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+using UncommonSense.CBreeze.Core;
+
+namespace UncommonSense.CBreeze.Automation
+{
+    [Cmdlet(VerbsCommon.New, "CBreezeContainerPageControl", DefaultParameterSetName = ParameterSetNames.NewWithoutID)]
+    [OutputType(typeof(ContainerPageControl))]
+    [Alias("Container")]
+    public class NewCBreezeContainerPageControl : NewItemWithIDCmdlet<PageControl, int, PSObject>
+    {
+        protected override void AddItemToInputObject(PageControl item, PSObject inputObject)
+        {
+            switch (inputObject.BaseObject)
+            {
+                case PageControls c:
+                    AddItemToInputObject(item, c);
+                    break;
+
+                case IPage p:
+                    AddItemToInputObject(item, p);
+                    break;
+
+                default:
+                    base.AddItemToInputObject(item, inputObject);
+                    break;
+            }
+        }
+
+        protected void AddItemToInputObject(PageControl item, IPage page) => AddItemToInputObject(item, page.Controls);
+
+        protected void AddItemToInputObject(PageControl item, PageControls pageControls)
+        {
+            var position = Position.GetValueOrDefault(Core.Position.LastWithinContainer);
+
+            switch (position)
+            {
+                case Core.Position.FirstWithinContainer:
+                    pageControls.Insert(0, item);
+                    break;
+
+                case Core.Position.LastWithinContainer:
+                    pageControls.Add(item);
+                    break;
+            }
+        }
+
+        protected override IEnumerable<PageControl> CreateItems()
+        {
+            var containerPageControl = new ContainerPageControl(ID, 0, ContainerType.GetValueOrDefault(Core.ContainerType.ContentArea));
+            containerPageControl.Properties.CaptionML.Set(CaptionML);
+            containerPageControl.Properties.Description = Description;
+            containerPageControl.Properties.Name = Name;
+            containerPageControl.AutoCaption(AutoCaption);
+            yield return containerPageControl;
+
+            var variables = new List<PSVariable>() { new PSVariable("Indentation", 1) };
+            var childControls = ChildControls?
+                .InvokeWithContext(null, variables)
+                .Select(o => o.BaseObject)
+                .Cast<PageControl>()
+                ?? Enumerable.Empty<PageControl>();
+
+            foreach (var childControl in childControls)
+            {
+                yield return childControl;
+            }
+        }
+
+        [Parameter()] public SwitchParameter AutoCaption { get; set; }
+        [Parameter()] public Hashtable CaptionML { get; set; }
+
+        [Parameter(Position = 1, ParameterSetName = ParameterSetNames.NewWithoutID)]
+        [Parameter(Position = 1, ParameterSetName = ParameterSetNames.AddWithoutID)]
+        [Parameter(Position = 2, ParameterSetName = ParameterSetNames.NewWithID)]
+        [Parameter(Position = 2, ParameterSetName = ParameterSetNames.AddWithID)]
+        public ScriptBlock ChildControls { get; set; }
+
+        [Parameter()] public ContainerType? ContainerType { get; set; }
+        [Parameter()] public string Description { get; set; }
+        [Parameter()] public string Name { get; set; }
+
+        [Parameter(ParameterSetName = ParameterSetNames.AddWithoutID)]
+        [Parameter(ParameterSetName = ParameterSetNames.AddWithID)]
+        public Position? Position { get; set; }
+    }
+}
---
+++ CBreeze/UncommonSense.CBreeze.Automation/NewCBreezeGroupPageControl.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+using UncommonSense.CBreeze.Core;
+
+namespace UncommonSense.CBreeze.Automation
+{
+    [Cmdlet(VerbsCommon.New, "CBreezeGroupPageControl", DefaultParameterSetName = ParameterSetNames.NewWithoutID)]
+    [OutputType(typeof(GroupPageControl))]
+    [Alias("Group")]
+    public class NewCBreezeGroupPageControl : NewItemWithIDCmdlet<PageControl, int, PSObject>
+    {
+        protected override void AddItemToInputObject(PageControl item, PSObject inputObject)
+        {
+            // FIXME:
+            //base.AddItemToInputObject(item, inputObject);
+        }
+
+        protected override IEnumerable<PageControl> CreateItems()
+        {
+            var groupPageControl = new GroupPageControl(ID, GetIndentation(), GroupType.GetValueOrDefault(Core.GroupType.Group));
+            groupPageControl.Properties.CaptionML.Set(CaptionML);
+            groupPageControl.Properties.Description = Description;
+            groupPageControl.Properties.Name = Name;
+            groupPageControl.Properties.Editable = Editable;
+            groupPageControl.Properties.Enabled = Enabled;
+            groupPageControl.Properties.FreezeColumnID = FreezeColumnID;
+            groupPageControl.Properties.IndentationColumnName = IndentationColumnName;
+            groupPageControl.Properties.IndentationControls.Set(IndentationControls);
+            groupPageControl.Properties.Layout = Layout;
+            groupPageControl.Properties.ShowAsTree = ShowAsTree;
+            groupPageControl.Properties.Visible = Visible;
+            groupPageControl.AutoCaption(AutoCaption);
+            yield return groupPageControl;
+
+            var variables = new List<PSVariable>() { new PSVariable("Indentation", groupPageControl.IndentationLevel + 1) };
+            var childControls = ChildControls?
+                .InvokeWithContext(null, variables)
+                .Select(o => o.BaseObject)
+                .Cast<PageControl>()
+                ?? Enumerable.Empty<PageControl>();
+
+            foreach (var childControl in childControls)
+            {
+                yield return childControl;
+            }
+        }
+
+        protected int GetIndentation()
+        {
+            return ParameterSetNames.IsNew(ParameterSetName)
+                ? (int)GetVariableValue("Indentation", 0)
+                : GetParentIndentation() + 1;
+        }
+
+        protected int GetParentIndentation()
+        {
+            throw new NotImplementedException(); // FIXME
+        }
+
+        [Parameter()] public SwitchParameter AutoCaption { get; set; }
+        [Parameter()] public Hashtable CaptionML { get; set; }
+
+        [Parameter(Position = 1, ParameterSetName = ParameterSetNames.NewWithoutID)]
+        [Parameter(Position = 1, ParameterSetName = ParameterSetNames.AddWithoutID)]
+        [Parameter(Position = 2, ParameterSetName = ParameterSetNames.NewWithID)]
+        [Parameter(Position = 2, ParameterSetName = ParameterSetNames.AddWithID)]
+        public ScriptBlock ChildControls { get; set; }
+
+        [Parameter()] public string Description { get; set; }
+        [Parameter()] public string Editable { get; set; }
+        [Parameter()] public string Enabled { get; set; }
+        [Parameter()] public string FreezeColumnID { get; set; }
+        [Parameter()] public GroupType? GroupType { get; set; }
+        [Parameter()] public string IndentationColumnName { get; set; }
+        [Parameter()] public string[] IndentationControls { get; set; }
+        [Parameter()] public GroupPageControlLayout? Layout { get; set; }
+        [Parameter()] public string Name { get; set; }
+        [Parameter()] public bool? ShowAsTree { get; set; }
+        [Parameter()] public string Visible { get; set; }
+    }
+}
