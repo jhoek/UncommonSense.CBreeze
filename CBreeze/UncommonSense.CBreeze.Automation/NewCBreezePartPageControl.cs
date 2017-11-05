a88c8d5facffe4c9041d66efbac949db2227c940
Jan Hoek <github@uncommonsense.nl>
2017-11-05 16:07:37 +0100

    New-CBreezePartPageControl

--- CBreeze/UncommonSense.CBreeze.Automation/NewCBreezePartPageControl.cs
+++ CBreeze/UncommonSense.CBreeze.Automation/NewCBreezePartPageControl.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using System.Management.Automation;
-using System.Text;
-using System.Threading.Tasks;
 using UncommonSense.CBreeze.Core;
 
 namespace UncommonSense.CBreeze.Automation
@@ -14,6 +11,84 @@ namespace UncommonSense.CBreeze.Automation
     [Alias("PartControl")]
     public class NewCBreezePartPageControl : NewItemWithIDCmdlet<PageControl, int, PSObject>
     {
+        protected override void AddItemToInputObject(PageControl item, PSObject inputObject)
+        {
+            switch (inputObject.BaseObject)
+            {
+                case IPage p:
+                    p.Controls.Add(item, Position);
+                    break;
+
+                case ContainerPageControl c:
+                    c.AddChildPageControl(item, Position.GetValueOrDefault(Core.Position.LastWithinContainer));
+                    break;
+
+                case GroupPageControl g:
+                    g.AddChildPageControl(item, Position.GetValueOrDefault(Core.Position.LastWithinContainer));
+                    break;
+
+                case PageControls c:
+                    c.Add(item, Position);
+                    break;
+
+                default:
+                    base.AddItemToInputObject(item, inputObject);
+                    break;
+            }
+        }
+
+        protected override IEnumerable<PageControl> CreateItems()
+        {
+            var partPageControl = new PartPageControl(ID, GetIndentation());
+            partPageControl.Properties.CaptionML.Set(CaptionML);
+            partPageControl.Properties.Description = Description;
+            partPageControl.Properties.Editable = Editable;
+            partPageControl.Properties.Enabled = Enabled;
+            partPageControl.Properties.Name = Name;
+
+            if (ChartPartID != null)
+            {
+                partPageControl.Properties.PartType = PartType.Chart;
+                partPageControl.Properties.ChartPartID = ChartPartID;
+            }
+            else if (PagePartID != null)
+            {
+                partPageControl.Properties.PartType = PartType.Page;
+                partPageControl.Properties.PagePartID = PagePartID;
+            }
+            else if (SystemPartID != null)
+            {
+                partPageControl.Properties.PartType = PartType.System;
+                partPageControl.Properties.SystemPartID = SystemPartID;
+            }
+
+            partPageControl.Properties.ProviderID = ProviderID;
+            partPageControl.Properties.ShowFilter = ShowFilter;
+            partPageControl.Properties.SubPageView.Key = SubPageViewKey;
+            partPageControl.Properties.SubPageView.Order = SubPageViewOrder;
+#if NAV2015
+            partPageControl.Properties.UpdatePropagation = UpdatePropagation;
+#endif
+            partPageControl.Properties.Visible = Visible;
+            partPageControl.AutoCaption(AutoCaption);
+
+            yield return partPageControl;
+        }
+
+        protected int GetIndentation()
+        {
+            return ParameterSetNames.IsNew(ParameterSetName)
+                ? (int)GetVariableValue("Indentation", 0)
+                : GetParentIndentation() + 1;
+        }
+
+        protected int GetParentIndentation()
+        {
+            return InputObject.BaseObject is PageControl
+                ? (InputObject.BaseObject as PageControl).IndentationLevel.GetValueOrDefault(0)
+                : 0;
+        }
+
         [Parameter()] public SwitchParameter AutoCaption { get; set; }
         [Parameter()] public Hashtable CaptionML { get; set; }
         [Parameter()] public string ChartPartID { get; set; }
@@ -21,6 +96,7 @@ namespace UncommonSense.CBreeze.Automation
         [Parameter()] public string Editable { get; set; }
         [Parameter()] public string Enabled { get; set; }
         [Parameter()] public string Name { get; set; }
+        [Parameter()] public Position? Position { get; set; }
         [Parameter()] public int? PagePartID { get; set; }
         [Parameter()] public int? ProviderID { get; set; }
         [Parameter()] public bool ShowFilter { get; set; }
@@ -31,14 +107,5 @@ namespace UncommonSense.CBreeze.Automation
         [Parameter()] public UpdatePropagation? UpdatePropagation { get; set; }
 #endif
         [Parameter()] public string Visible { get; set; }
-
-        protected override void AddItemToInputObject(PageControl item, PSObject inputObject)
-        {
-        }
-
-        protected override IEnumerable<PageControl> CreateItems()
-        {
-            //throw new NotImplementedException();
-        }
     }
 }
