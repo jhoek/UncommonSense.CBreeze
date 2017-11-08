a8f9d079e633af5d3ffce4f7a5ee64283da7df92
Jan Hoek <github@uncommonsense.nl>
2017-11-08 14:06:07 +0100

    Last bool? parameters dealt with; only Nullable<(B|b)ool(ean)>s remain

--- CBreeze/UncommonSense.CBreeze.Automation/NewCBreezeFieldPageControl.cs
+++ CBreeze/UncommonSense.CBreeze.Automation/NewCBreezeFieldPageControl.cs
@@ -7,7 +7,7 @@ namespace UncommonSense.CBreeze.Automation
 {
     // FIXME: Scriptblock for table relation
 
-    [Cmdlet(VerbsCommon.New, "CBreezeFieldPageControl", DefaultParameterSetName =ParameterSetNames.NewWithoutID)]
+    [Cmdlet(VerbsCommon.New, "CBreezeFieldPageControl", DefaultParameterSetName = ParameterSetNames.NewWithoutID)]
     [OutputType(typeof(FieldPageControl))]
     [Alias("FieldControl")]
     public class NewCBreezeFieldPageControl : NewItemWithIDCmdlet<PageControl, int, PSObject>
@@ -18,22 +18,22 @@ namespace UncommonSense.CBreeze.Automation
         }
 
         [Parameter()] public AccessByPermission AccessByPermission { get; set; }
-        [Parameter()] public bool? AssistEdit { get; set; }
+        [Parameter()] public SwitchParameter AssistEdit { get; set; }
         [Parameter()] public SwitchParameter AutoCaption { get; set; }
         [Parameter()] public string AutoFormatExpr { get; set; }
         [Parameter()] public AutoFormatType? AutoFormatType { get; set; }
         [Parameter()] public BlankNumbers? BlankNumbers { get; set; }
-        [Parameter()] public bool? BlankZero { get; set; }
+        [Parameter()] public SwitchParameter BlankZero { get; set; }
         [Parameter()] public string CaptionClass { get; set; }
         [Parameter()] public string CharAllowed { get; set; }
-        [Parameter()] public bool? ClosingDates { get; set; }
+        [Parameter()] public SwitchParameter ClosingDates { get; set; }
         [Parameter()] public int? ColumnSpan { get; set; }
         [Parameter()] public string ControlAddIn { get; set; }
-        [Parameter()] public bool? DateFormula { get; set; }
+        [Parameter()] public SwitchParameter DateFormula { get; set; }
         [Parameter()] [ValidateRange(0, int.MaxValue)] public int? DecimalPlacesAtLeast { get; set; }
         [Parameter()] [ValidateRange(0, int.MaxValue)] public int? DecimalPlacesAtMost { get; set; }
         [Parameter()] public string Description { get; set; }
-        [Parameter()] public bool? DrillDown { get; set; }
+        [Parameter()] public SwitchParameter DrillDown { get; set; }
         [Parameter()] public string DrillDownPageID { get; set; }
         [Parameter()] public string Editable { get; set; }
         [Parameter()] public string Enabled { get; set; }
@@ -43,23 +43,23 @@ namespace UncommonSense.CBreeze.Automation
         [Parameter()] public string Image { get; set; }
 #endif
         [Parameter()] public Importance? Importance { get; set; }
-        [Parameter()] public bool? Lookup { get; set; }
+        [Parameter()] public SwitchParameter Lookup { get; set; }
         [Parameter()] public string LookupPageID { get; set; }
         [Parameter()] public object MaxValue { get; set; }
         [Parameter()] public object MinValue { get; set; }
-        [Parameter()] public bool? MultiLine { get; set; }
+        [Parameter()] public SwitchParameter MultiLine { get; set; }
         [Parameter()] public string Name { get; set; }
-        [Parameter()] public bool? NotBlank { get; set; }
-        [Parameter()] public bool? Numeric { get; set; }
+        [Parameter()] public SwitchParameter NotBlank { get; set; }
+        [Parameter()] public SwitchParameter Numeric { get; set; }
         [Parameter()] public Hashtable OptionCaptionML { get; set; }
         [Parameter()] public string QuickEntry { get; set; }
         [Parameter()] public int? RowSpan { get; set; }
-        [Parameter()] public bool? ShowCaption { get; set; }
+        [Parameter()] public SwitchParameter ShowCaption { get; set; }
 #if NAV2015
         [Parameter()] public string ShowMandatory { get; set; }
 #endif
 
-        [Parameter(Mandatory = true, Position = 1, ParameterSetName =ParameterSetNames.NewWithoutID)]
+        [Parameter(Mandatory = true, Position = 1, ParameterSetName = ParameterSetNames.NewWithoutID)]
         [Parameter(Mandatory = true, Position = 2, ParameterSetName = ParameterSetNames.NewWithID)]
         [Parameter(Mandatory = true, Position = 1, ParameterSetName = ParameterSetNames.AddWithoutID)]
         [Parameter(Mandatory = true, Position = 2, ParameterSetName = ParameterSetNames.AddWithID)]
@@ -67,7 +67,7 @@ namespace UncommonSense.CBreeze.Automation
 
         [Parameter()] public Style? Style { get; set; }
         [Parameter()] public string StyleExpr { get; set; }
-        [Parameter()] public bool? Title { get; set; }
+        [Parameter()] public SwitchParameter Title { get; set; }
         [Parameter()] public string ValuesAllowed { get; set; }
         [Parameter()] public string Visible { get; set; }
         [Parameter()] [ValidateRange(0, int.MaxValue)] public int? Width { get; set; }
@@ -77,21 +77,21 @@ namespace UncommonSense.CBreeze.Automation
             var fieldPageControl = new FieldPageControl(SourceExpr, ID, GetIndentation());
 
             fieldPageControl.Properties.AccessByPermission.Set(AccessByPermission);
-            fieldPageControl.Properties.AssistEdit = AssistEdit;
+            fieldPageControl.Properties.AssistEdit = NullableBooleanFromSwitch(nameof(AssistEdit));
             fieldPageControl.Properties.AutoFormatExpr = AutoFormatExpr;
             fieldPageControl.Properties.AutoFormatType = AutoFormatType;
             fieldPageControl.Properties.BlankNumbers = BlankNumbers;
-            fieldPageControl.Properties.BlankZero = BlankZero;
+            fieldPageControl.Properties.BlankZero = NullableBooleanFromSwitch(nameof(BlankZero));
             fieldPageControl.Properties.CaptionClass = CaptionClass;
             fieldPageControl.Properties.CharAllowed = CharAllowed;
-            fieldPageControl.Properties.ClosingDates = ClosingDates;
+            fieldPageControl.Properties.ClosingDates = NullableBooleanFromSwitch(nameof(ClosingDates));
             fieldPageControl.Properties.ColumnSpan = ColumnSpan;
             fieldPageControl.Properties.ControlAddIn = ControlAddIn;
-            fieldPageControl.Properties.DateFormula = DateFormula;
+            fieldPageControl.Properties.DateFormula = NullableBooleanFromSwitch(nameof(DateFormula));
             fieldPageControl.Properties.DecimalPlaces.AtLeast = DecimalPlacesAtLeast;
             fieldPageControl.Properties.DecimalPlaces.AtMost = DecimalPlacesAtMost;
             fieldPageControl.Properties.Description = Description;
-            fieldPageControl.Properties.DrillDown = DrillDown;
+            fieldPageControl.Properties.DrillDown = NullableBooleanFromSwitch(nameof(DrillDown));
             fieldPageControl.Properties.DrillDownPageID = DrillDownPageID;
             fieldPageControl.Properties.Editable = Editable;
             fieldPageControl.Properties.Enabled = Enabled;
@@ -101,24 +101,24 @@ namespace UncommonSense.CBreeze.Automation
             fieldPageControl.Properties.Image = Image;
 #endif
             fieldPageControl.Properties.Importance = Importance;
-            fieldPageControl.Properties.Lookup = Lookup;
+            fieldPageControl.Properties.Lookup = NullableBooleanFromSwitch(nameof(Lookup));
             fieldPageControl.Properties.LookupPageID = LookupPageID;
             fieldPageControl.Properties.MaxValue = MaxValue;
             fieldPageControl.Properties.MinValue = MinValue;
-            fieldPageControl.Properties.MultiLine = MultiLine;
+            fieldPageControl.Properties.MultiLine = NullableBooleanFromSwitch(nameof(MultiLine));
             fieldPageControl.Properties.Name = Name;
-            fieldPageControl.Properties.NotBlank = NotBlank;
-            fieldPageControl.Properties.Numeric = Numeric;
+            fieldPageControl.Properties.NotBlank = NullableBooleanFromSwitch(nameof(NotBlank));
+            fieldPageControl.Properties.Numeric = NullableBooleanFromSwitch(nameof(Numeric));
             fieldPageControl.Properties.OptionCaptionML.Set(OptionCaptionML);
             fieldPageControl.Properties.QuickEntry = QuickEntry;
             fieldPageControl.Properties.RowSpan = RowSpan;
-            fieldPageControl.Properties.ShowCaption = ShowCaption;
+            fieldPageControl.Properties.ShowCaption = NullableBooleanFromSwitch(nameof(ShowCaption));
 #if NAV2015
             fieldPageControl.Properties.ShowMandatory = ShowMandatory;
 #endif
             fieldPageControl.Properties.Style = Style;
             fieldPageControl.Properties.StyleExpr = StyleExpr;
-            fieldPageControl.Properties.Title = Title;
+            fieldPageControl.Properties.Title = NullableBooleanFromSwitch(nameof(Title));
             fieldPageControl.Properties.ValuesAllowed = ValuesAllowed;
             fieldPageControl.Properties.Visible = Visible;
             fieldPageControl.Properties.Width = Width;
