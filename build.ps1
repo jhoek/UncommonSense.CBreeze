1dc9f14cdb38c3077b07dda22373737f447b866c
Jan Hoek <github@uncommonsense.nl>
2018-01-27 21:16:10 +0100

    Added support for 2016; changed verbosity

--- build.ps1
+++ build.ps1
@@ -8,7 +8,10 @@ Properties {
 
 Task Default -Depends NAV2017
 
+Task NAV2016 {
+    Exec { msbuild $SolutionFileName /t:Build /p:"Configuration=Debug;DefineConstants=NAV2013 NAV2013R2 NAV2015 NAV2016;PreBuildEvent=;PostBuildEvent=" /v:minimal }
+}
+
 Task NAV2017 {
-    # FIXME: /p:OutDir=...
-    Exec { msbuild $SolutionFileName /t:Build /p:"Configuration=Debug;DefineConstants=NAV2013 NAV2013R2 NAV2015 NAV2016 NAV2017;PreBuildEvent=;PostBuildEvent=" /v:quiet }
+    Exec { msbuild $SolutionFileName /t:Build /p:"Configuration=Debug;DefineConstants=NAV2013 NAV2013R2 NAV2015 NAV2016 NAV2017;PreBuildEvent=;PostBuildEvent=" /v:minimal }
 }
