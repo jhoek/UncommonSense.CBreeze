6ff54a0a66aca14789e08148c5d2cd83fb5ad308
Jan Hoek <github@uncommonsense.nl>
2018-01-31 19:04:44 +0100

    Register PageNameCompleter

--- CBreeze/UncommonSense.CBreeze.Automation/UncommonSense.CBreeze.Automation.ps1
+++ CBreeze/UncommonSense.CBreeze.Automation/UncommonSense.CBreeze.Automation.ps1
@@ -15,6 +15,23 @@
         ForEach-Object { "'$_'" }
 }
 
+function PageNameCompleter {
+    param
+    (
+        $commandName, 
+        $parameterName, 
+        $wordToComplete, 
+        $commandAst, 
+        $fakeBoundParameter
+    )
+
+    [UncommonSense.CBreeze.Core.BaseApp+PageNames].GetFields('Static,Public') | 
+        ForEach-Object { $_.GetValue($null) } |
+        Where-Object { $_ -like "$WordToComplete*" } |
+        Sort-Object |
+        ForEach-Object { "'$_'" }
+}
+
 function ImageNameCompleter {
     param
     (
@@ -33,5 +50,8 @@ function ImageNameCompleter {
 }
 
 Register-ArgumentCompleter -ParameterName TableName -ScriptBlock ${function:TableNameCompleter}
+Register-ArgumentCompleter -ParameterName CardPageID -ScriptBlock ${function:PageNameCompleter}
+Register-ArgumentCompleter -ParameterName DrillDownPageID -ScriptBlock ${function:PageNameCompleter}
+Register-ArgumentCompleter -ParameterName LookupPageID -ScriptBlock ${function:PageNameCompleter}
 Register-ArgumentCompleter -CommandName 'New-CBreezePageAction' -ParameterName Image -ScriptBlock ${function:ImageNameCompleter}
 Register-ArgumentCompleter -CommandName 'New-CBreezePageGroup' -ParameterName Image -ScriptBlock ${function:ImageNameCompleter}
