1b43845e5baf6a26f8a95f8d21382636775087a8
Jan Hoek <github@uncommonsense.nl>
2018-02-09 20:03:30 +0100

    Adding copying of manifest, formats and init script to build script

--- build.ps1
+++ build.ps1
@@ -1,1 +1,5 @@
 dotnet msbuild /p:DefineConstants="NAV2013R2 NAV2015 NAV2016 NAV2017"
+
+copy ./UncommonSense.CBreeze.Automation/manifest.psd1 './UncommonSense.CBreeze.Automation/bin/Debug/netcoreapp2.0/UncommonSense.CBreeze.Automation.psd1'
+copy ./UncommonSense.CBreeze.Automation/format.ps1xml './UncommonSense.CBreeze.Automation/bin/Debug/netcoreapp2.0/UncommonSense.CBreeze.Automation.Format.ps1xml'
+copy ./UncommonSense.CBreeze.Automation/init.ps1 './UncommonSense.CBreeze.Automation/bin/Debug/netcoreapp2.0/UncommonSense.CBreeze.Automation.ps1'
