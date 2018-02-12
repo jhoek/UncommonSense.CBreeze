10c5f5dbaa1a6136c62442c6b9bc3a88adca715c
Jan Hoek <github@uncommonsense.nl>
2018-02-12 20:20:39 +0100

    Script for publishing module to PSGallery

---
+++ CBreeze/UncommonSense.CBreeze.Automation/Publish-ToPowerShellGallery.ps1
@@ -0,0 +1,6 @@
+﻿$ErrorActionPreference = 'Stop'
+
+Publish-Module `
+    -Path (Join-Path -Path $PSScriptRoot -ChildPath bin/Debug/UncommonSense.CBreeze.Automation) `
+    -Repository PSGallery `
+    -NuGetApiKey $env:NuGetApiKey
