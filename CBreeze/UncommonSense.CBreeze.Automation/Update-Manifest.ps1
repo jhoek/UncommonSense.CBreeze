35f8cc03a46df5a3e23f2b372bb8270e84339f9c
Jan Hoek <github@uncommonsense.nl>
2018-02-12 20:19:46 +0100

    Script to update CmdletsToExport and AliasesToExport in manifest

---
+++ CBreeze/UncommonSense.CBreeze.Automation/Update-Manifest.ps1
@@ -0,0 +1,8 @@
+﻿$ErrorActionPreference = 'Stop'
+
+Import-Module (Join-Path -Path $PSScriptRoot -ChildPath 'bin/debug/UncommonSense.CBreeze.Automation/UncommonSense.CBreeze.Automation.dll') -Force
+
+Update-ModuleManifest `
+    -Path (Join-Path -Path $PSScriptRoot -ChildPath manifest.psd1) `
+    -CmdletsToExport (Get-Command -Module UncommonSense.CBreeze.Automation -CommandType Cmdlet | Select-Object -ExpandProperty Name) `
+    -AliasesToExport (Get-Command -Module UncommonSense.CBreeze.Automation -CommandType Alias | Select-Object -ExpandProperty Name) 
