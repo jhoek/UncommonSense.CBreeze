d4436d75740c36712219fd557680f6fc50cd0947
Jan Hoek <github@uncommonsense.nl>
2018-02-14 11:20:47 +0100

    Updated build script; udpated AppVeyor build definition

--- build.ps1
+++ build.ps1
@@ -1,15 +1,16 @@
-Framework '4.6'
-
 Properties {
     $Configuration = "Debug"
-    $CompilationConstants = 'NAV2013 NAV2013R2 NAV2015 NAV2016 NAV2017'
     $RootFolder = $psake.build_script_dir
     $SolutionFolder = Join-Path -Path $RootFolder -ChildPath CBreeze
     $SolutionFileName = Join-Path -Path $SolutionFolder -ChildPath UncommonSense.CBreeze.sln
 }
 
-Task -Name Default -Depends Build
+Task -Name Default -Depends Build2017, Build2016 #, Build2015, Build2013R2, Build2013
 
-Task -Name Build {
-    Exec { msbuild $SolutionFileName /t:Build /p:"Configuration=$Configuration;DefineConstants=$CompilationConstants;PreBuildEvent=;PostBuildEvent=" /v:minimal }
+Task -Name Build2017 {
+    Exec { msbuild $SolutionFileName /t:Build /p:"Configuration=$Configuration;DefineConstants="NAV2017; NAV2016; NAV2015; NAV2013R2; NAV2013";PreBuildEvent=;PostBuildEvent=" /v:minimal }
+}
+
+Task -Name Build2016 {
+    Exec { msbuild $SolutionFileName /t:Build /p:"Configuration=$Configuration;DefineConstants="NAV2016; NAV2015; NAV2013R2; NAV2013";PreBuildEvent=;PostBuildEvent=" /v:minimal }
 }
