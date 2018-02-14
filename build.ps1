b9e219e29f756b8a3f1b0ee85ff8338a4ecc1fe2
Jan Hoek <github@uncommonsense.nl>
2018-02-14 15:50:18 +0100

    Invoke-MSBuild function

--- build.ps1
+++ build.ps1
@@ -2,27 +2,64 @@ Properties {
     $Configuration = "Debug"
     $RootFolder = $psake.build_script_dir
     $SolutionFolder = Join-Path -Path $RootFolder -ChildPath CBreeze
-    $SolutionFileName = Join-Path -Path $SolutionFolder -ChildPath UncommonSense.CBreeze.sln
 }
 
-Task -Name Default -Depends Build2017, Build2016 #, Build2015, Build2013R2, Build2013
+function Invoke-MSBuild 
+{
+    param
+    (
+        [Parameter(Mandatory, Position = 1)][string]$ProjectName,
+        [ValidateNotNullOrEmpty()][string]$Target = 'Build',
+        [ValidateNotNullOrEmpty()][string]$Configuration = 'Debug',
+        [Parameter(Mandatory, Position = 2)][ValidateSet('NAV2017', 'NAV2016', 'NAV2015', 'NAV2013R2', 'NAV2013')][string]$NAVVersion,
+        [Parameter()][string]$PreBuildEvent = '',
+        [Parameter()][string]$PostBuildEvent = '',
+        [ValidateSet('quiet', 'minimal', 'normal', 'detailed', 'diagnostic')]$Verbosity = 'Normal'
+    )   
+    
+    $ProjectFolderPath = Join-Path -Path $SolutionFolder -ChildPath $ProjectName
+    $ProjectFilePath = Join-Path -Path $ProjectFolderPath -ChildPath "$ProjectName.csproj"
+    $AssemblyFileName = "$ProjectName.$NAVVersion"
+
+    $DefineConstants = switch ($NAVVersion)
+    {
+        'NAV2017' { 'NAV2017 NAV2016 NAV2015 NAV2013R2 NAV2013' }
+        'NAV2016' { 'NAV2016 NAV2015 NAV2013R2 NAV2013' }
+        'NAV2015' { 'NAV2015 NAV2013R2 NAV2013' }
+        'NAV2013R2' { 'NAV2013R2 NAV2013'}
+        'NAV2013' { 'NAV2013'}
+    }
+
+    Exec { 
+        msbuild /target:$Target /property:Configuration=$Configuration /property:DefineConstants="$DefineConstants" /property:AssemblyName=$AssemblyFileName /property:PreBuildEvent=$PreBuildEvent /property:PostBuildEvent=$PostBuildEvent /verbosity:$Verbosity $ProjectFilePath
+    }
+}
+
+Task -Name Default -Depends Build2017, Build2016
 
 Task -Name Build2017 {
-    Exec { msbuild $SolutionFileName /t:Build /p:"Configuration=$Configuration;DefineConstants="NAV2017; NAV2016; NAV2015; NAV2013R2; NAV2013";PreBuildEvent=;PostBuildEvent=" /v:minimal }
+    Invoke-MSBuild UncommonSense.CBreeze.Common NAV2017
 }
 
 Task -Name Build2016 {
-    Exec { msbuild $SolutionFileName /t:Build /p:"Configuration=$Configuration;DefineConstants="NAV2016; NAV2015; NAV2013R2; NAV2013";PreBuildEvent=;PostBuildEvent=" /v:minimal }
+    Invoke-MSBuild UncommonSense.CBreeze.Common NAV2016
+}
+
+<#
+
+Task -Name Build2016 {
+    Exec { msbuild $SolutionFileName /t:Build /p:"Configuration=$Configuration; DefineConstants=`"NAV2016; NAV2015; NAV2013R2; NAV2013`"; PreBuildEvent=; PostBuildEvent=" /v:minimal }
 }
 
 Task -Name Build2015 {
-    Exec { msbuild $SolutionFileName /t:Build /p:"Configuration=$Configuration;DefineConstants="NAV2015; NAV2013R2; NAV2013";PreBuildEvent=;PostBuildEvent=" /v:minimal }
+    Exec { msbuild $SolutionFileName /t:Build /p:'Configuration = $Configuration; DefineConstants = "NAV2015; NAV2013R2; NAV2013"; PreBuildEvent = ; PostBuildEvent = " /v:minimal }
 }
 
 Task -Name Build2013R2 {
-    Exec { msbuild $SolutionFileName /t:Build /p:"Configuration=$Configuration;DefineConstants="NAV2013R2; NAV2013";PreBuildEvent=;PostBuildEvent=" /v:minimal }
+    Exec { msbuild $SolutionFileName /t:Build /p:"Configuration = $Configuration; DefineConstants = "NAV2013R2; NAV2013"; PreBuildEvent = ; PostBuildEvent = " /v:minimal }
 }
 
 Task -Name Build2013 {
-    Exec { msbuild $SolutionFileName /t:Build /p:"Configuration=$Configuration;DefineConstants="NAV2013";PreBuildEvent=;PostBuildEvent=" /v:minimal }
+    Exec { msbuild $SolutionFileName /t:Build /p:"Configuration = $Configuration; DefineConstants = "NAV2013"; PreBuildEvent = ; PostBuildEvent = " /v:minimal }
 }
+#>
