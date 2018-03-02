3f12e20a61f04bb0063159e727cf6577d6939fb3
Jan Hoek <github@uncommonsense.nl>
2018-03-02 19:40:29 +0100

    Added missing versions

--- CBreeze/UncommonSense.CBreeze.Core/Properties/AssemblyInfo.cs
+++ CBreeze/UncommonSense.CBreeze.Core/Properties/AssemblyInfo.cs
@@ -7,3 +7,6 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCompany("uncommonsense.nl")]
 [assembly: AssemblyProduct("UncommonSense.CBreeze.Automation")]
 [assembly: AssemblyCopyright("Copyright © 2018 Jan Hoek")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+
