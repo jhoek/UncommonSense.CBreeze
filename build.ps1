14d7be8b5ee50f47a1236179c54fdf51dfdc83f3
Jan Hoek <github@uncommonsense.nl>
2018-03-02 20:13:05 +0100

    Removed -Container parameter

--- build.ps1
+++ build.ps1
@@ -54,11 +54,8 @@ function Invoke-MSBuild
         }
     }
 
-    Exec {
-        # /property:OutputPath=$OutputFolderPath
-        msbuild /target:$Target /property:Configuration=$Configuration /property:DefineConstants="$DefineConstants" /property:PreBuildEvent=$PreBuildEvent /property:PostBuildEvent=$PostBuildEvent /verbosity:$Verbosity $SolutionFileName
-        Move-Item -Path (Join-Path -Path $SolutionFolder -ChildPath "UncommonSense.CBreeze.Automation/bin/$Configuration/UncommonSense.CBreeze.Automation/*") -Destination $OutputFolderPath -Container
-    }
+    Exec { msbuild /target:$Target /property:Configuration=$Configuration /property:DefineConstants="$DefineConstants" /property:PreBuildEvent=$PreBuildEvent /property:PostBuildEvent=$PostBuildEvent /verbosity:$Verbosity $SolutionFileName }
+    Move-Item -Path (Join-Path -Path $SolutionFolder -ChildPath "UncommonSense.CBreeze.Automation/bin/$Configuration/UncommonSense.CBreeze.Automation/*") -Destination $OutputFolderPath
 }
 
 Task -Name Default -Depends Build2017, Build2016
