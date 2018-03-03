550c19be0039f1a2e2e2e2ae12a2a5f542ff9daf
Jan Hoek <github@uncommonsense.nl>
2018-03-03 18:51:33 +0100

    Added descriptions

--- CBreeze/UncommonSense.CBreeze.Automation/CompileCBreezeApplication.cs
+++ CBreeze/UncommonSense.CBreeze.Automation/CompileCBreezeApplication.cs
@@ -8,42 +8,21 @@ using UncommonSense.CBreeze.IO;
 
 namespace UncommonSense.CBreeze.Automation
 {
+    /// <summary>
+    /// <para type="description">Compiles one or more objects in a Microsoft Dynamics NAV database</para>
+    /// </summary>
     [Cmdlet("Compile", "CBreezeApplication")]
     [Alias("Compile")]
     public class CompileCBreezeApplication : DevClientCmdlet
     {
-        public CompileCBreezeApplication()
-        {
-            ServerName = ".";
-        }
+        /// <summary>
+        /// <para type="description">Defines the set of objects to be compiled</para>
+        /// </summary>
+        [Parameter(Mandatory = true, ValueFromPipeline = true, Position = 0)] public Application Application { get; set; }
 
-        [Parameter(Mandatory = true, ValueFromPipeline = true, Position = 0)]
-        public Application Application
-        {
-            get;
-            set;
-        }
-
-        [Parameter()]
-        public string DevClientPath
-        {
-            get;
-            set;
-        }
-
-        [Parameter()]
-        public string ServerName
-        {
-            get;
-            set;
-        }
-
-        [Parameter(Mandatory = true)]
-        public string Database
-        {
-            get;
-            set;
-        }
+        [Parameter()] public string DevClientPath { get; set; }
+        [Parameter()] public string ServerName { get; set; } = ".";
+        [Parameter(Mandatory = true)] public string Database { get; set; }
 
         protected override void ProcessRecord()
         {
