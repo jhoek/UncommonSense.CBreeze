5d83e60a9c36ba5ddb6a0dcf713b42d39c5a6ac3
Jan Hoek <github@uncommonsense.nl>
2018-04-04 15:16:45 +0200

    Updated assembly info files

--- CBreeze/UncommonSense.CBreeze.Write/Properties/AssemblyInfo.cs
+++ CBreeze/UncommonSense.CBreeze.Write/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("3e128a93-651e-426f-9847-d8035d087f1d")]
-[assembly: AssemblyVersion("0.1.10004.1")]
-[assembly: AssemblyFileVersion("0.1.10004.1")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
