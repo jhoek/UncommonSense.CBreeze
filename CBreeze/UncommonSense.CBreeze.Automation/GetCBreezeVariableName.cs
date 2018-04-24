ce67f3a0b3d8980a6f56e9c9f73c7d62163f82a6
Jan Hoek <github@uncommonsense.nl>
2018-04-24 12:57:29 +0200

    Added Get-CBreezeVariableName

---
+++ CBreeze/UncommonSense.CBreeze.Automation/GetCBreezeVariableName.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+using UncommonSense.CBreeze.Core;
+
+namespace UncommonSense.CBreeze.Automation
+{
+    /// <summary>
+    /// <para type="synopsis">Converts a name that may contain spaces and special characters to a valid C/SIDE variable name</para>
+    /// </summary>
+    [Cmdlet(VerbsCommon.Get, "CBreezeVariableName")]
+    [OutputType(typeof(string))]
+    public class GetCBreezeVariableName : Cmdlet
+    {
+        /// <summary>
+        /// <para type="synopsis">The name to be converted</para>
+        /// </summary>
+        [Parameter(Mandatory = true, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true)]
+        public string[] Name { get; set; }
+
+        /// <exclude/>
+        protected override void ProcessRecord()
+        {
+            foreach (var name in Name)
+            {
+                WriteObject(name.MakeVariableName());
+            }
+        }
+    }
+}
