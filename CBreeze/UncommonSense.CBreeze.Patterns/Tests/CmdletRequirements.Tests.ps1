6707e7ce10fcd57cf5a8dc3f093c193894673e3f
Jan Hoek <github@uncommonsense.nl>
2018-08-04 19:26:40 +0200

    Experimenting with Pester tests to uniformize cmdlets

---
+++ CBreeze/UncommonSense.CBreeze.Patterns/Tests/CmdletRequirements.Tests.ps1
@@ -0,0 +1,17 @@
+Describe 'UncommonSense.CBreeze.Patterns cmdlets' {
+    It 'Cmdlets should have Table as OutputType' {
+        Get-ChildItem -Path $PSScriptRoot/../Public/*.ps1 |
+            Should -FileContentMatch '\[OutputType\(\[Table\]\)\]'
+    }
+
+    It 'Cmdlets should return a Table' {
+        Get-ChildItem -Path $PSScriptRoot/../Public/*.ps1 | 
+            Should -FileContentMatch '^\s*\$Table$'
+    }
+
+    It 'Cmdlets should have consistent names' {
+        Get-ChildItem -Path $PSScriptRoot/../Public/*.ps1 |
+            Select-Object -ExpandProperty BaseName |
+            Should -Not -Match 'Field$'
+    }
+}
