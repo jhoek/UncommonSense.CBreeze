1d51ae890a31c522b848e2da298ce825d5083d01
Jan Hoek <github@uncommonsense.nl>
2018-08-21 14:48:25 +0200

    Test with entire folder of objects

--- CBreeze/UncommonSense.CBreeze.Test/Program.cs
+++ CBreeze/UncommonSense.CBreeze.Test/Program.cs
@@ -14,13 +14,13 @@ namespace UncommonSense.CBreeze.Test
     {
         private static void Main(string[] args)
         {
-            //ApplicationBuilder
-            //    .ReadFromFolder(@"C:\Users\jhoek\Desktop\Input")
-            //    .WriteToFolder(@"c:\users\jhoek\Desktop\Output");
-
             ApplicationBuilder
-                .ReadFromFile(@"C:\Users\jhoek\Desktop\df.txt")
-                .WriteToFile(@"C:\Users\jhoek\Desktop\df.out.txt");
+                .ReadFromFolder(@"C:\Users\jhoek\Desktop\Input")
+                .WriteToFolder(@"c:\users\jhoek\Desktop\Output");
+
+            //ApplicationBuilder
+            //    .ReadFromFile(@"C:\Users\jhoek\Desktop\df.txt")
+            //    .WriteToFile(@"C:\Users\jhoek\Desktop\df.out.txt");
         }
     }
 }
