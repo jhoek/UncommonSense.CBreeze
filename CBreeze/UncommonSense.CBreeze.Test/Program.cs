06a11bd655e993cdf99fd850698378ed5020cbc4
Jan Hoek <github@uncommonsense.nl>
2018-08-21 14:17:15 +0200

    Test with Data Framework objects

--- CBreeze/UncommonSense.CBreeze.Test/Program.cs
+++ CBreeze/UncommonSense.CBreeze.Test/Program.cs
@@ -14,9 +14,13 @@ namespace UncommonSense.CBreeze.Test
     {
         private static void Main(string[] args)
         {
+            //ApplicationBuilder
+            //    .ReadFromFolder(@"C:\Users\jhoek\Desktop\Input")
+            //    .WriteToFolder(@"c:\users\jhoek\Desktop\Output");
+
             ApplicationBuilder
-                .ReadFromFolder(@"C:\Users\jhoek\Desktop\Input")
-                .WriteToFolder(@"c:\users\jhoek\Desktop\Output");
+                .ReadFromFile(@"C:\Users\jhoek\Desktop\df.txt")
+                .WriteToFile(@"C:\Users\jhoek\Desktop\df.out.txt");
         }
     }
 }
