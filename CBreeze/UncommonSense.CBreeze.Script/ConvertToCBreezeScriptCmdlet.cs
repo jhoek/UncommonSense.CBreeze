b007062425a263c867e099444cb8794a0e7eafb5
Jan Hoek <github@uncommonsense.nl>
2018-08-23 16:01:01 +0200

    Remove parameter set for writing to stdout; only write to a file, because we need to know the output directory, so that we can write additional files

--- CBreeze/UncommonSense.CBreeze.Script/ConvertToCBreezeScriptCmdlet.cs
+++ CBreeze/UncommonSense.CBreeze.Script/ConvertToCBreezeScriptCmdlet.cs
@@ -9,37 +9,23 @@ using UncommonSense.CBreeze.Core;
 
 namespace UncommonSense.CBreeze.Script
 {
-    [Cmdlet(VerbsData.ConvertTo, "CBreezeScript", DefaultParameterSetName = ParameterSets.ToStdOut)]
-    [OutputType(typeof(string), ParameterSetName = new string[] { ParameterSets.ToStdOut })]
-    [OutputType(typeof(FileInfo), ParameterSetName = new string[] { ParameterSets.ToPath })]
+    [Cmdlet(VerbsData.ConvertTo, "CBreezeScript")]
+    [OutputType(typeof(FileInfo))]
     public class ConvertToCBreezeScriptCmdlet : PSCmdlet
     {
-        public class ParameterSets
-        {
-            public const string ToPath = "ToPath";
-            public const string ToStdOut = "ToStdOut";
-        }
-
         [Parameter(Mandatory = true, ValueFromPipeline = true)]
         public Application Application { get; set; }
 
-        [Parameter(Mandatory = true, Position = 0, ParameterSetName = ParameterSets.ToPath)]
+        [Parameter(Mandatory = true, Position = 0)]
         public string Path { get; set; }
 
         protected override void EndProcessing()
         {
-            switch (ParameterSetName)
-            {
-                case ParameterSets.ToStdOut:
-                    WriteObject(Application.ToInvocation().ToString());
-                    break;
+            var path = GetUnresolvedProviderPathFromPSPath(Path);
+            var directory = System.IO.Path.GetDirectoryName(path);
 
-                case ParameterSets.ToPath:
-                    var path = GetUnresolvedProviderPathFromPSPath(Path);
-                    File.WriteAllText(path, Application.ToInvocation().ToString(), Encoding.UTF8);
-                    WriteObject(new FileInfo(path));
-                    break;
-            }
+            File.WriteAllText(path, Application.ToInvocation(directory).ToString(), Encoding.UTF8);
+            WriteObject(new FileInfo(path));
         }
     }
 }
