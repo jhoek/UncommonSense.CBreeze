88d68c1ef8a64dfaf38b5771d56284bdc3d2d9f4
Jan Hoek <github@uncommonsense.nl>
2018-08-29 14:48:50 +0200

    Use test folders

--- CBreeze/UncommonSense.CBreeze.Test/Program.cs
+++ CBreeze/UncommonSense.CBreeze.Test/Program.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UncommonSense.CBreeze.Core;
-using UncommonSense.CBreeze.Read;
+﻿using UncommonSense.CBreeze.Read;
 using UncommonSense.CBreeze.Write;
 
 namespace UncommonSense.CBreeze.Test
@@ -15,12 +8,12 @@ namespace UncommonSense.CBreeze.Test
         private static void Main(string[] args)
         {
             ApplicationBuilder
-                .ReadFromFolder(@"C:\Users\jhoek\Desktop\Input")
-                .WriteToFolder(@"c:\users\jhoek\Desktop\Output");
+                .ReadFromFolder(@"C:\Users\jhoek\Desktop\2018\input\nl\rtm")
+                .WriteToFolder(@"c:\users\jhoek\Desktop\2018\Output");
 
             //ApplicationBuilder
-            //    .ReadFromFile(@"C:\Users\jhoek\Desktop\df.txt")
-            //    .WriteToFile(@"C:\Users\jhoek\Desktop\df.out.txt");
+            //    .ReadFromFile(@"C:\Users\jhoek\Desktop\2018\Input\NL\rtm\PAG1170.TXT")
+            //    .WriteToFile(@"C:\Users\jhoek\Desktop\out.txt");
         }
     }
 }
