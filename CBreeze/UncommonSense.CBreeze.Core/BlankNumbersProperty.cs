3639dd886c4e9281c5f7dcc530f1111d8554e9fb
Jan Hoek <github@uncommonsense.nl>
2019-04-15 09:51:11 +0200

    Minor reformatting

--- CBreeze/UncommonSense.CBreeze.Core/BlankNumbersProperty.cs
+++ CBreeze/UncommonSense.CBreeze.Core/BlankNumbersProperty.cs
@@ -1,15 +1,7 @@
-using System;
-using System.Linq;
-using System.Collections;
-using System.Collections.Generic;
-
 namespace UncommonSense.CBreeze.Core
 {
-        public class BlankNumbersProperty : NullableValueProperty<BlankNumbers>
+    public class BlankNumbersProperty : NullableValueProperty<BlankNumbers>
     {
-        internal BlankNumbersProperty(string name)
-            : base(name)
-        {
-        }
+        internal BlankNumbersProperty(string name) : base(name) { }
     }
 }
