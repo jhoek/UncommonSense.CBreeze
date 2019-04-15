41fd41197ad4e932c7309719c8737a4e79b3dde2
Jan Hoek <github@uncommonsense.nl>
2019-04-15 12:04:57 +0200

    Test with pages

--- CBreeze/UncommonSense.CBreeze.Test/Program.cs
+++ CBreeze/UncommonSense.CBreeze.Test/Program.cs
@@ -7,13 +7,13 @@ namespace UncommonSense.CBreeze.Test
     {
         private static void Main(string[] args)
         {
-            //ApplicationBuilder
-            //    .ReadFromFolder(@"C:\Users\jhoek\Desktop\objects\codeunits")
-            //    .WriteToFolder(@"c:\users\jhoek\Desktop\output");
-
             ApplicationBuilder
-                .ReadFromFile(@"C:\Users\jhoek\Desktop\objects\codeunits\cod1008.TXT")
-                .WriteToFile(@"C:\Users\jhoek\Desktop\output.txt");
+                .ReadFromFolder(@"C:\Users\jhoek\Desktop\objects\pages")
+                .WriteToFolder(@"c:\users\jhoek\Desktop\output");
+
+            //ApplicationBuilder
+            //    .ReadFromFile(@"C:\Users\jhoek\Desktop\objects\codeunits\cod1008.TXT")
+            //    .WriteToFile(@"C:\Users\jhoek\Desktop\output.txt");
         }
     }
 }
