e3fb3db3c7243212c20c28db7e53e15575f9a10c
Jan Hoek <github@uncommonsense.nl>
2019-04-15 10:15:00 +0200

    Test with all objects again

--- CBreeze/UncommonSense.CBreeze.Test/Program.cs
+++ CBreeze/UncommonSense.CBreeze.Test/Program.cs
@@ -7,13 +7,13 @@ namespace UncommonSense.CBreeze.Test
     {
         private static void Main(string[] args)
         {
-            //ApplicationBuilder
-            //    .ReadFromFolder(@"C:\Users\jhoek\Desktop\objects")
-            //    .WriteToFolder(@"c:\users\jhoek\Desktop\output");
+            ApplicationBuilder
+                .ReadFromFolder(@"C:\Users\jhoek\Desktop\objects")
+                .WriteToFolder(@"c:\users\jhoek\Desktop\output");
 
-            ApplicationBuilder            
-                .ReadFromFile(@"C:\Users\jhoek\Desktop\objects\PAG1.TXT")
-                .WriteToFile(@"C:\Users\jhoek\Desktop\output.txt");
+            //ApplicationBuilder            
+            //    .ReadFromFile(@"C:\Users\jhoek\Desktop\objects\PAG1.TXT")
+            //    .WriteToFile(@"C:\Users\jhoek\Desktop\output.txt");
         }
     }
 }
